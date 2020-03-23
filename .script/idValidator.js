52e8af5a83fc7c1cca13efb5524e80993e07a545
necoh <t-necoh@microsoft.com>
2020-03-23 20:28:52 +0200

    add logs

--- .script/idValidator.js
+++ .script/idValidator.js
@@ -9,23 +9,27 @@ async function getDiff() {
     let pr = await avocado.devOps.createPullRequestProperties(config);
     let changedFiles = await pr.diff();
     console.log("Changed files:\n" + changedFiles);
-    var options = [pr.targetBranch, pr.sourceBranch];
-
-    try {
+    
+    for (const filePath of changedFiles) {
+        var options = [pr.targetBranch, pr.sourceBranch, filePath];
         diffSummary = await git(workingDir).diff(options, null);
-    }
-    catch (e) {
-        console.log("An error occerrued")
-    }
-    return diffSummary;
+        if (diffSummary.search(templateIdRegex) > 0){
+            console.log("Some of the files ID has changed")
+        }    
+        else {
+            console.log("All tests passed successfuly")
+        }    
+    }    
+
 }
 
-getDiff().then(function(result){
-    console.log(result);
-    if (result.search(templateIdRegex) > 0){
-        console.log("Some of the files ID has changed")
-    }    
-    else {
-        console.log("All tests passed successfuly")
-    }    
-})    
+getDiff();
+// getDiff().then(function(result){
+//     console.log(result);
+//     if (result.search(templateIdRegex) > 0){
+//         console.log("Some of the files ID has changed")
+//     }    
+//     else {
+//         console.log("All tests passed successfuly")
+//     }    
+// })    
