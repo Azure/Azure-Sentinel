67fd265ee7ac98ccd90f5edec263a86667b8b0f8
necoh <t-necoh@microsoft.com>
2020-03-23 18:25:55 +0200

    fix

--- .script/idValidator.js
+++ .script/idValidator.js
@@ -1,20 +1,12 @@
 const git = require('simple-git/promise');
 var templateIdRegex = "id: [0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
 var targetBranch = "master";
-var sourceBranch = null;
 var workingDir = __dirname
-
 
 async function getBranch(options) {
     var branchSummary = await git(workingDir).branch(options, null);
-    
     return branchSummary;
 }
-
-getBranch([]).then(branchs => sourceBranch = branchs.current);
-
-var options = [sourceBranch, targetBranch];
-////var options = ["--name-status"];
 
 async function getDiff(options) {
    let diffSummary = null;
@@ -27,14 +19,20 @@ async function getDiff(options) {
    return diffSummary;
 }
 
-getDiff(options).then(function(result){
-    if (result.search(templateIdRegex) > 0){
-        console.log("Some of the files ID has changed")
-    }
-    else {
-        console.log("All tests passed successfuly")
-    }
-})
+
+getBranch([]).then(function(branches){
+    var options = [branches.current, targetBranch];
+
+    getDiff(options).then(function(result){
+        if (result.search(templateIdRegex) > 0){
+            console.log("Some of the files ID has changed")
+        }    
+        else {
+            console.log("All tests passed successfuly")
+        }    
+    })    
+});    
+
 
 
 
