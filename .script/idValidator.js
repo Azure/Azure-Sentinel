76ad551b0fef9524b62b8b4b30c27e3a56479738
necoh <t-necoh@microsoft.com>
2020-03-23 20:14:36 +0200

    add logs

--- .script/idValidator.js
+++ .script/idValidator.js
@@ -1,22 +1,14 @@
 const git = require('simple-git/promise');
 const avocado = require("@azure/avocado");
-var templateIdRegex = "id: [0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
-var targetBranch = process.env.SYSTEM_PULLREQUEST_TARGETBRANCH;
-// const sourceBranch = 'source-b6791c5f-e0a5-49b1-9175-d7fd3e341cb8'
-
-
-
-// async function getBranch(options) {
-//     var branchSummary = await git(workingDir).branch(options, null);
-//     return branchSummary;
-// }
+const templateIdRegex = "id: [0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
 
 async function getDiff() {
     let diffSummary = null;
-    var config = avocado.cli.defaultConfig();
-    var workingDir = config.cwd;
-    var pr = await avocado.devOps.createPullRequestProperties(config);
-    // sourceBranch = pr.sourceBranch;
+    let config = avocado.cli.defaultConfig();
+    let workingDir = config.cwd;
+    let pr = await avocado.devOps.createPullRequestProperties(config);
+    let changedFiles = await pr.diff();
+    console.log("Changed files:\n" + changedFiles);
     var options = [pr.targetBranch, pr.sourceBranch];
 
     try {
@@ -28,8 +20,6 @@ async function getDiff() {
     return diffSummary;
 }
 
-
-
 getDiff().then(function(result){
     console.log(result);
     if (result.search(templateIdRegex) > 0){
@@ -39,36 +29,3 @@ getDiff().then(function(result){
         console.log("All tests passed successfuly")
     }    
 })    
-
-
-// getBranch([]).then(function(branches){
-//     console.log("target: " + targetBranch);
-//     console.log("source: " + sourceBranch);
-//     // console.log("branch name: " + branches.current);
-//     var options = [targetBranch, sourceBranch];
-
-//     getDiff(options).then(function(result){
-//         console.log(result);
-//         if (result.search(templateIdRegex) > 0){
-//             console.log("Some of the files ID has changed")
-//         }    
-//         else {
-//             console.log("All tests passed successfuly")
-//         }    
-//     })    
-// });    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
