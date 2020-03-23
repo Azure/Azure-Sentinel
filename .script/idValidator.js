70f26070367ee70f8cb5b4ad88f6626a8d9aecc8
necoh <t-necoh@microsoft.com>
2020-03-23 21:15:49 +0200

    add logs

--- .script/idValidator.js
+++ .script/idValidator.js
@@ -4,15 +4,6 @@ const avocado = require("@azure/avocado");
 const templateIdRegex = "id: [0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
 console.log(process.cwd());
 
-// let fileTypeSuffixes = ["json", "yaml", "yml"];
-// const changedFiles = await GetDiffFiles(fileTypeSuffixes);
-// console.log("-------Test:    " + changedFiles[0].path);
-
-let a = 5;
-let b = 10;
-console.log(`Fifteen is ${a + b} and
-not ${2 * a + b}.`);
-
 
 async function getDiff() {
     let diffSummary = null;
@@ -20,19 +11,17 @@ async function getDiff() {
     let workingDir = config.cwd;
     let pr = await avocado.devOps.createPullRequestProperties(config);
     let changedFiles = await pr.diff();
-    
+    console.log(`${changedFiles.length} files changed in current PR`);
+
     for (const file of changedFiles) {
-        var filePath = workingDir + '/' + file.path;
-        console.log("-------------------\nFile path: " + filePath + "\n---------------------------------")
+        let filePath = workingDir + '/' + file.path;
+        console.log(`----File path: ${filePath} ----`);
         var options = [pr.targetBranch, pr.sourceBranch, filePath];
         diffSummary = await git(workingDir).diff(options, null);
-        console.log(diffSummary);
         if (diffSummary.search(templateIdRegex) > 0){
-            console.log('File - ' +file.path + "is incorrect. Don't change id please");
+            console.log(diffSummary);
+            console.log(`As you can see, the id of file - "${filePath}" has changed.`);
             return -1;
-        }    
-        else {
-            console.log("All tests passed successfuly")
         }    
     }    
 
@@ -40,16 +29,8 @@ async function getDiff() {
 }
 
 getDiff().then(function(result){
+    if (result == 0){
+        console.log(`${changedFiles.length} files changed in current PR`);
+    }
     process.exit(result);
 })    
-
-
-// getDiff().then(function(result){
-//     console.log(result);
-//     if (result.search(templateIdRegex) > 0){
-//         console.log("Some of the files ID has changed")
-//     }    
-//     else {
-//         console.log("All tests passed successfuly")
-//     }    
-// })    
