1a70bcacc2bf03ae308af563709ec6ec79ff4f65
Maitreya Badola <mabadola@microsoft.com>
2020-04-22 19:35:54 +0530

    Extract teams logs using O365 Activity Management API.
    This function app will poll O365 Activity Managment API every 10 mins for logs. It is designed to get Teams events under Audit.General.

---
+++ DataConnectors/O365 DataCSharp/Teams.CustomConnector.Common/KeyVaultHelper.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace Teams.CustomConnector.Common
+{
+    public class KeyVaultHelper
+    {
+        //Azure Functions are stateless in nature. Therefore even though we use the standard.Net objects to cache values, 
+        //they don’t persist if the Azure Function scales out or is idle for some time.
+        //In consumption plan, functionTimeout is 10 minutes (max).
+        //for app service plan its configurable as per platform and will persists cache for the configuered time.
+
+        //Available approaches
+        //1. In memory using standard dotnet objects, downside of using this approach is, during scale out operation it will be again reach out to external endpoint. 
+        //2. using external (Redis) 
+        //3. External storage options (table/blobs) etc, This will require encryption/Decryption logic along with implementation of push/pull mechanism
+
+        //This lightweight keyvaulthelper is will use option 1 for caching during the available functionapp lifetime
+
+        static readonly ObjectCache secretCache = MemoryCache.Default;
+        private static readonly RetryWithExponentialBackoff retryWithExponentialBackoff = new RetryWithExponentialBackoff(5);
+
+
+        /// <summary>Gets the key value asynchronous.</summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public static async Task<string> GetKeyValueAsync(string key)
+        {
+            string secretValue;
+            if (Convert.ToBoolean(Environment.GetEnvironmentVariable(Constants.KeyVaultEnabled)) && !string.IsNullOrWhiteSpace(key) )
+            {
+                CacheItem cacheContents = secretCache.GetCacheItem(key);
+                if (cacheContents == null)
+                {
+                    // cache doesn’t exist. Add key to cache.
+                    CacheItemPolicy policy = new CacheItemPolicy
+                    {
+                        Priority = CacheItemPriority.Default,
+                        // Setting expiration timing for the cache, although doesn't make sense in consumption plan
+                        // for ASP , we can get this value from config, to get more control
+                        AbsoluteExpiration = DateTimeOffset.Now.AddHours(24)
+                    };
+
+                    secretValue = await LoadKeysFromVault(key);
+
+                    cacheContents = new CacheItem(key, secretValue);
+                    secretCache.Set(cacheContents, policy);
+                }
+                else
+                {
+                    secretValue = cacheContents.Value.ToString();
+                }
+            }
+            else
+            {
+                secretValue = key;
+            }
+            return secretValue;
+        }
+
+
+
+        /// <summary>Loads the keys from vault.</summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        private static async Task<string> LoadKeysFromVault(string key)
+        {
+            try
+            {
+                var baseUrl = Environment.GetEnvironmentVariable(Constants.KeyVaultBaseUrl);
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+                SecretBundle secretBundle = null;
+
+                await retryWithExponentialBackoff.RunAsync(async () =>
+                {
+                    secretBundle = await keyVaultClient.GetSecretAsync($"{baseUrl}/secrets/{key}");
+                });
+
+                return secretBundle?.Value;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+     
+    }
+}
---
+++ DataConnectors/O365 DataCSharp/Teams.CustomConnector.StorageHandler/StorageHandler.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Teams.CustomConnector.Common;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Constants = Teams.CustomConnector.Common.Constants;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Teams.CustomConnector.StorageHandler
+{
+
+    /// <summary></summary>
+    public enum OperationType
+    {
+        Data,
+        Log
+    }
+
+    /// <summary></summary>
+    public class StorageHandler
+    {
+        readonly ILogger log;
+        private CloudStorageAccount storageAccount;
+        public StorageHandler(ILogger log)
+        {
+            this.log = log;
+        
+        }
+
+
+        /// <summary>Uploads the data to container asynchronous.</summary>
+        /// <param name="opType">Type of the op.</param>
+        /// <param name="blobName">Name of the BLOB.</param>
+        /// <param name="data">The data.</param>
+        public async Task UploadDataToContainerAsync(OperationType opType, string blobName, OperationDetails operationDetails)
+        {
+            log.LogInformation(Common.Constants.UploadDataToStorageContainerStarted);
+            try
+            {
+                CreateCloudStroageHandler();
+                var myClient = storageAccount.CreateCloudBlobClient();
+                var container = myClient.GetContainerReference(opType == OperationType.Data ? Environment.GetEnvironmentVariable(Constants.DataContainerName) : Environment.GetEnvironmentVariable(Constants.LogContainerName));
+                var blockBlob = container.GetBlockBlobReference(blobName);
+                var data = Newtonsoft.Json.JsonConvert.SerializeObject(operationDetails);
+                byte[] byteArray = Encoding.ASCII.GetBytes(data);
+                MemoryStream stream = new MemoryStream(byteArray);
+                await blockBlob.UploadFromStreamAsync(stream);
+                log.LogInformation(Common.Constants.UploadDataToStorageContainerCompleted);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.InnerException.ToString());
+                throw;
+            }
+        }
+
+        /// <summary>Uploads the data to container asynchronous.</summary>
+        /// <param name="opType">Type of the op.</param>
+        /// <param name="blobName">Name of the BLOB.</param>
+        /// <param name="data">The data.</param>
+        public async Task UploadDataToContainerAsync(OperationType opType, string blobName, string data)
+        {
+            log.LogInformation(Common.Constants.UploadDataToStorageContainerStarted);
+            try
+            {
+                CreateCloudStroageHandler();
+                var myClient = storageAccount.CreateCloudBlobClient();
+                var container = myClient.GetContainerReference(opType == OperationType.Data ? Environment.GetEnvironmentVariable(Constants.DataContainerName) : Environment.GetEnvironmentVariable(Constants.LogContainerName));
+                var blockBlob = container.GetBlockBlobReference(blobName);
+                byte[] byteArray = Encoding.ASCII.GetBytes(data);
+                MemoryStream stream = new MemoryStream(byteArray);
+                await blockBlob.UploadFromStreamAsync(stream);
+                log.LogInformation(Common.Constants.UploadDataToStorageContainerCompleted);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.InnerException.ToString());
+                throw;
+            }
+        }
+
+
+        /// <summary>Gets the last operation details from logs asynchronous.</summary>
+        /// <param name="blobName">Name of the BLOB.</param>
+        /// <returns></returns>
+        public async Task<OperationDetails> GetLastOperationDetailsFromLogsAsync(string blobName)
+        {
+            OperationDetails operationDetails = null;
+            try
+            {
+                log.LogInformation(Common.Constants.RequestLastExecutionTime);
+                CreateCloudStroageHandler();
+                var myClient = storageAccount.CreateCloudBlobClient();
+                var container = myClient.GetContainerReference(Environment.GetEnvironmentVariable(Constants.LogContainerName));
+                if (await container.ExistsAsync())
+                {
+                    var blockBlob = container.GetBlockBlobReference(blobName);
+                    if (await blockBlob.ExistsAsync())
+                    {
+                        var result = await blockBlob.DownloadTextAsync();
+                        operationDetails = Newtonsoft.Json.JsonConvert.DeserializeObject<OperationDetails>(result);
+                        return operationDetails;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.InnerException.ToString());
+             //   throw;
+            }
+            return operationDetails;
+        }
+
+        private void CreateCloudStroageHandler()
+        {
+          
+
+            bool.TryParse(Environment.GetEnvironmentVariable(Constants.KeyVaultEnabled), out bool isKeyVaultEnabled);
+            if (isKeyVaultEnabled)
+            {
+                storageAccount = CloudStorageAccount.Parse(KeyVaultHelper.GetKeyValueAsync(Constants.StorageContainerConnectionString).Result);
+
+            }
+            else
+            {
+                storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable(Constants.StorageContainerConnectionString));
+            }
+
+            EnsureRequiredContainers();
+        }
+
+        /// <summary>Ensures the required containers.</summary>
+        private async void EnsureRequiredContainers()
+        {
+            var myClient = storageAccount.CreateCloudBlobClient();
+            var container = myClient.GetContainerReference(Environment.GetEnvironmentVariable(Constants.DataContainerName));
+            await container.CreateIfNotExistsAsync(); //blocking call
+            container = myClient.GetContainerReference(Environment.GetEnvironmentVariable(Constants.LogContainerName));
+            await container.CreateIfNotExistsAsync(); //blocking call
+        }
+    }
+}
