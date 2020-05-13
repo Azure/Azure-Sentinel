419bdeed58e780b7ae5dfa08393d34160df972ca
Igal Shapira <igshapir@microsoft.com>
2020-05-13 13:11:52 +0300

    fix test

--- .script/documentsLinkValidator.ts
+++ .script/documentsLinkValidator.ts
@@ -6,9 +6,9 @@ import * as logger from "./utils/logger";
 export async function IsFileContainsLinkWithLocale(filePath: string): Promise<ExitCode> {
   const content = fs.readFileSync(filePath, "utf8");
   if (/(https:\/\/docs.microsoft.com|https:\/\/azure.microsoft.com)(\/[a-z]{2}-[a-z]{2})/i.test(content)) {
-      return ExitCode.SUCCESS;
+      return ExitCode.ERROR;
   }
-  return ExitCode.ERROR;
+  return ExitCode.SUCCESS;
 }
 
 let fileKinds = ["Added", "Modified"];
--- .script/tests/documentsLinkValidatorTest/documentsLinkValidator.Test.ts
+++ .script/tests/documentsLinkValidatorTest/documentsLinkValidator.Test.ts
@@ -1,4 +1,4 @@
-import { IsValidJsonFile } from "../../jsonFileValidator";
+import { IsFileContainsLinkWithLocale } from "../../documentsLinkValidator";
 import { ExitCode } from "../../utils/exitCode";
 import chai from "chai";
 import { expect } from "chai";
@@ -8,17 +8,17 @@ chai.use(chaiAsPromised);
 
 describe("documentsLinkValidator", () => {
   it("should pass when no links", async () => {
-    let result = await IsValidJsonFile(".script/tests/documentsLinkValidatorTest/nodoclinks.md");
+    let result = await IsFileContainsLinkWithLocale(".script/tests/documentsLinkValidatorTest/nodoclinks.md");
     expect(result).to.equal(ExitCode.SUCCESS);
   });
 
   it("should pass when link is valid", async () => {
-    let result = await IsValidJsonFile(".script/tests/documentsLinkValidatorTest/validlink.md");
-    expect(result).to.equal(ExitCode.ERROR);
+    let result = await IsFileContainsLinkWithLocale(".script/tests/documentsLinkValidatorTest/validlink.md");
+    expect(result).to.equal(ExitCode.SUCCESS);
   });
 
-  it("should fail when link conains locale", async () => {
-    let result = await IsValidJsonFile(".script/tests/documentsLinkValidatorTest/badlink.md");
+  it("should fail when link contains locale", async () => {
+    let result = await IsFileContainsLinkWithLocale(".script/tests/documentsLinkValidatorTest/badlink.md");
     expect(result).to.equal(ExitCode.ERROR);
   });
 });
