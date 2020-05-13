63e417af300e917f1c18241bb7680d1cba5bb29a
Igal Shapira <igshapir@microsoft.com>
2020-05-13 12:23:56 +0300

    fix

--- .script/documentsLinkValidator.ts
+++ .script/documentsLinkValidator.ts
@@ -5,7 +5,7 @@ import * as logger from "./utils/logger";
 
 export async function IsFileContainsLinkWithLocale(filePath: string): Promise<ExitCode> {
   const content = fs.readFileSync(filePath, "utf8");
-  if (content.test(/(https:\/\/docs.microsoft.com|https:\/\/azure.microsoft.com)(\/[a-z]{2}-[a-z]{2})/i)) {
+  if (/(https:\/\/docs.microsoft.com|https:\/\/azure.microsoft.com)(\/[a-z]{2}-[a-z]{2})/i.test(content)) {
       return ExitCode.SUCCESS;
   }
   return ExitCode.ERROR;
@@ -17,7 +17,7 @@ let CheckOptions = {
     return IsFileContainsLinkWithLocale(filePath);
   },
   onExecError: async (e: any, filePath: string) => {
-    console.log(`Documentation links should not include locale: ${filePath}`);
+    console.log(`Documentation links should not include locale: ${filePath}, ${e.message}`);
   },
   onFinalFailed: async () => {
     logger.logError("An error occurred, please open an issue");
