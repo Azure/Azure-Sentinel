b88ecec2a7e7cfe141f6cde5ea5fff40bc6c3384
Eran Toledano <eran.toledano@microsoft.com>
2020-08-14 11:28:27 -0700

    cosmetic fixes

--- .script/tests/jsonFileValidatorTest/jsonFileValidator.test.ts
+++ .script/tests/jsonFileValidatorTest/jsonFileValidator.test.ts
@@ -1,7 +1,8 @@
-import chai, { expect } from "chai";
-import chaiAsPromised from "chai-as-promised";
 import { IsValidJsonFile } from "../../jsonFileValidator";
 import { ExitCode } from "../../utils/exitCode";
+import chai from "chai";	
+import { expect } from "chai";	
+import chaiAsPromised from "chai-as-promised";
 
 chai.use(chaiAsPromised);
 
@@ -15,6 +16,8 @@ describe("jsonFileValidator", () => {
   it("should throw exception when json file is invalid", async () => {
     let filePath = ".script/tests/jsonFileValidatorTest/invalidFile.json";
 
-    await expect(IsValidJsonFile(filePath)).eventually.rejectedWith(Error).and.have.property("name", "SyntaxError");
+    await expect(IsValidJsonFile(filePath))
+      .eventually.rejectedWith(Error)
+      .and.have.property("name", "SyntaxError");
   });
 });
