a3a7b8a8c81795a3dcd0c5069edcd5cbbdb83fe9
Offir Shvartz <ofshvart@microsoft.com>
2020-09-06 13:43:31 +0300

    try add tests

---
+++ .azure-pipelines/KqlvalidationsTests/DetectionsYamlFilesTestData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kqlvalidations.Tests
+{
+    public class DetectionsYamlFilesTestData : TheoryData<string>
+    {
+        public DetectionsYamlFilesTestData()
+        {
+            var rootDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            var detectionPath = $@"{rootDir}\..\..\..\..\..\Detections".Replace(@"file:\", string.Empty);
+            var files = Directory.GetFiles(detectionPath, "*.yaml", SearchOption.AllDirectories).ToList();
+            files.ForEach(f => AddData(f));
+        }
+    }
+}
---
+++ .azure-pipelines/KqlvalidationsTests/KqlValidationTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Sentinel.KustoServices.Contract;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+using YamlDotNet.Serialization;
+
+namespace Kqlvalidations.Tests
+{
+    public class KqlValidationTests
+    {
+        private readonly IKqlQueryValidator _queryValidator;
+        public KqlValidationTests()
+        {
+            _queryValidator = new KqlQueryValidatorBuilder()
+               .WithSentinelDefaultTableSchemas()
+               //.WithCustomTableSchema(GetCustomLogsSchemas())
+               .Build();
+        }
+
+        [Theory]
+        [ClassData(typeof(DetectionsYamlFilesTestData))]
+        public void validate_detectionqueries_succeed(string detectionsYamlFile)
+        {
+            var yaml = File.ReadAllText(detectionsYamlFile);
+            var deserializer = new DeserializerBuilder().Build();
+            var res = deserializer.Deserialize<dynamic>(yaml);
+            string queryStr = res["query"];
+
+            var validationRes = _queryValidator.ValidateSyntax(queryStr);
+
+            Assert.True(validationRes.IsValid, validationRes.IsValid ? string.Empty : validationRes.Diagnostics.Select(d => d.Message).ToList().Aggregate((s1, s2) => s1 + "," + s2));
+        }
+
+    }
+}
