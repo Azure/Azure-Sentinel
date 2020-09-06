bc3e5ed8b09ba35559f9c0f8782bd92fe0b9a1ae
Offir Shvartz <ofshvart@microsoft.com>
2020-09-06 10:55:43 +0300

    try add tests

--- .azure-pipelines/KqlvalidationsTests/Class1.cs
+++ .azure-pipelines/KqlvalidationsTests/Class1.cs
@@ -6,6 +6,16 @@ namespace KqlvalidationsTests
     public class KqlValidationTests
     {
         [Fact]
-        public void  validate_detectionqueries_succeed() { }
+        public void  validate_detectionqueries_succeed() 
+        {
+            throw new ArgumentException("arg");
+        }
+
+
+        [Fact]
+        public void validate_detectionqueries_succeed2()
+        {
+           // throw new ArgumentException("arg");
+        }
     }
 }
