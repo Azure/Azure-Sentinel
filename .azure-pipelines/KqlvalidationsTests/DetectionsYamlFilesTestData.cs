211b84e490f6659f0352cb155ab8d580237f373b
Offir Shvartz <ofshvart@microsoft.com>
2020-09-06 14:37:48 +0300

    try add tests

--- .azure-pipelines/KqlvalidationsTests/DetectionsYamlFilesTestData.cs
+++ .azure-pipelines/KqlvalidationsTests/DetectionsYamlFilesTestData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Kqlvalidations.Tests
@@ -10,10 +11,22 @@ namespace Kqlvalidations.Tests
     {
         public DetectionsYamlFilesTestData()
         {
-            var rootDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
-            var detectionPath = $"{rootDir}/../../../../../Detections".Replace(@"file:/", string.Empty);
+            var rootDir = Directory.CreateDirectory(GetAssemblyDirectory());
+            for (int i = 0; i < 5; i++)
+            {
+                rootDir = rootDir.Parent;
+            }
+            var detectionPath = Path.Combine(rootDir.FullName, "Detections");
             var files = Directory.GetFiles(detectionPath, "*.yaml", SearchOption.AllDirectories).ToList();
             files.ForEach(f => AddData(f));
+        }
+
+        private static string GetAssemblyDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
         }
     }
 }
