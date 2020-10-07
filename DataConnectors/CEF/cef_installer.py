6a74a62e173e8026ccd7e90a9680c77360356a2c
Noam Landress <noamlandress@microsoft.com>
2020-10-07 09:27:54 +0300

    Potential bugfix if return code is empty due to bad regex parsing

--- DataConnectors/CEF/cef_installer.py
+++ DataConnectors/CEF/cef_installer.py
@@ -141,7 +141,7 @@ def install_omsagent(workspace_id, primary_key, oms_agent_install_url):
     if e is not None:
         handle_error(e, error_response_str="Error: could not install omsagent.")
         sys.exit()
-    elif return_code.group(1) != '0':
+    elif return_code is not None and return_code.group(1) != '0':
         handle_error(o, error_response_str="Error: could not install omsagent.")
         sys.exit()
     print_ok("Installed omsagent successfully.")
