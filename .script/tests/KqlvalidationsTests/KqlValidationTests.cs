c0f523377faae82f807bc782ff88ece32d227108
ehudk-msft <73052016+ehudk-msft@users.noreply.github.com>
2020-11-08 08:40:17 +0200

    validate template structure

--- .script/tests/KqlvalidationsTests/KqlValidationTests.cs
+++ .script/tests/KqlvalidationsTests/KqlValidationTests.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Azure.Sentinel.KustoServices.Contract;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Linq;
+using Microsoft.Azure.Sentinel.Analytics.Management.AnalyticsTemplatesService.Interface.Model;
+using Microsoft.Azure.Sentinel.KustoServices.Contract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 using YamlDotNet.Serialization;
 
@@ -13,6 +14,7 @@ namespace Kqlvalidations.Tests
     {
         private readonly IKqlQueryAnalyzer _queryValidator;
         private static readonly string DetectionPath = DetectionsYamlFilesTestData.GetDetectionPath();
+
         public KqlValidationTests()
         {
             _queryValidator = new KqlQueryAnalyzerBuilder()
@@ -33,14 +35,49 @@ namespace Kqlvalidations.Tests
             string id = res["id"];
 
             //we ignore known issues (in progress)
-            if (TemplatesToSkipValidationReader.WhiteListTemplateIds.Contains(id))
+            if (TemplatesToSkipValidationReader.WhiteListTemplateIds_MalformedQueries.Contains(id))
             {
                 return;
             }
             var validationRes = _queryValidator.ValidateSyntax(queryStr);
             Assert.True(validationRes.IsValid, validationRes.IsValid ? string.Empty : $"Template Id:{id} is not valid Errors:{validationRes.Diagnostics.Select(d => d.ToString()).ToList().Aggregate((s1, s2) => s1 + "," + s2)}");
         }
+
+        [Theory]
+        [ClassData(typeof(DetectionsYamlFilesTestData))]
+        public void Validate_DetectionQueries_HaveValidTemplateStructure(string detectionsYamlFileName)
+        {
+            var detectionsYamlFile = Directory.GetFiles(DetectionPath, detectionsYamlFileName, SearchOption.AllDirectories).Single();
+            var yaml = File.ReadAllText(detectionsYamlFile);
+
+            //we ignore known issues (in progress)
+            foreach (var templateToSkip in TemplatesToSkipValidationReader.WhiteListTemplateIds_MalformedTemplateStructure)
+            {
+                if (yaml.Contains(templateToSkip) || detectionsYamlFile.Contains(templateToSkip))
+                {
+                    return;
+                }
+            }
+
+            var jObj = JObject.Parse(ConvertYamlToJson(yaml));
+            jObj.Add("kind", "Scheduled");
+            jObj.Add("lastUpdatedDateUTC", DateTime.UtcNow);
+            jObj.Add("createdDateUTC", DateTime.UtcNow);
+
+            jObj.ToObject<ScheduledTemplateInternalModel>();
+        }
+
+        public static string ConvertYamlToJson(string yaml)
+        {
+            var deserializer = new Deserializer();
+            var yamlObject = deserializer.Deserialize<object>(yaml);
+
+            using (var jsonObject = new StringWriter())
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(jsonObject, yamlObject);
+                return jsonObject.ToString();
+            }
+        }
     }
-
 }
-
--- .script/tests/KqlvalidationsTests/TemplatesToSkipValidationReader.cs
+++ .script/tests/KqlvalidationsTests/TemplatesToSkipValidationReader.cs
@@ -8,18 +8,28 @@ namespace Kqlvalidations.Tests
 {
     public static class TemplatesToSkipValidationReader
     {
-        private const string SKipJsonFileName = "SkipValidationsTemplates.json";
+        private const string WhiteListTemplateIds_MalformedQueries_FileName = "SkipValidationsTemplates_MalformedQuery.json";
+        private const string WhiteListTemplateIds_TemplatesWithMalformedQueries_FileName = "SkipValidationsTemplates_MalformedTemplateStructure.json";
 
         static TemplatesToSkipValidationReader()
         {
-            var jsonFilePath = Path.Combine(DetectionsYamlFilesTestData.GetSkipTemplatesPath(), SKipJsonFileName);
+            var jsonFilePath = Path.Combine(DetectionsYamlFilesTestData.GetSkipTemplatesPath(), WhiteListTemplateIds_MalformedQueries_FileName);
             using (StreamReader r = new StreamReader(jsonFilePath))
             {
                 string json = r.ReadToEnd();
-                WhiteListTemplateIds = JsonConvert.DeserializeObject<IEnumerable<string>>(json);
+                WhiteListTemplateIds_MalformedQueries = JsonConvert.DeserializeObject<IEnumerable<string>>(json);
+            }
+
+            jsonFilePath = Path.Combine(DetectionsYamlFilesTestData.GetSkipTemplatesPath(), WhiteListTemplateIds_TemplatesWithMalformedQueries_FileName);
+            using (StreamReader r = new StreamReader(jsonFilePath))
+            {
+                string json = r.ReadToEnd();
+                WhiteListTemplateIds_MalformedTemplateStructure = JsonConvert.DeserializeObject<IEnumerable<string>>(json);
             }
         }
 
-        public static IEnumerable<string> WhiteListTemplateIds { get; private set; }
+        public static IEnumerable<string> WhiteListTemplateIds_MalformedQueries { get; private set; }
+
+        public static IEnumerable<string> WhiteListTemplateIds_MalformedTemplateStructure { get; private set; }
     }
 }
