3502112c6e81ea0c7e95720acfb90021b819c0b4
ehudk-msft <73052016+ehudk-msft@users.noreply.github.com>
2020-11-08 17:10:23 +0200

    seperate template structure validation to another pipeline

---
+++ .script/tests/TemplateStructureValidation/TemplateStructureValidationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Azure.Sentinel.Analytics.Management.AnalyticsTemplatesService.Interface.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using YamlDotNet.Serialization;
+
+namespace Kqlvalidations.Tests
+{
+    public class TemplateStructureValidationTests
+    {
+        private static readonly string DetectionPath = DetectionsYamlFilesTestData.GetDetectionPath();
+
+        [Theory]
+        [ClassData(typeof(DetectionsYamlFilesTestData))]
+        public void Validate_DetectionTemplates_HaveValidTemplateStructure(string detectionsYamlFileName)
+        {
+            var detectionsYamlFile = Directory.GetFiles(DetectionPath, detectionsYamlFileName, SearchOption.AllDirectories).Single();
+            var yaml = File.ReadAllText(detectionsYamlFile);
+
+            //we ignore known issues (in progress)
+            foreach (var templateToSkip in TemplatesToSkipValidationReader.WhiteListTemplateIds)
+            {
+                if (yaml.Contains(templateToSkip) || detectionsYamlFile.Contains(templateToSkip))
+                {
+                    return;
+                }
+            }
+
+            var jObj = JObject.Parse(ConvertYamlToJson(yaml));
+
+            jObj.ToObject<ScheduledTemplateInternalModel>();
+        }
+
+        public static string ConvertYamlToJson(string yaml)
+        {
+            var deserializer = new Deserializer();
+            var yamlObject = deserializer.Deserialize<object>(yaml);
+
+            using (var jsonObject = new StringWriter())
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(jsonObject, yamlObject);
+                return jsonObject.ToString();
+            }
+        }
+    }
+}
