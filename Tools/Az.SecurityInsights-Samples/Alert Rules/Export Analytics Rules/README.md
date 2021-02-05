1eda0a5510c81b9e239d2e2596f3aaa4bcc38869
Tiander Turpijn <tiandert@microsoft.com>
2021-02-05 20:10:06 +0100

    Added importAzureSentinelRules script
    This sample script allows bulk import of Azure Sentinel rules

--- Tools/Az.SecurityInsights-Samples/Alert Rules/Export Analytics Rules/README.md
+++ Tools/Az.SecurityInsights-Samples/Alert Rules/Export Analytics Rules/README.md
@@ -1,6 +1,7 @@
 # Export Azure Sentinel Analytics Rules
+Author: Tiander Turpijn - Microsoft
 
-**Objective:** Export Analytics Rules to a local folder <br/>
+**Objective:** Export Azure Sentinel analytics rules to a local folder <br/>
 
 **Current supported rule kinds:**
 * Scheduled
@@ -12,100 +13,26 @@
 <br/>
 
 **Prerequisites:**
-* Az.Accounts module
-* Az.SecurityInsights module
+The following Azure PowerShell modules are required and will be installed if missing:
+* Az.Accounts
+* Az.SecurityInsights
 * Azure Sentinel Reader permissions
+<br/><br/>
 
+#### Script Parameters
+The script will prompt you for the following parameter values:
+* *subscriptionId* - the subscription ID where your Azure Sentinel workspace resides in
+* *resourceGroupName* - the Azure resource group name where the Azure Sentinel workspace resides in
+* *workspaceName* - the name of the Azure Sentinel workspace
+* *ruleExportPath* - the name of the folder where your exported rules files will be stored, for example **c:\SentinelRules\Export** if the folder does not exist, the script will create it for you
+<br/><br/>
 
-**Required script values to update:**
-* Azure Subscription Id
-* Azure Resource Group name (where the Azure Sentinel workspace resides in)
-* Workspace name (the name of the Azure Sentinel workspace)
-* Target folder for the exported rules <br/>
-<br/>
-
-
-
+#### Running the script 
+After your have downloaded the sample script, you can run the script with parameters as follows:
 ```powershell
-# Sample script to export Azure Sentinel rules
-# Author: Tiander Turpijn - Microsoft
-# Dependencies: Az.Accounts and Az.SecurityInsights PowerShell modules imported,
-# please download from https://www.powershellgallery.com
-#
-# ToDo:
-# 1. Login to Azure
-# 2. Provide values for your Azure Sentinel Resource Group and Workspace
-# 3. Provide a name for your rule export folder
-
-$ErrorActionPreference = "Stop"
-
-# *** Values to update ***
-# Provide your Azure Sentinel connection settings
-$subscriptionId = "<yourAzureSubscriptionId>"
-$SentinelConnection = @{
-    ResourceGroupName = "<yourResourceGroupName>"  #your Resource Group name where your workspace is located
-    WorkspaceName = "<yourSentinelWorkspaceName>"  #your Sentinel Workspace name
-}
-# Configure your rule export folder
-$ruleExportPath = "C:\SentinelRules\Export\" # specify your rule export folder
-
-# Login to Azure and selecting your Azure Sentinel subscription -  make sure you have installed the Az.Accounts module
-Login-AzAccount
-Set-AzContext -SubscriptionId $subscriptionId
-
-# Testing your Azure connection
-$azureConnection = Get-AzContext
-If([string]::IsNullOrEmpty($azureConnection.Account)) {            
-    Write-Host ("You are not connected to Azure, please login") -ForegroundColor Red
-    break
-} 
-
-# Create folder if it does not exist
-if (!(Test-Path -Path $ruleExportPath))
-{
-    Write-Host ("Folder " + $ruleExportPath + " does not exist, creating the folder for you....") -ForegroundColor Red
-    New-Item -itemType Directory -Path $ruleExportPath
-}
-
-# Export Scheduled Rules
-try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "Scheduled"} 
-    Write-Host ("Exporting " + $rules.count + " Scheduled rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "Scheduled.json") -Force
-}
-catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
-    break
-}
-
-
-# Export Fusion Rules
-try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "Fusion"} 
-    Write-Host ("Exporting " + $rules.count + " Fusion rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "Fusion.json") -Force
-}
-catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
-    break
-}
-
-# Export MicrosoftSecurityIncidentCreation Rules
-try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "MicrosoftSecurityIncidentCreation"} 
-    Write-Host ("Exporting " + $rules.count + " MicrosoftSecurityIncidentCreation rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "MicrosoftSecurityIncidentCreation.json") -Force
-}
-catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
-    break
-}
-
-Write-Host ("Azure Analytics Rules are exported to " + $ruleExportPath) -ForegroundColor Yellow
+.\exportAzureSentinelRules.ps1 -subscriptionId "382b2a53-c53c-4092-8d4a-7210f6a44a0c" -resourceGroupName "mySentinelRG" -workspaceName "Sentinelworkspace" -ruleExportPath "C:\SentinelRules\Export"
 ```
+
 
 
 
--- Tools/Az.SecurityInsights-Samples/Alert Rules/Export Analytics Rules/exportAzureSentinelRules.ps1
+++ Tools/Az.SecurityInsights-Samples/Alert Rules/Export Analytics Rules/exportAzureSentinelRules.ps1
@@ -1,77 +1,121 @@
-# Sample script to export Azure Sentinel rules
-# Author: Tiander Turpijn - Microsoft
-# Dependencies: Az.Accounts and Az.SecurityInsights PowerShell modules imported,
-# please download from https://www.powershellgallery.com
-#
-# ToDo:
-# 1. Login to Azure
-# 2. Provide values for your Azure Sentinel Resource Group and Workspace
-# 3. Provide a name for your rule export folder
+param (
+    [parameter(Mandatory = $true)]
+    #The subscription that holds your Azure Sentinel workspace
+    [string] $subscriptionId,
+    [parameter(Mandatory = $true)]
+    # The resource group name which holds your Azure Sentinel workspace
+    [string] $resourceGroupName,
+    [parameter(Mandatory = $true)]
+    # The Azure Sentinel workspace name
+    [string] $workspaceName,
+    [parameter(Mandatory = $true)]
+    # The export folder name where your rules will be exported to, for example C:\SentinelRules\Export
+    [string] $ruleExportPath
+)
 
 $ErrorActionPreference = "Stop"
 
-# *** Values to update ***
-# Provide your Azure Sentinel connection settings
-$subscriptionId = "<yourAzureSubscriptionId>"
-$SentinelConnection = @{
-    ResourceGroupName = "<yourResourceGroupName>"  #your Resource Group name where your workspace is located
-    WorkspaceName = "<yourSentinelWorkspaceName>"  #your Sentinel Workspace name
-}
-# Configure your rule export folder
-$ruleExportPath = "C:\SentinelRules\Export\" # specify your rule export folder
-
-# Login to Azure and selecting your Azure Sentinel subscription -  make sure you have installed the Az.Accounts module
-Login-AzAccount
-Set-AzContext -SubscriptionId $subscriptionId
-
-# Testing your Azure connection
-$azureConnection = Get-AzContext
-If([string]::IsNullOrEmpty($azureConnection.Account)) {            
-    Write-Host ("You are not connected to Azure, please login") -ForegroundColor Red
+# Check powershell version, needs to be 5 or higher
+if ($host.Version.Major -lt 5) {
+    Write-Host "Supported PowerShell version for this script is 5 or above" -ForegroundColor Red
+    Write-Host "Aborting...." -ForegroundColor Red
     break
-} 
+}
+
+#Check if the Az.Accounts PowerShell module is installed, install if it is not
+$AzModule = Get-InstalledModule -Name Az.Accounts -ErrorAction SilentlyContinue
+if ($AzModule -eq $null) {
+    Write-Warning "Az.Accounts PowerShell module is not found"
+    #check for Admin Privleges
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+    if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+        #Not an Admin, install to current user
+        Write-Warning -Message "Can not install the Az.Accounts module. You are not running as Administrator"
+        Write-Warning -Message "Installing the Az.Accounts module to current user Scope"
+        Install-Module Az.Accounts -Scope CurrentUser -Force
+    }
+    Else {
+        #Admin, install to all users
+        Write-Warning -Message "Installing Az.Accounts module to all users"
+        Install-Module -Name Az.Accounts -AllowClobber -Force
+        Import-Module -Name Az.Accounts -Force
+    }
+}
+
+#Check if the Az.SecurityInsights module is installed, if not install it
+$AzSecurityInsightsModule = Get-InstalledModule -Name Az.SecurityInsights -ErrorAction SilentlyContinue
+if ($AzSecurityInsightsModule -eq $null) {
+    Write-Warning "The Az.SecurityInsights PowerShell module is not found"
+        #check for Admin Privleges
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+        if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+            #Not an Admin, install to current user
+            Write-Warning -Message "Can not install the Az.SecurityInsights module. You are not running as Administrator"
+            Write-Warning -Message "Installing Az.SecurityInsights module to current user Scope"
+            Install-Module Az.SecurityInsights -Scope CurrentUser -Force
+        }
+        Else {
+            #Admin, install to all users
+            Write-Warning -Message "Installing the Az.SecurityInsights module to all users"
+            Install-Module -Name Az.SecurityInsights -Force
+            Import-Module -Name Az.SecurityInsights -Force
+        }
+}
+
+#Check the Azure subscription context
+$subIdContext = (Get-AzContext).Subscription.Id 
+if ($subIdContext -ne $subscriptionId) {
+    $setSub = Set-AzContext -SubscriptionName $subscriptionId -ErrorAction SilentlyContinue
+    if ($setSub -eq $Null) {
+        Write-Warning "$subscriptionId is not set, please login and run this script again"
+        Login-AzAccount
+        break
+    }
+}
 
 # Create folder if it does not exist
 if (!(Test-Path -Path $ruleExportPath))
 {
-    Write-Host ("Folder " + $ruleExportPath + " does not exist, creating the folder for you....") -ForegroundColor Red
+    Write-Warning ("Folder " + $ruleExportPath + " does not exist, creating the folder for you....")
     New-Item -itemType Directory -Path $ruleExportPath
 }
 
 # Export Scheduled Rules
 try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "Scheduled"} 
+    $rules = Get-AzSentinelAlertRule -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName | Where-Object {$_.Kind -eq "Scheduled"} 
     Write-Host ("Exporting " + $rules.count + " Scheduled rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "Scheduled.json") -Force
+    $rules | ConvertTo-Json -Depth 15 | Out-File ($ruleExportPath + "Scheduled.json") -Force
 }
 catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
+    Write-Warning "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect"
+    Write-Warning $_.Exception.Message -ForegroundColor Red
     break
 }
 
 
 # Export Fusion Rules
 try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "Fusion"} 
+    $rules = Get-AzSentinelAlertRule -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName | Where-Object {$_.Kind -eq "Fusion"} 
     Write-Host ("Exporting " + $rules.count + " Fusion rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "Fusion.json") -Force
+    $rules | ConvertTo-Json -Depth 15 | Out-File ($ruleExportPath + "Fusion.json") -Force
 }
 catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
+    Write-Warning "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect"
+    Write-Warning $_.Exception.Message -ForegroundColor Red
     break
 }
 
 # Export MicrosoftSecurityIncidentCreation Rules
 try {
-    $rules = Get-AzSentinelAlertRule @SentinelConnection | Where-Object {$_.Kind -eq "MicrosoftSecurityIncidentCreation"} 
+    $rules = Get-AzSentinelAlertRule -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName | Where-Object {$_.Kind -eq "MicrosoftSecurityIncidentCreation"} 
     Write-Host ("Exporting " + $rules.count + " MicrosoftSecurityIncidentCreation rules...") -ForegroundColor Yellow
-    $rules | ConvertTo-Json -Depth 15 | Out-File ($myExportPath + "MicrosoftSecurityIncidentCreation.json") -Force
+    $rules | ConvertTo-Json -Depth 15 | Out-File ($ruleExportPath + "MicrosoftSecurityIncidentCreation.json") -Force
 }
 catch {
-    Write-Host "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Red
+    Write-Warning "Either your Azure connection is invalid or your Azure Sentinel settings are incorrect"
+    Write-Warning $_.Exception.Message -ForegroundColor Red
     break
 }
 
