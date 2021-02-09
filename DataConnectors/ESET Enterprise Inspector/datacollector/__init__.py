83fc6f787277f4b870bd76576d35ce56c4ad26ef
v-ampami <v-ampami@microsoft.com>
2021-02-09 16:49:48 +0530

    ESETEnterpriseInspector Gov Changes

--- DataConnectors/ESET Enterprise Inspector/datacollector/__init__.py
+++ DataConnectors/ESET Enterprise Inspector/datacollector/__init__.py
@@ -6,8 +6,16 @@ import datetime
 import hashlib
 import hmac
 import base64
+import os
+import re
 
 from enterpriseinspector.eifunctions import exit_error
+logAnalyticsUri = os.environ['logAnalyticsUri']
+pattern = r"http:\/\/([\w\-]+)\.ods\.opinsights\.azure.([\w\.]+)"
+match = re.match(pattern,str(logAnalyticsUri))
+if(not match):
+    exit_error("ESET Enterprise Inspector: Invalid Log Analytics Uri.")
+    sys.exit()
 
 #####################
 ######Functions######  
@@ -31,8 +39,10 @@ def post_data(customer_id, shared_key, body, log_type):
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + resource + '?api-version=2016-04-01'
 
+    if(not (logAnalyticsUri and not logAnalyticsUri.isspace())):        
+        logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
+    logAnalyticsUri = logAnalyticsUri + resource + '?api-version=2016-04-01'
     headers = {
         'content-type': content_type,
         'Authorization': signature,
@@ -40,7 +50,7 @@ def post_data(customer_id, shared_key, body, log_type):
         'x-ms-date': rfc1123date
     }
 
-    response = requests.post(uri,data=body, headers=headers)
+    response = requests.post(logAnalyticsUri,data=body, headers=headers)
 
     if (response.status_code >= 200 and response.status_code <= 299):
         print('Accepted')
