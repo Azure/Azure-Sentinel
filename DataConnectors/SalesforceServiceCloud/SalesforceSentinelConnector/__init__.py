e4b12fbc24a99c9c0f1d7eb78353c349ba01f87f
v-ampami <v-ampami@microsoft.com>
2021-02-09 19:34:36 +0530

    Salesforce Service Cloud Gov Changes

--- DataConnectors/SalesforceServiceCloud/SalesforceSentinelConnector/__init__.py
+++ DataConnectors/SalesforceServiceCloud/SalesforceSentinelConnector/__init__.py
@@ -25,6 +25,13 @@ interval = "hourly"
 hours_interval = 1
 days_interval = 1
 url = "https://login.salesforce.com/services/oauth2/token"
+logAnalyticsUri = os.environ['logAnalyticsUri']
+
+pattern = r"https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([\w\.]+)"
+match = re.match(pattern,str(logAnalyticsUri))
+if(not match):
+    logging.error("Salesforce Service Cloud: Invalid Log Analytics Uri.")
+    sys.exit()
 
 
 def _get_token():
@@ -175,14 +182,16 @@ def post_data(customer_id, shared_key, body, log_type, chunk_count):
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + resource + '?api-version=2016-04-01'
+    if(not (logAnalyticsUri and not logAnalyticsUri.isspace())):        
+        logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
+    logAnalyticsUri = logAnalyticsUri + resource + '?api-version=2016-04-01'
     headers = {
         'content-type': content_type,
         'Authorization': signature,
         'Log-Type': log_type,
         'x-ms-date': rfc1123date
     }
-    response = requests.post(uri,data=body, headers=headers)
+    response = requests.post(logAnalyticsUri,data=body, headers=headers)
     if (response.status_code >= 200 and response.status_code <= 299):
         print('Accepted')
         logging.info("Chunk was processed({} events)".format(chunk_count))
