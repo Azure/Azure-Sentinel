75d3e99c973e49071335f9a068650cada26598c2
v-maudan <v-maudan@microsoft.com>
2021-02-18 17:00:31 +0530

    fix getByte Empty array error

--- DataConnectors/Okta Single Sign-On/AzureFunctionOktaSSO_V2/AzureFunctionOktaSSO/run.ps1
+++ DataConnectors/Okta Single Sign-On/AzureFunctionOktaSSO_V2/AzureFunctionOktaSSO/run.ps1
@@ -146,7 +146,7 @@ do {
         $resource = "/api/logs"
         $rfc1123date = [DateTime]::UtcNow.ToString("r")
         
-        $body = ([System.Text.Encoding]::UTF8.GetBytes($json))
+        $body = ([System.Text.Encoding]::UTF8.GetBytes("$($json)"))
         $contentLength = $body.Length
         $signature = new-BuildSignature `
             -customerId $customerId `
