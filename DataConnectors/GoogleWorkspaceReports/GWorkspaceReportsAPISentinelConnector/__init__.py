c2a632d65ac1bd0c42db3bac7a1908d198f8875a
v-ampami <v-ampami@microsoft.com>
2021-02-22 20:42:49 +0530

    Update Googleworkspace
    Update Googleworkspace

--- DataConnectors/GoogleWorkspaceReports/GWorkspaceReportsAPISentinelConnector/__init__.py
+++ DataConnectors/GoogleWorkspaceReports/GWorkspaceReportsAPISentinelConnector/__init__.py
@@ -24,11 +24,13 @@ SCOPES = ['https://www.googleapis.com/auth/admin.reports.audit.readonly']
 activities = ["login", "calendar", "drive", "admin", "mobile", "token", "user_accounts"]
 logAnalyticsUri = os.environ['logAnalyticsUri']
 
+if ((logAnalyticsUri in (None, '') or str(logAnalyticsUri).isspace())):    
+    logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
+
 pattern = r"https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([\w\.]+)"
 match = re.match(pattern,str(logAnalyticsUri))
 if(not match):
-    logging.error("Google Workspace Reports: Invalid Log Analytics Uri.")
-    sys.exit()
+    raise Exception("Google Workspace Reports: Invalid Log Analytics Uri.")
 
 def get_credentials():
     creds = None
@@ -84,8 +86,6 @@ def post_data(customer_id, shared_key, body, log_type):
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    if(not (logAnalyticsUri and not logAnalyticsUri.isspace())):        
-        logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
     logAnalyticsUri = logAnalyticsUri + resource + '?api-version=2016-04-01'
     headers = {
         'content-type': content_type,
