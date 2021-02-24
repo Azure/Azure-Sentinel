fd8e4db17169ad911c46da96965c37e774510899
v-rucdu <v-rucdu@microsoft.com>
2021-02-24 17:15:35 +0530

    Updated US Gov Code

--- DataConnectors/SalesforceServiceCloud/SalesforceSentinelConnector/__init__.py
+++ DataConnectors/SalesforceServiceCloud/SalesforceSentinelConnector/__init__.py
@@ -28,11 +28,13 @@ days_interval = 1
 url = "https://login.salesforce.com/services/oauth2/token"
 logAnalyticsUri = os.environ['logAnalyticsUri']
 
-pattern = r"https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([\w\.]+)"
+if ((logAnalyticsUri in (None, '') or str(logAnalyticsUri).isspace())):    
+    logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
+
+pattern = r'https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([a-zA-Z\.]+)$'
 match = re.match(pattern,str(logAnalyticsUri))
 if(not match):
-    logging.error("Salesforce Service Cloud: Invalid Log Analytics Uri.")
-    sys.exit()
+    raise Exception("Salesforce Service Cloud: Invalid Log Analytics Uri.")
 
 
 def _get_token():
@@ -183,9 +185,8 @@ def post_data(customer_id, shared_key, body, log_type, chunk_count):
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    if(not (logAnalyticsUri and not logAnalyticsUri.isspace())):        
-        logAnalyticsUri = 'https://' + customerId + '.ods.opinsights.azure.com'
     logAnalyticsUri = logAnalyticsUri + resource + '?api-version=2016-04-01'
+    
     headers = {
         'content-type': content_type,
         'Authorization': signature,
