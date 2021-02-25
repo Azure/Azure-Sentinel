6dbe4751654245f672f04309f2815699ebc2e328
v-rucdu <v-rucdu@microsoft.com>
2021-02-25 19:28:42 +0530

    Moved Logic to QualysKB method

--- DataConnectors/Qualys KB/AzureFunctionQualysKB/run.ps1
+++ DataConnectors/Qualys KB/AzureFunctionQualysKB/run.ps1
@@ -20,18 +20,6 @@ $currentUTCtime = (Get-Date).ToUniversalTime()
 
 $logAnalyticsUri = $env:logAnalyticsUri
 
-if ([string]::IsNullOrEmpty($logAnalyticsUri))
-{
-    $logAnalyticsUri = "https://" + $customerId + ".ods.opinsights.azure.com"
-}
-
-# Returning if the Log Analytics Uri is in incorrect format.
-# Sample format supported: https://" + $customerId + ".ods.opinsights.azure.com
-if($logAnalyticsUri -notmatch 'https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([a-zA-Z\.]+)$')
-{
-    throw "Qualys KB: Invalid Log Analytics Uri."
-}
-
 # The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
 if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
@@ -132,6 +120,18 @@ function QualysKB {
     $timeInterval = 5
     $filterparameters = $env:filterParameters
     $Uri = $env:Uri
+
+    if ([string]::IsNullOrEmpty($logAnalyticsUri))
+    {
+        $logAnalyticsUri = "https://" + $customerId + ".ods.opinsights.azure.com"
+    }
+    
+    # Returning if the Log Analytics Uri is in incorrect format.
+    # Sample format supported: https://" + $customerId + ".ods.opinsights.azure.com
+    if($logAnalyticsUri -notmatch 'https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([a-zA-Z\.]+)$')
+    {
+        throw "Qualys KB: Invalid Log Analytics Uri."
+    }
 
     # Validate Uri
     $UriValidation = UrlValidation -Uri $Uri
