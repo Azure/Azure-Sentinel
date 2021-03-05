12ec6e4ca6c1235ab6b59f86c690f192b353143e
v-maudan <v-maudan@microsoft.com>
2021-03-05 11:21:43 +0530

    moved gov changes to process detections

--- DataConnectors/ESET Enterprise Inspector/datacollector/__init__.py
+++ DataConnectors/ESET Enterprise Inspector/datacollector/__init__.py
@@ -10,7 +10,6 @@ import os
 import re
 
 from enterpriseinspector.eifunctions import exit_error
-logAnalyticsUri = os.environ.get('logAnalyticsUri')
 
 #####################
 ######Functions######  
@@ -27,25 +26,15 @@ def build_signature(customer_id, shared_key, date, content_length, method, conte
     return authorization
 
 # Build and send a request to the POST API
-def post_data(customer_id, shared_key, body, log_type):
-    global logAnalyticsUri
+def post_data(customer_id, shared_key, body, log_type, logAnalyticsUri):
     method = 'POST'
     content_type = 'application/json'
     resource = '/api/logs'
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-
-    if ((logAnalyticsUri in (None, '') or str(logAnalyticsUri).isspace())):    
-        logAnalyticsUri = 'https://' + customer_id + '.ods.opinsights.azure.com'
-
-    pattern = r'https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([a-zA-Z\.]+)$'
-    match = re.match(pattern,str(logAnalyticsUri))
-    
-    if(not match):
-        raise Exception("ESET Enterprise Inspector: Invalid Log Analytics Uri.")
-    
-    logAnalyticsUri = logAnalyticsUri + resource + '?api-version=2016-04-01'
+   
+    uri = logAnalyticsUri + resource + '?api-version=2016-04-01'
     
     headers = {
         'content-type': content_type,
@@ -54,7 +43,7 @@ def post_data(customer_id, shared_key, body, log_type):
         'x-ms-date': rfc1123date
     }
 
-    response = requests.post(logAnalyticsUri,data=body, headers=headers)
+    response = requests.post(uri, data=body, headers=headers)
 
     if (response.status_code >= 200 and response.status_code <= 299):
         print('Accepted')
