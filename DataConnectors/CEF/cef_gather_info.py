210b2530f0536ddb7651c35d7d262eca1ad2de4a
Noam Landress <noamlandress@microsoft.com>
2021-03-16 11:34:30 +0200

    First CR changes

--- DataConnectors/CEF/cef_gather_info.py
+++ DataConnectors/CEF/cef_gather_info.py
@@ -21,7 +21,6 @@
 # For more information please check the OMS-Agent-for-Linux documentation.
 #
 
-import time
 import subprocess
 
 
@@ -33,42 +32,39 @@ class SystemInfo:
     def __repr__(self):
         delimiter = "\n-----------------------------------\n"
         return str(
-            delimiter + "command: " + self.command + '\n' + "output: " + self.command_output + delimiter)
+            delimiter + "command: " + self.command + '\n' + "output: " + self.command_output + delimiter).replace('%',
+                                                                                                                  '%%')
 
 
 script_version = '1.0'
 output_file_path = '/tmp/cef_get_info'
 
-basic_command_dict = {
-    "script_version": "echo {}".format(script_version),
-    "date": "sudo date",
-    "netstat": "sudo netstat -lnpvt",
-    "df": "sudo df -h",
-    "free": "sudo free -m",
-    "iptables": "sudo iptables -vnL --line",
-    "selinux": "sudo cat /etc/selinux/config",
-    "os_version": "sudo cat /etc/issue",
-    "python_version": "sudo python -V",
-    "ram_stats": "sudo cat /proc/meminfo",
-    "cron_jobs": "sudo crontab -l",
-    "wd_list": "sudo ls -l .",
-    "internet_connection": "sudo curl -D - http://google.com",
-    "sudoers_list": "sudo cat /etc/sudoers",
-    "rotation_configuration": "sudo cat /etc/logrotate.conf",
-    "rsyslog_conf": "sudo cat /etc/rsyslog.conf",
-    "rsyslog_dir": "sudo ls -l /etc/rsyslog.d/",
-    "rsyslog_dir_content": "sudo find /etc/rsyslog.d/ -type f -exec cat {} \;",
-    "syslog_ng_conf": "sudo cat /etc/syslog-ng/syslog-ng.conf",
-    "syslog_ng_dir": "sudo ls -l /etc/syslog-ng/conf.d/",
-    "syslog_ng_dir_content": "find /etc/syslog-ng/conf.d/ -type f -exec cat {} \;",
-    "agent_log_snip": "sudo tail -15 /var/opt/microsoft/omsagent/log/omsagent.log",
-    "agent_config_dir": "sudo ls -lR /etc/opt/microsoft/omsagent/",
-    "agent_cef_config": "sudo cat /etc/opt/microsoft/omsagent/conf/omsagent.d/security_events.conf",
-    "tcpdump": "sudo timeout 2 tcpdump -A -ni any port 25226 -vv"
-}
-
-# command with a pipe are considered special commands
-advanced_command_dict = {
+command_dict = {
+    "script_version": ["echo {}".format(script_version)],
+    "date": ["sudo date"],
+    "netstat": ["sudo netstat -lnpvt"],
+    "df": ["sudo df -h"],
+    "free": ["sudo free -m"],
+    "iptables": ["sudo iptables -vnL --line"],
+    "selinux": ["sudo cat /etc/selinux/config"],
+    "os_version": ["sudo cat /etc/issue"],
+    "python_version": ["sudo python -V"],
+    "ram_stats": ["sudo cat /proc/meminfo"],
+    "cron_jobs": ["sudo crontab -l"],
+    "wd_list": ["sudo ls -l ."],
+    "internet_connection": ["sudo curl -D - http://google.com"],
+    "sudoers_list": ["sudo cat /etc/sudoers"],
+    "rotation_configuration": ["sudo cat /etc/logrotate.conf"],
+    "rsyslog_conf": ["sudo cat /etc/rsyslog.conf"],
+    "rsyslog_dir": ["sudo ls -l /etc/rsyslog.d/"],
+    "rsyslog_dir_content": ["sudo find /etc/rsyslog.d/ -type f -exec cat {} \;"],
+    "syslog_ng_conf": ["sudo cat /etc/syslog-ng/syslog-ng.conf"],
+    "syslog_ng_dir": ["sudo ls -l /etc/syslog-ng/conf.d/"],
+    "syslog_ng_dir_content": ["find /etc/syslog-ng/conf.d/ -type f -exec cat {} \;"],
+    "agent_log_snip": ["sudo tail -15 /var/opt/microsoft/omsagent/log/omsagent.log"],
+    "agent_config_dir": ["sudo ls -lR /etc/opt/microsoft/omsagent/"],
+    "agent_cef_config": ["sudo cat /etc/opt/microsoft/omsagent/conf/omsagent.d/security_events.conf"],
+    "tcpdump": ["sudo timeout 2 tcpdump -A -ni any port 25226 -vv"],
     "top_processes": ["sudo top -bcn1 -w512", "head -n 20"],
     "omsagent_process": ["sudo ps -aux", "grep omsagent"]
 }
@@ -77,7 +73,7 @@ advanced_command_dict = {
 def print_notice(input_str):
     '''
     Print given text in white background
-    :param input_str:
+    :param input_str: the string whose format will be changed.
     '''
     print("\033[0;30;47m" + input_str + "\033[0m")
 
@@ -88,13 +84,13 @@ def append_content_to_file(command_object, file_path=output_file_path):
     :param command_object: consists of the name and the output
     :param file_path: a file to share the commands outputs
     """
-    output = repr(command_object).replace('%', '%%')
-    command_tokens = ["sudo", "bash", "-c", "printf '" + "\n" + output + "' >> " + file_path]
+    output = repr(command_object)
+    cef_get_info_file = open(file_path, 'a')
     try:
-        write_new_content = subprocess.Popen(command_tokens, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        write_new_content.communicate()
+        cef_get_info_file.write(output)
     except Exception:
         print(str(command_object.command) + "was not documented successfully")
+    cef_get_info_file.close()
 
 
 def run_command(command):
@@ -103,31 +99,19 @@ def run_command(command):
     :param command: the key value pair from the command dict
     :return: a command object consisting of a name and an output
     """
-    command_to_run = subprocess.Popen(basic_command_dict[command].split(' '), stdout=subprocess.PIPE,
-                                      stderr=subprocess.STDOUT)
+    if len(command_dict[command]) == 1:
+        command_to_run = subprocess.Popen(command_dict[command][0].split(' '), stdout=subprocess.PIPE,
+                                          stderr=subprocess.STDOUT)
+    # for commands that consist of a single pipe
+    else:
+        first_command = subprocess.Popen(command_dict[command][0].split(' '), stdout=subprocess.PIPE,
+                                         stderr=subprocess.STDOUT)
+        command_to_run = subprocess.Popen(command_dict[command][1].split(' '), stdin=first_command.stdout,
+                                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     try:
         o, e = command_to_run.communicate()
     except Exception:
-        print(basic_command_dict[command] + "failed to run")
-    o = o.decode(encoding='UTF-8')
-    command_object = SystemInfo(command, o)
-    return command_object
-
-
-def run_special_command(command):
-    """
-
-    :param command: the key value pair from the special command dict
-    :return: a command object consisting of a name and an output
-    """
-    first_command = subprocess.Popen(advanced_command_dict[command][0].split(' '), stdout=subprocess.PIPE,
-                                     stderr=subprocess.STDOUT)
-    second_command = subprocess.Popen(advanced_command_dict[command][1].split(' '), stdin=first_command.stdout,
-                                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    try:
-        o, e = second_command.communicate()
-    except Exception:
-        print(command + "command failed to run")
+        pass
     o = o.decode(encoding='UTF-8')
     command_object = SystemInfo(command, o)
     return command_object
@@ -147,19 +131,14 @@ def clean_up(path=output_file_path):
 
 def main():
     print_notice("Note this script should be run in elevated privileges")
-    print("Beginning to collect server data- This may take a few minutes")
+    print("Beginning to collect server data- This may take a moment")
     clean_up()
-    for command in basic_command_dict.keys():
+    for command in command_dict.keys():
         command_object = run_command(command)
         append_content_to_file(command_object)
-        time.sleep(1)
-
-    for command in advanced_command_dict.keys():
-        command_object = run_special_command(command)
-        append_content_to_file(command_object)
-        time.sleep(1)
     print_notice(
-        "Data collection complete. Please provide CSS with the content of the file {}".format(output_file_path))
+        "Data collection completed successfully. Please provide CSS with the content of the file {}".format(
+            output_file_path))
 
 
 if __name__ == '__main__':
