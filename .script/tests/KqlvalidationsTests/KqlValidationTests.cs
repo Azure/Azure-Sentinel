79a5117b1ad7d0baefee472c10c3e46c3d10e7ab
Amit Bergman <amitbergman@gmail.com>
2021-04-12 09:35:13 +0300

    remove unneeded code

--- .script/tests/KqlvalidationsTests/KqlValidationTests.cs
+++ .script/tests/KqlvalidationsTests/KqlValidationTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Azure.Sentinel.KustoServices.Contract;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
--- .script/tests/KqlvalidationsTests/TemplatesToSkipValidationReader.cs
+++ .script/tests/KqlvalidationsTests/TemplatesToSkipValidationReader.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace Kqlvalidations.Tests
 {
