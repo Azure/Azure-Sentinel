55f6d0c6bf7953e4ef4f99c4639151d6f93f2faa
Itai Yankelevsky <ityankel@microsoft.com>
2021-05-23 12:38:05 +0300

    Add UT

---
+++ .script/tests/playbooksValidatorTest/playbooksValidator.test.ts
@@ -0,0 +1,25 @@
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { ExitCode } from "../../utils/exitCode";
+import { IsValidTemplate } from "../../playbooksValidator";
+
+chai.use(chaiAsPromised);
+
+describe("Playbooks validator", () => {
+  it(`Should pass when playbook template is valid`, async () => {
+    await checkValid(".script/tests/playbooksValidatorTest/testFiles/validPlaybookTemplate.json");
+  });
+
+  it(`Should throw an exception when template is missing 'PlaybookName' parameter`, async () => {
+    await checkInvalid(".script/tests/workbooksValidatorTest/testFiles/workbooksTemplateFiles/invalidValueForFromTemplateId.json");
+  });
+
+  async function checkInvalid(filePath: string): Promise<Chai.PromisedAssertion> {
+    await expect(IsValidTemplate(filePath)).eventually.rejectedWith(Error).and.have.property("name", "PlaybookValidationError");
+  }
+
+  async function checkValid(filePath: string): Promise<Chai.PromisedAssertion> {
+    let result = await IsValidTemplate(filePath);
+    expect(result).to.equal(ExitCode.SUCCESS);
+  }
+});
