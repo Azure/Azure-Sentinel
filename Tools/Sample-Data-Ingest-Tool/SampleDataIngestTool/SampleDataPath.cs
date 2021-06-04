2875e51a4b700032a6b6b13837220fbeeaf57cfe
v-rucdu <v-rucdu@microsoft.com>
2021-06-04 17:17:13 +0530

    Handled validation for parsers under solutions folder

--- Tools/Sample-Data-Ingest-Tool/SampleDataIngestTool/SampleDataPath.cs
+++ Tools/Sample-Data-Ingest-Tool/SampleDataIngestTool/SampleDataPath.cs
@@ -4,7 +4,7 @@ namespace SampleDataIngestTool
 {
     public class SampleDataPath
     {
-        static readonly string subDirPath = "\\Sample Data\\Custom\\";
+        static readonly string subDirPath = "C:\\Custom\\";
         public SampleDataPath()
         {
 
@@ -18,7 +18,7 @@ namespace SampleDataIngestTool
                 var basePath = currentDirectory.Split(new string[] { "\\Tools" }, StringSplitOptions.None)[0];
                 var dirPath = basePath + subDirPath;
 
-                return dirPath;
+                return subDirPath;
             }
             catch (Exception ex)
             {
