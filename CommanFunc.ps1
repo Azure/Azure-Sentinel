151894ad4ded24d19fb19408c3bff4b021e67628
v-maudan <v-maudan@microsoft.com>
2021-06-17 09:48:06 +0530

    added Telemetry file

---
+++ CommanFunc.ps1
@@ -0,0 +1,21 @@
+﻿#common function to verify azure function
+
+param
+(
+  [string]$uri,
+  [System.Collections.Generic.Dictionary[[String],[String]]]$headers,
+  [string]$body
+)
+
+#$response = Invoke-RestMethod -Headers $authHeaders -Uri $uri
+Write-Host("URI : $uri")
+Write-Host("header :$($headers)")
+Write-Host("$body")
+
+$response = Invoke-WebRequest -Uri $uri -Method 'GET' -Headers $headers -Body $body -UseBasicParsing
+
+$responseObj = (ConvertFrom-Json $response.Content)
+$responseCount = $responseObj.count
+
+Write-Host("API Response : $($responseObj)")
+Write-Host("API returned :$($responseCount) records")
