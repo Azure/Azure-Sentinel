b89d04bf47144202d24c7a603a66dafde451c7d7
v-maudan <75478517+v-maudan@users.noreply.github.com>
2021-06-21 10:49:36 +0530

    Updated port value as integer

--- DataConnectors/GoogleWorkspaceReports/Generate_Google_Pickle_String.py
+++ DataConnectors/GoogleWorkspaceReports/Generate_Google_Pickle_String.py
@@ -24,7 +24,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port='8081')
+            creds = flow.run_local_server(port=8081)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)               
