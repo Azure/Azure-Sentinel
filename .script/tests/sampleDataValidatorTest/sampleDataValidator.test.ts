03f5f6213c84c45bf21213b9288b86b919a5bf82
NikTripathi <v-ntripathi@microsoft.com>
2021-08-10 11:52:13 +0530

    Added more test in sampleDataValidator

--- .script/tests/sampleDataValidatorTest/sampleDataValidator.test.ts
+++ .script/tests/sampleDataValidatorTest/sampleDataValidator.test.ts
@@ -5,12 +5,18 @@ import { ExitCode } from "../../utils/exitCode";
 
 chai.use(chaiAsPromised);
 
-describe("dataConnectorValidator", () => {
+describe("sampleDataValidator", () => {
     it("should pass when sampleDataWithArray.json is valid", async () => {
         await checkValid(".script/tests/sampleDataValidatorTest/testFiles/sampleDataWithArray.json");
       });
     it("should throw an exception when sampleDataWithStartWithBracket.json is missing a required property", async () => {
         await checkInvalid(".script/tests/sampleDataValidatorTest/testFiles/sampleDataWithStartWithBracket.json", "SchemaError");
+      }); 
+      it("should throw an exception when sampleDataWithStartWithBracket.json is missing a required property", async () => {
+        await checkInvalid(".script/tests/sampleDataValidatorTest/testFiles/sampleDataWithInvalidEmail.json", "SchemaError");
+      }); 
+      it("should throw an exception when sampleDataWithStartWithBracket.json is missing a required property", async () => {
+        await checkInvalid(".script/tests/sampleDataValidatorTest/testFiles/sampleDataWithValidEmail.json", "SchemaError");
       }); 
 
   async function checkValid(filePath: string): Promise<Chai.PromisedAssertion> {
