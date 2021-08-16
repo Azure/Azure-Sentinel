f07303dec2831e59aa6a053a7e2d122f8a1e1ecb
Amit Bergman <38046493+Amitbergman@users.noreply.github.com>
2021-08-16 09:55:16 +0300

    Update checkThatTemplatesVersionWasChanged.sh

--- .script/checkThatTemplatesVersionWasChanged.sh
+++ .script/checkThatTemplatesVersionWasChanged.sh
@@ -2,11 +2,11 @@
 
 var=$(echo $(git diff origin/master -U0))
 
-if [[ "$var" == *"version"* ]];
+if [[ "$var" == *"version:"* ]];
 then
     echo "Changed the version of the template, nice!"
     exit 0
 else
-    echo "Hey there, you did not change the version of the template, please update the version of the template you changed."
+    echo "There are one or more templates that were modified and their version was not updated. Please update the version of the template(s) you changed."
     exit 1
 fi
