03727c647501b3dbd0559a99b93a2095cbbe97af
Amit Bergman <38046493+Amitbergman@users.noreply.github.com>
2021-08-16 13:08:05 +0300

    Update checkThatTemplatesVersionWasChanged.sh

--- .script/checkThatTemplatesVersionWasChanged.sh
+++ .script/checkThatTemplatesVersionWasChanged.sh
@@ -4,7 +4,7 @@ filesThatWereChanged=$(echo $(git diff origin/master --name-only))
 for file in $filesThatWereChanged
 do
     echo processing the file $file that was changed
-	if [[ "$file" == *"detection"* ]];
+	if [[ "$file" == *"Detection"* ]];
 	then
 		echo $file is a detection
 		diffs=$(echo $(git diff origin/master -U0 $file))
