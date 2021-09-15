e14b15ecbaf702f38660730b52d007197b6eddfa
NikTripathi <v-ntripathi@microsoft.com>
2021-09-15 18:21:04 +0530

    Holm Security Test purpose.

---
+++ Solutions/HOLM/AzureFunctionHolmSecurityAssets/.python_packages/lib/site-packages/azure/functions/extension/function_extension_exception.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+class FunctionExtensionException(Exception):
+    """Excpetion emitted from Azure Functions Python Worker extension
+    """
+    pass
