36bc4ab9e85b7fb1310ba16e2a0e63490be1a7a1
YoavDaniely <63583192+YoavDaniely@users.noreply.github.com>
2021-11-11 11:10:10 +0200

    Update sapcon-instance-update.sh

--- Solutions/SAP/sapcon-instance-update.sh
+++ Solutions/SAP/sapcon-instance-update.sh
@@ -3,8 +3,8 @@
 echo 'Microsoft Azure Sentinel SAP Continuous Threat Monitoring.
 SAP ABAP Logs Connector -  Preview
 
-Copyright (c) Microsoft Corporation. 
-You may use this preview software internally and only in accordance with the Azure preview terms, located at https://azure.microsoft.com/support/legal/preview-supplemental-terms/  
+Copyright (c) Microsoft Corporation.
+You may use this preview software internally and only in accordance with the Azure preview terms, located at https://azure.microsoft.com/support/legal/preview-supplemental-terms/
 
 Microsoft reserves all other rights
 ****'
@@ -21,19 +21,20 @@ containername=sapcon
 sysconf=systemconfig.ini
 acr=sentinel4sapprivateprview.azurecr.io
 sdkfileloc=/sapcon-app/inst/
+grepvars="ABAP\|JAVA\|LOG_WS\|AZURE_"
 
 echo '
 -----Update All MS SAPcon instances----
 This process will download the latest version of Sentinel SAP Connector, Updates current image and containers. A currently running version of the instance will be stopped and automatically start after the process.
-In order to process you will need the following prerequisites: 
+In order to process you will need the following prerequisites:
 '
 
 echo 'Starting Docker image Pull'
 docker pull $dockerimage$tagver
 if [ $? -eq 1 ];
-then 
-	echo 'There is an error with the docker image - please Check network connection'
-	exit 1
+then
+        echo 'There is an error with the docker image - please Check network connection'
+        exit 1
 fi
 pause '
 Image has been downloaded - Press <Enter> key to continue with the Update'
@@ -41,54 +42,62 @@ Image has been downloaded - Press <Enter> key to continue with the Update'
 contlist=$(docker container ls -a | awk 'NR!=1 {print $1}')
 
 while IFS= read -r contid
-do	
-	contimg=$(docker inspect --format='{{.Config.Image}}' $contid)
-	contname=$(docker ps -a --filter id=$contid --format '{{.Names}}')
-    
-	if [[ "$contimg" = *"$dockerimage"* ]] || [[ "$contimg" = *"$olddockerimage"* ]]
-	then
-		echo ' '		
-		echo Updating $contname...
-		if [  ! -z $contid ]
-		then
-			sysfileloc=$(docker inspect -f '{{ .Mounts }}' $contname | awk 'NR==1 {print $2}')
-			if [  ! -z $sysfileloc ]
-			then
-				last=${sysfileloc: -1}
+do
+        contimg=$(docker inspect --format='{{.Config.Image}}' $contid)
+        contname=$(docker ps -a --filter id=$contid --format '{{.Names}}')
 
-				if [ "$last" != "/" ];
-				then
-					sysfileloc="$sysfileloc/"
-				fi
-				
-				contstate=$(docker inspect --format='{{.State.Running}}' $contname )
-				
-				# pause 'press enter'
-				if [ $contstate == "false" ]
-				then
-						docker cp $contname:$sdkfileloc $(pwd)
-						docker container rm $contname >/dev/null
-						docker create -v $sysfileloc:/sapcon-app/sapcon/config/system --name $contname $dockerimage$tagver >/dev/null
-						docker cp "$(pwd)/inst/" $contname:/sapcon-app/ >/dev/null
-						echo 'Container "'"$contname"'" was updated - please start the app by running "docker start '"$contname"'"'
-						echo ''
-				else
-					docker cp $contname:$sdkfileloc $(pwd)
-					docker stop $contname  >/dev/null
-					docker container rm $contname  >/dev/null
-					docker create -v $sysfileloc:/sapcon-app/sapcon/config/system --name $contname $dockerimage$tagver  >/dev/null
-					docker cp "$(pwd)/inst/" $contname:/sapcon-app/  >/dev/null
-					docker start $contname >/dev/null
-					echo 'Container "'"$contname"'" was updated'
-					echo ''
-				fi
-			else
-				echo 'Container "'"$contname"'" cannot be updated - The mount point is empty'
-				echo ' '
-			fi
-		else
-			echo ''
-		fi
-		
-	fi 	 
+        if [[ "$contimg" = *"$dockerimage"* ]] || [[ "$contimg" = *"$olddockerimage"* ]]
+        then
+                echo ' '
+                echo Updating $contname...
+                # getting env vars to env.file.current
+                rm ./env.file.current > /dev/null 2>&1
+                docker inspect -f "{{ .Config.Env }}" $contname | tail -c +2 | head -c -2| sed 's/\s\+/\n/g' | grep $grepvars> env.file.current
+                if [  ! -z $contid ]
+                then
+                        sysfileloc=$(docker inspect -f '{{ .Mounts }}' $contname | awk 'NR==1 {print $2}')
+                        if [  ! -z $sysfileloc ]
+                        then
+                                last=${sysfileloc: -1}
+
+                                if [ "$last" != "/" ];
+                                then
+                                        sysfileloc="$sysfileloc/"
+                                fi
+
+                                contstate=$(docker inspect --format='{{.State.Running}}' $contname )
+
+                                # pause 'press enter'
+                                if [ $contstate == "false" ]
+                                then
+                                                docker cp $contname:$sdkfileloc $(pwd)
+                                                docker container rm $contname >/dev/null
+                                                docker create -v --env-file=./env.file.current $sysfileloc:/sapcon-app/sapcon/config/system --name $contname $dockerimage$tagver >/dev/null
+						#removing env file
+                                                rm ./env.file.current > /dev/null 2>&1
+                                                docker cp "$(pwd)/inst/" $contname:/sapcon-app/ >/dev/null
+                                                echo 'Container "'"$contname"'" was updated - please start the app by running "docker start '"$contname"'"'
+                                                echo ''
+                                else
+                                        docker cp $contname:$sdkfileloc $(pwd)
+                                        docker stop $contname  >/dev/null
+                                        docker container rm $contname  >/dev/null
+                                        docker create --env-file=./env.file.current -v $sysfileloc:/sapcon-app/sapcon/config/system --name $contname $dockerimage$tagver  >/dev/null
+                                        #removing env file
+					rm ./env.file.current > /dev/null 2>&1
+
+                                        docker cp "$(pwd)/inst/" $contname:/sapcon-app/  >/dev/null
+                                        docker start $contname >/dev/null
+                                        echo 'Container "'"$contname"'" was updated'
+                                        echo ''
+                                fi
+                        else
+                                echo 'Container "'"$contname"'" cannot be updated - The mount point is empty'
+                                echo ' '
+                        fi
+                else
+                        echo ''
+                fi
+
+        fi
 done <<< "$contlist"
