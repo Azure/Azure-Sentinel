97d1ed31acb25aaabcc9a13d49c1271ba366e72d
Pete Bryan <pebryan@microsoft.com>
2021-11-17 21:56:44 -0800

    Adding AWSGuardDuty Table Schema and removing debugging

--- .script/tests/detectionTemplateSchemaValidation/DetectionTemplateSchemaValidationTests.cs
+++ .script/tests/detectionTemplateSchemaValidation/DetectionTemplateSchemaValidationTests.cs
@@ -116,16 +116,7 @@ namespace Kqlvalidations.Tests
             var AllFiles = Directory.GetFiles(detectionPath[0],"*", SearchOption.AllDirectories).ToList();
             AllFiles.AddRange(Directory.GetFiles(detectionPath[1], "*", SearchOption.AllDirectories).ToList().Where(s => s.Contains("Analytic Rules")));
             var numberOfNotYamlFiles = 1; //This is the readme.md file in the directory
-            foreach(var fil in AllFiles)
-            {
-                Console.WriteLine(fil);
-            }
-            Console.WriteLine("----------------------------");
-            foreach(var yfil in yamlFiles)
-            {
-                Console.WriteLine(yfil);
-            }
-            Assert.True(AllFiles.Count == yamlFiles.Count + numberOfNotYamlFiles,  $"All the files in detections and solution (Analytics rules) folder are supposed to end with .yaml {numberOfNotYamlFiles} {AllFiles.Count} {yamlFiles.Count}");
+            Assert.True(AllFiles.Count == yamlFiles.Count + numberOfNotYamlFiles,  $"All the files in detections and solution (Analytics rules) folder are supposed to end with .yaml");
         }
 
         [Fact]
