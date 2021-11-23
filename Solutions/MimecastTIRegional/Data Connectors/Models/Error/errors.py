4dacc8c49ee7196caad08757c34cb8e721470389
Lazar Stajcic <lstajcic@mimecast.com>
2021-11-23 13:59:57 +0100

    Add Mimecast TI Regional Data connector

---
+++ Solutions/MimecastTIRegional/Data Connectors/Models/Error/errors.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class BaseError(Exception):
+    request_id = None
+
+    def __init__(self, message, request_id: Optional):
+        if request_id:
+            self.request_id = request_id
+        super(BaseError, self).__init__(message)
+
+
+class MimecastRequestError(BaseError):
+    pass
+
+
+class ParsingError(MimecastRequestError):
+    pass
+
+
+class InvalidDataError(MimecastRequestError):
+    pass
+
+
+class GraphAPIRequestError(BaseError):
+    pass
