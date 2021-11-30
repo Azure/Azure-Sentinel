9dc6169dd004874c0750b923e347bbcefb58980c
ShaniFelig <t-shfeli@microsoft.com>
2021-11-30 14:43:14 +0200

    StringEnumConverterAttribute on AggregationKind

--- .script/tests/detectionTemplateSchemaValidation/Models/EventGroupingSettings.cs
+++ .script/tests/detectionTemplateSchemaValidation/Models/EventGroupingSettings.cs
@@ -1,10 +1,12 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Microsoft.Azure.Sentinel.Analytics.Management.AnalyticsTemplatesService.Interface.Model
 {
     public class EventGroupingSettings
     {
         [JsonProperty("aggregationKind", Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))] //test to see if this works and is enough. Will probably not catch cases that enter an int as the aggregationKind. Are we ok with that? Will it catch an int that is out of the bounds of the enum such as 9 when there are only 2 values in the enum?
         public EventGroupingAggregationKind AggregationKind { get; set; }
     }
 
