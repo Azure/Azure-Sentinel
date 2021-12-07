476f54a45b5adb5b8cbc39269d1b7d07ede39a1c
Sreedhar Ande <22670063+sreedharande@users.noreply.github.com>
2021-12-07 15:25:17 -0800

    Automation Script

---
+++ Tools/Microsoft-Sentinel-Archived-Logs/Configure-Long-Term-Retention.ps1
@@ -0,0 +1,389 @@
+<#       
+  	THE SCRIPT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SCRIPT OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+    .SYNOPSIS
+        This PowerShell script configures Basic or Analytics. 
+        
+
+    .DESCRIPTION
+        It performs the following actions:
+            1. Check if table has any configuration (Basic\Analytics)
+            2. Update Table configuration either to Analytics
+            3. Update Table Retention based to Analytics
+           
+    
+    .PARAMETER TenantId
+        Enter Azure Tenant Id (required)  
+
+    .NOTES
+        AUTHOR: Sreedhar Ande
+        LASTEDIT: 12/6/2021
+
+    .EXAMPLE
+        .\Configure-Long-Term-Retention.ps1 -TenantId xxxx `       
+#>
+
+#region UserInputs
+
+param(
+    [parameter(Mandatory = $true, HelpMessage = "Enter your Tenant Id")]
+    [string] $TenantID    
+) 
+
+#endregion UserInputs
+      
+
+
+#region HelperFunctions
+
+function Write-Log {
+    <#
+    .DESCRIPTION 
+    Write-Log is used to write information to a log file and to the console.
+    
+    .PARAMETER Severity
+    parameter specifies the severity of the log message. Values can be: Information, Warning, or Error. 
+    #>
+
+    [CmdletBinding()]
+    param(
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+        [string]$LogFileName,
+ 
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Information', 'Warning', 'Error')]
+        [string]$Severity = 'Information'
+    )
+    # Write the message out to the correct channel											  
+    switch ($Severity) {
+        "Information" { Write-Host $Message -ForegroundColor Green }
+        "Warning" { Write-Host $Message -ForegroundColor Yellow }
+        "Error" { Write-Host $Message -ForegroundColor Red }
+    } 											  
+    try {
+        [PSCustomObject]@{
+            Time     = (Get-Date -f g)
+            Message  = $Message
+            Severity = $Severity
+        } | Export-Csv -Path "$PSScriptRoot\$LogFileName" -Append -NoTypeInformation -Force
+    }
+    catch {
+        Write-Error "An error occurred in Write-Log() method" -ErrorAction SilentlyContinue		
+    }    
+}
+
+function Get-RequiredModules {
+    <#
+    .DESCRIPTION 
+    Get-Required is used to install and then import a specified PowerShell module.
+    
+    .PARAMETER Module
+    parameter specifices the PowerShell module to install. 
+    #>
+
+    [CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $Module        
+    )
+    
+    try {
+        $installedModule = Get-InstalledModule -Name $Module -ErrorAction SilentlyContinue
+        if ($null -eq $installedModule) {
+            Write-Log -Message "The $Module PowerShell module was not found" -LogFileName $LogFileName -Severity Warning
+            #check for Admin Privleges
+            $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+            if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+                #Not an Admin, install to current user            
+                Write-Log -Message "Can not install the $Module module. You are not running as Administrator" -LogFileName $LogFileName -Severity Warning
+                Write-Log -Message "Installing $Module module to current user Scope" -LogFileName $LogFileName -Severity Warning
+                
+                Install-Module -Name $Module -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+                Import-Module -Name $Module -Force
+            }
+            else {
+                #Admin, install to all users																		   
+                Write-Log -Message "Installing the $Module module to all users" -LogFileName $LogFileName -Severity Warning
+                Install-Module -Name $Module -Repository PSGallery -Force -AllowClobber
+                Import-Module -Name $Module -Force
+            }
+        }
+        # Install-Module will obtain the module from the gallery and install it on your local machine, making it available for use.
+        # Import-Module will bring the module and its functions into your current powershell session, if the module is installed.  
+    }
+    catch {
+        Write-Log -Message "An error occurred in Get-RequiredModules() method" -LogFileName $LogFileName -Severity Error																			
+        exit
+    }
+}
+
+#endregion
+
+#region MainFunctions
+
+function Configure-AzureMonitorTables {
+	[CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $RetentionMethod                
+    )
+	
+	$TablesArray = New-Object System.Collections.Generic.List[System.Object]
+	
+	try {       
+        Write-Log -Message "Retrieving tables from $LogAnalyticsWorkspaceName" -LogFileName $LogFileName -Severity Information
+        $QueryAllTables = 'search *| distinct $table| sort by $table asc nulls last'
+        $ResultsAllTables = (Invoke-AzOperationalInsightsQuery -WorkspaceId $LogAnalyticsWorkspaceId -Query $QueryAllTables).Results | Out-GridView -Title "Select Table (For Multi-Select use CTRL)" -PassThru
+        
+        foreach ($table in $ResultsAllTables) {
+            $TableName = $table.'$table'
+            $TablesArray.Add($TableName.ToString().Trim())
+        }
+    }
+    catch {
+        Write-Log $_ -LogFileName $LogFileName -Severity Error
+        Write-Log -Message "An error occurred in querying table names from $LogAnalyticsWorkspaceName" -LogFileName $LogFileName -Severity Error         
+        exit
+    }
+	
+	return $TablesArray	
+}
+
+
+function Get-TableConfiguration {
+	[CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $LaTables        
+    )
+	$QualifiedTables = @{}
+	
+	foreach ($LaTable in $LaTables) {
+		$TablesApi = "https://management.azure.com/subscriptions/$SubscriptionId/resourcegroups/$LogAnalyticsResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$LogAnalyticsWorkspaceName/tables/$LaTable" + "?api-version=2021-07-01-privatepreview"
+				
+		try {        
+			$TablesApiResult = Invoke-RestMethod -Uri $TablesApi -Method "GET" -Headers $LaAPIHeaders		
+		} 
+		catch {                    
+			Write-Log -Message "Get-TableConfiguration $($_)" -LogFileName $LogFileName -Severity Error		                
+		}
+
+		If ($TablesApiResult) {        
+            Write-Log -Message "$LaTable configuration : $($TablesApiResult.properties)" -LogFileName $LogFileName -Severity Information   
+            $QualifiedTables += @{$LaTable = $TablesApiResult.properties.retentionInDays}
+		}
+	}
+	
+	return $QualifiedTables	
+}
+
+
+function Set-TableConfiguration {
+	[CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $QualifiedTables,
+		[parameter(Mandatory = $true)] $RetentionType
+    )
+	
+	$SuccessTables = New-Object System.Collections.Generic.List[System.Object]
+	
+	foreach ($Qtable in $QualifiedTables) {
+		$TablesApi = "https://management.azure.com/subscriptions/$SubscriptionId/resourcegroups/$LogAnalyticsResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$LogAnalyticsWorkspaceName/tables/$Qtable" + "?api-version=2021-07-01-privatepreview"								
+		
+		$TablesApiBody = @"
+			{
+				"properties": {
+					"plan": $RetentionType
+				}
+			}
+"@
+		
+		try {        
+			$TablesApiResult = Invoke-RestMethod -Uri $TablesApi -Method "PUT" -Headers $LaAPIHeaders -Body $TablesApiBody           
+			Write-Log -Message $TablesApiResult -LogFileName $LogFileName -Severity Information
+		} 
+		catch {                    
+			Write-Log -Message "Set-TableConfiguration $($_)" -LogFileName $LogFileName -Severity Error		                
+		}
+
+		If ($TablesApiResult.StatusCode -ne 200) {
+			$SuccessTables.Add($Qtable)
+		}
+	}
+
+}
+
+
+function Update-TablesRetention {
+	[CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $TablesForRetention,		
+		[parameter(Mandatory = $true)] $TotalRetentionInDays
+    )
+		
+	$TablesForRetention.GetEnumerator() | ForEach-Object {
+		$TablesApi = "https://management.azure.com/subscriptions/$SubscriptionId/resourcegroups/$LogAnalyticsResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$LogAnalyticsWorkspaceName/tables/$($_.Key)" + "?api-version=2021-07-01-privatepreview"						
+					
+		$TablesApiBody = @"
+			{
+				"properties": {
+					"retentionInDays": $($_.Value),
+					"totalRetentionInDays":$TotalRetentionInDays
+				}
+			}
+"@
+		
+		try {        
+			$TablesApiResult = Invoke-RestMethod -Uri $TablesApi -Method "PUT" -Headers $LaAPIHeaders -Body $TablesApiBody           
+			
+		} 
+		catch {                    
+			Write-Log -Message "Update-TablesRetention $($_)" -LogFileName $LogFileName -Severity Error		                
+		}
+
+        if($TablesApiResult) {
+            Write-Log -Message "Table : $($_.Key) retention updated successfully from $($_.Value) days to $TotalRetentionInDays" -LogFileName $LogFileName -Severity Information
+        }
+		
+	}
+}
+
+function Collect-Input {
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = 'Data Entry Form'
+    $form.Size = New-Object System.Drawing.Size(300,200)
+    $form.StartPosition = 'CenterScreen'
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(75,120)
+    $okButton.Size = New-Object System.Drawing.Size(75,23)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(150,120)
+    $cancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $cancelButton.Text = 'Cancel'
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10,20)
+    $label.Size = New-Object System.Drawing.Size(300,60)
+    $label.Text = 'Enter value for total Retention In Days (between 7 and 730)*'
+    $form.Controls.Add($label)
+
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Point(10,80)
+    $textBox.Size = New-Object System.Drawing.Size(260,20)
+    $form.Controls.Add($textBox)
+
+    $form.Topmost = $true
+
+    $form.Add_Shown({$textBox.Select()})
+    $result = $form.ShowDialog()
+
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+    {
+        $x = $textBox.Text.Trim()        
+    }
+
+    if ($null -ne $x) {
+        return $x
+    }
+    else {
+        Write-Log "Total Retention Days is required" -LogFileName $LogFileName -Severity -Error
+    }
+}
+
+#endregion
+
+#region DriverProgram
+Get-RequiredModules("Az")
+Get-RequiredModules("Az.SecurityInsights")
+
+$TimeStamp = Get-Date -Format yyyyMMdd_HHmmss 
+$LogFileName = '{0}_{1}.csv' -f "Sentinel_Long_Term_Retention", $TimeStamp
+
+# Check Powershell version, needs to be 5 or higher
+if ($host.Version.Major -lt 5) {
+    Write-Log "Supported PowerShell version for this script is 5 or above" -LogFileName $LogFileName -Severity Error    
+    exit
+}
+
+#disconnect exiting connections and clearing contexts.
+Write-Log "Clearing existing Azure connection" -LogFileName $LogFileName -Severity Information
+    
+$null = Disconnect-AzAccount -ContextName 'MyAzContext' -ErrorAction SilentlyContinue
+    
+Write-Log "Clearing existing Azure context `n" -LogFileName $LogFileName -Severity Information
+    
+get-azcontext -ListAvailable | ForEach-Object{$_ | remove-azcontext -Force -Verbose | Out-Null} #remove all connected content
+    
+Write-Log "Clearing of existing connection and context completed." -LogFileName $LogFileName -Severity Information
+Try {
+    #Connect to tenant with context name and save it to variable
+    Connect-AzAccount -Tenant $TenantID -ContextName 'MyAzContext' -Force -ErrorAction Stop
+        
+    #Select subscription to build
+    $GetSubscriptions = Get-AzSubscription -TenantId $TenantID | Where-Object {($_.state -eq 'enabled') } | Out-GridView -Title "Select Subscription to Use" -PassThru       
+}
+catch {    
+    Write-Log "Error When trying to connect to tenant : $($_)" -LogFileName $LogFileName -Severity Error
+    exit    
+}
+
+$AzureAccessToken = (Get-AzAccessToken).Token            
+$LaAPIHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$LaAPIHeaders.Add("Content-Type", "application/json")
+$LaAPIHeaders.Add("Authorization", "Bearer $AzureAccessToken")
+
+#loop through each selected subscription.. 
+foreach($CurrentSubscription in $GetSubscriptions)
+{
+    Try 
+    {
+        #Set context for subscription being built
+        $null = Set-AzContext -Subscription $CurrentSubscription.id
+        $SubscriptionId = $CurrentSubscription.id
+        Write-Log "Working in Subscription: $($CurrentSubscription.Name)" -LogFileName $LogFileName -Severity Information
+
+        $LAWs = Get-AzOperationalInsightsWorkspace | Where-Object { $_.ProvisioningState -eq "Succeeded" } | Out-GridView -Title "Select Log Analytics workspace" -PassThru 
+        if($null -eq $LAWs) {
+            Write-Log "No Log Analytics workspace found..." -LogFileName $LogFileName -Severity Error 
+        }
+        else {
+            Write-Log "Listing Log Analytics workspace" -LogFileName $LogFileName -Severity Information
+                        
+            foreach($LAW in $LAWs) {
+                
+                $LogAnalyticsWorkspaceName = $LAW.Name
+                $LogAnalyticsResourceGroup = $LAW.ResourceGroupName
+                $LogAnalyticsWorkspaceId = $LAW.CustomerId              
+                
+                #Get all the tables from the selected Azure Log Analytics Workspace
+                $WorkspaceTables = Configure-AzureMonitorTables -RetentionMethod "Analytics"
+                $TotalRetentionInDays = Collect-Input 
+                $QualifiedTables = Get-TableConfiguration -LaTables $WorkspaceTables
+                #$ConfiguredTables = Set-TableConfiguration -QualifiedTables $QualifiedTables -RetentionType "Analytics"
+                Update-TablesRetention -TablesForRetention $QualifiedTables -TotalRetentionInDays $TotalRetentionInDays                
+            }                  
+
+        } 	
+    }
+    catch [Exception]
+    { 
+        Write-Log $_ -LogFileName $LogFileName -Severity Error                         		
+    }		 
+}
+#endregion DriverProgram 
