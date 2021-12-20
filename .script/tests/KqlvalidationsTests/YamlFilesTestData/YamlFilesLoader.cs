5a3f4adb76daa9bf7199f3959666b5ec07fb11f4
rpressburger <rpressburger@microsoft.com>
2021-12-20 14:26:36 +0200

    code review

--- .script/tests/KqlvalidationsTests/YamlFilesTestData/YamlFilesLoader.cs
+++ .script/tests/KqlvalidationsTests/YamlFilesTestData/YamlFilesLoader.cs
@@ -6,26 +6,30 @@ namespace Kqlvalidations.Tests
 {
     public abstract class YamlFilesLoader
     {
-        private static List<string> DirectoryNames { get; set; }
         protected List<string> DirectoryPaths { get; }
+
+        private const int TestFolderDepth = 6;
 
         protected YamlFilesLoader(List<string> directoryNames)
         {
-            DirectoryNames = directoryNames;
-            DirectoryPaths = GetYamlFilesPaths();
+            DirectoryPaths = GetYamlFilesPaths(directoryNames);
         }
 
         public virtual List<string> GetFilesNames()
         {
-            return Directory.GetFiles(DirectoryPaths[0], "*.yaml", SearchOption.AllDirectories).ToList();
+            return DirectoryPaths.Aggregate(new List<string>(), (accumulator, directoryPath) =>
+            {
+                var files = Directory.GetFiles(directoryPath, "*.yaml", SearchOption.AllDirectories).ToList();
+                return accumulator.Concat(files).ToList();
+            });
         }
 
-        private static List<string> GetYamlFilesPaths()
+        private List<string> GetYamlFilesPaths(List<string> directoryNames)
         {
-            var rootDir = Utils.GetTestDirectory(6);
+            var rootDir = Utils.GetTestDirectory(TestFolderDepth);
             List<string> detectionPaths = new List<string>();
             
-            foreach (var dirName in DirectoryNames)
+            foreach (var dirName in directoryNames)
             {
                 detectionPaths.Add(Path.Combine(rootDir, dirName));
             }
