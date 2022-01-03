ee97399b426a21878776c4c7b4835efc04b47393
Ofer Shezaf <github@shezaf.com>
2022-01-03 16:21:46 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Azure/Azure-Sentinel""
    This reverts commit ff69f85224e6fd92fae82b5c781b7e62ae9fb6ea.

---
+++ .script/tests/KqlvalidationsTests/YamlFilesTestData/DetectionsYamlFilesLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Kqlvalidations.Tests
+{
+    public class DetectionsYamlFilesLoader : YamlFilesLoader
+    {
+        protected override List<string> GetDirectoryPaths()
+        {
+            var basePath = Utils.GetTestDirectory(TestFolderDepth);
+            var detectionsDir = new List<string> { Path.Combine(basePath, "Detections")};
+            var solutionDirectories = Path.Combine(basePath, "Solutions");
+            var analyticsRulesDir  = Directory.GetDirectories(solutionDirectories, "Analytic Rules", SearchOption.AllDirectories);
+
+            return analyticsRulesDir.Concat(detectionsDir).ToList();
+        }
+    }
+}
