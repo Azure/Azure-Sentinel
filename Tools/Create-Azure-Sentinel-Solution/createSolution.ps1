7b8e77c538dc5368bda31f162beaf4dee0e29099
Eli Forbes <v-eliforbes@microsoft.com>
2022-01-13 10:32:30 -0800

    fix miscommit

--- Tools/Create-Azure-Sentinel-Solution/createSolution.ps1
+++ Tools/Create-Azure-Sentinel-Solution/createSolution.ps1
@@ -864,14 +864,7 @@ foreach ($inputFile in $(Get-ChildItem $path)) {
                                 }
                                 $huntingQueryObj.properties.tags += $tacticsObj
                             }
-                            function getQueryResourceLocation () {
-                                for($i = 0; $i -lt $baseMainTemplate.resources.Length; $i++){
-                                    if ($baseMainTemplate.resources[$i].type -eq "Microsoft.OperationalInsights/workspaces") {
-                                        return $i
-                                    }
-                                }
-                            }
-                            $baseMainTemplate.resources[$(getQueryResourceLocation)].resources += $huntingQueryObj
+                            $baseMainTemplate.resources[$baseMainTemplate.resources.Length - 1].resources += $huntingQueryObj
 
                             $dependencyDescription = ""
                             if ($yaml.requiredDataConnectors) {
