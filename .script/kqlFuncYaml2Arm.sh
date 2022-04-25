0484484835c307c2d2e880a92061469c68e3d260
sivanguetta <sivang@microsoft.com>
2022-04-25 14:40:55 +0300

    Update main to master

--- .script/kqlFuncYaml2Arm.sh
+++ .script/kqlFuncYaml2Arm.sh
@@ -5,13 +5,13 @@ failed=0
 parsersSchemas=(ASimDns ASimNetworkSession ASimWebSession)
 for schema in ${parsersSchemas[@]}
 	do  
-		filesThatWereChanged=$(echo $(git diff origin/main  --name-only -- Parsers/$schema/))
-		echo $filesThatWereChanged
+		filesThatWereChanged=$(echo $(git diff origin/master  --name-only -- Parsers/$schema/))
 		if [ "$filesThatWereChanged" = "" ]; then 
 			echo No files were changed under Azure-Sentinel/Parsers/$schema/
 		else
 			echo Regenerate ARM templates under Azure-Sentinel/Parsers/$schema/ARM
-			rm -rfv Parsers//$schema/ARM
+			echo $filesThatWereChanged
+			rm -rf Parsers//$schema/ARM
 			python ASIM/dev/ASimYaml2ARM/KqlFuncYaml2Arm.py -m asim -d Parsers//$schema/ARM Parsers//$schema/Parsers
 		fi
 	done
