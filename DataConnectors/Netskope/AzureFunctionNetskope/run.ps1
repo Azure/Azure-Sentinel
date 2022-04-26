abfde44ba5759364421d57c7508cba795b46fac7
Devika Mehra <demehra@microsoft.com>
2022-04-26 21:41:34 +0530

    Added Comment for time so far

--- DataConnectors/Netskope/AzureFunctionNetskope/run.ps1
+++ DataConnectors/Netskope/AzureFunctionNetskope/run.ps1
@@ -135,12 +135,13 @@ function GetUrl ($uri, $ApiKey, $StartTime, $EndTime, $LogType, $Page, $Skip){
                 }
 
                 $functionCurrentTimeEpoch = (Get-Date -Date ((Get-Date).DateTime) -UFormat %s)
+                Write-Host "Time of Execution so far : LogType - $($logtype) : Skip - $($skip) : $($functionCurrentTimeEpoch - $functionStartTimeEpoch)"
 
-                if ($functionCurrentTimeEpoch - $functionStartTimeEpoch -gt 570) {
-                    UpdateCheckpointTime -CheckpointFile $checkPointFile -LogType $logtype -LastSuccessfulTime $startTime -skip $skip
-                    Write-Host "Exiting from do while loop for logType : $($logtype) to avoid function timeout."
-                    break
-                }
+                #if ($functionCurrentTimeEpoch - $functionStartTimeEpoch -gt 570) {
+                #    UpdateCheckpointTime -CheckpointFile $checkPointFile -LogType $logtype -LastSuccessfulTime $startTime -skip $skip
+                #    Write-Host "Exiting from do while loop for logType : $($logtype) to avoid function timeout."
+                #    break
+                #}
 
             }
             catch {
