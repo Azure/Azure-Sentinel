0d693486e15e204ba423e9898ce864b28330e788
Devika Mehra <demehra@microsoft.com>
2022-04-28 00:48:58 +0530

    Split logic modified

--- DataConnectors/Netskope/AzureFunctionNetskope/run.ps1
+++ DataConnectors/Netskope/AzureFunctionNetskope/run.ps1
@@ -196,12 +196,14 @@ function GetUrl ($uri, $ApiKey, $StartTime, $EndTime, $LogType, $Page, $Skip){
             else {
                 Write-Host "Warning!: Total data size is > 30mb hence performing the operation of split and process."
                 
-                $counter = [pscustomobject] @{ Value = 0 }
-                $groupSize = [math]::Ceiling($alleventobjs.Length/2) 
-                $groups = $alleventobjs | Group-Object -Property { [math]::Floor($counter.Value++ / $groupSize) }
+                $part1 = $alleventobjs[0..([math]::Ceiling($alleventobjs.Length/2)-1)]
+                $part2 = $alleventobjs[([math]::Ceiling($alleventobjs.Length/2))..$alleventobjs.Length]
 
-                $responseCode0 = ProcessData -allEventsLength $groups[0].Group.Count -alleventobjs $groups[0].Group -checkPointFile $checkPointFile -logtype $logtype -endTime $endTime
-                $responseCode1 = ProcessData -allEventsLength $groups[1].Group.Count -alleventobjs $groups[1].Group -checkPointFile $checkPointFile -logtype $logtype -endTime $endTime
+                $responseCode0 = ProcessData -allEventsLength $part1.Length -alleventobjs $part1 -checkPointFile $checkPointFile -logtype $logtype -endTime $endTime
+                Write-Host "Processed Part 1 with length $($part1.length) | Result : $($responseCode0)"
+
+                $responseCode1 = ProcessData -allEventsLength $part2.Length -alleventobjs $part1 -checkPointFile $checkPointFile -logtype $logtype -endTime $endTime
+                Write-Host "Processed Part 2 with length $($part2.length) | Result : $($responseCode1)"
 
                 $responseCode = responseCode0 -ne 200 ? responseCode0 : responseCode1
             }
