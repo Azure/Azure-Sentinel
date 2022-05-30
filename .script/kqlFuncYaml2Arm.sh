9342b76b249909b4a2992cda7aeab012f489b4b8
sivanguetta <sivang@microsoft.com>
2022-05-30 14:26:40 +0300

    change script

--- .script/kqlFuncYaml2Arm.sh
+++ .script/kqlFuncYaml2Arm.sh
@@ -4,6 +4,15 @@ failed=0
 
 # The KqlFuncYaml2Arm script generates deployable ARM templates from KQL function YAML files.
 # Currently, the script only runs on the Schemas listed below.
+
+git merge origin/master
+status = $(echo $(git status))
+if [[ "$status" == *"conflict"* ]]; then
+	$failed=1
+	echo "There are merge conflics between origin/master and the pull request branch. Please resolve them."
+  	exit $failed
+fi
+
 parsersSchemas=(ASimDns ASimNetworkSession ASimWebSession ASimProcessEvent)
 for schema in ${parsersSchemas[@]}
 	do  
