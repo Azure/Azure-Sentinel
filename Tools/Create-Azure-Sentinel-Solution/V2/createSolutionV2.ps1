5a474c0ca363760eb8e7ee6ca40080b8804d937f
v-rucdu <v-rucdu@microsoft.com>
2022-07-29 18:35:03 +0530

    fixed playbook and workbook related bugs

--- Tools/Create-Azure-Sentinel-Solution/V2/createSolutionV2.ps1
+++ Tools/Create-Azure-Sentinel-Solution/V2/createSolutionV2.ps1
@@ -151,7 +151,7 @@ foreach ($inputFile in $(Get-ChildItem $path)) {
     $baseCreateUiDefinitionPath = "$PSScriptRoot/templating/baseCreateUiDefinition.json"
     $metadataPath = "$PSScriptRoot/../../../Solutions/$($contentToImport.Name)/$($contentToImport.Metadata)"
 
-    $workbookMetadataPath = "https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/"
+    $workbookMetadataPath = "C:\GitHub\Azure-Sentinel\"
     # Base JSON Objects
     $baseMainTemplate = Get-Content -Raw $baseMainTemplatePath | Out-String | ConvertFrom-Json
     $baseCreateUiDefinition = Get-Content -Raw $baseCreateUiDefinitionPath | Out-String | ConvertFrom-Json
@@ -403,7 +403,7 @@ foreach ($inputFile in $(Get-ChildItem $path)) {
                                     };
                                     author    = $authorDetails;
                                     support   = $baseMetadata.support;
-                                    dependencies = $workbookDependencies;
+                                    # dependencies = $workbookDependencies;
                                 }
                             }
 
@@ -1064,6 +1064,23 @@ foreach ($inputFile in $(Get-ChildItem $path)) {
                                 }
                             }
 
+                            if ($null -ne $playbookTemplateSpecContent.properties.mainTemplate.metadata.entities -and
+                                $playbookTemplateSpecContent.properties.mainTemplate.metadata.entities.count -le 0)
+                            {
+                                $playbookTemplateSpecContent.properties.mainTemplate.metadata.PSObject.Properties.Remove("entities");
+                            }
+
+                            if ($null -ne $playbookTemplateSpecContent.properties.mainTemplate.metadata.tags -and
+                                $playbookTemplateSpecContent.properties.mainTemplate.metadata.tags.count -le 0)
+                            {
+                                $playbookTemplateSpecContent.properties.mainTemplate.metadata.PSObject.Properties.Remove("tags");
+                            }
+
+                            if ($null -ne $playbookTemplateSpecContent.properties.mainTemplate.metadata.prerequisites -and
+                            ![string]::IsNullOrWhitespace($playbookTemplateSpecContent.properties.mainTemplate.metadata.prerequisites))
+                            {
+                                $playbookTemplateSpecContent.properties.mainTemplate.metadata.PSObject.Properties.Remove("prerequisites");
+                            }
                             $baseMainTemplate.resources += $playbookTemplateSpecContent;
                         }
                         else
@@ -1532,7 +1549,7 @@ foreach ($inputFile in $(Get-ChildItem $path)) {
 
                         # Add Watchlist ID to MainTemplate parameters
                         $watchlistIdParameterName = "watchlist$watchlistCounter-id"
-                        $watchlistIdParameter = [PSCustomObject] @{ type = "string"; defaultValue = "[newGuid()]"; minLength = 1; metadata = [PSCustomObject] @{ description = "Unique id for the watchlist" }; }
+                        $watchlistIdParameter = [PSCustomObject] @{ type = "string"; defaultValue = "$($watchlistData.properties.watchlistAlias)"; minLength = 1; metadata = [PSCustomObject] @{ description = "Unique id for the watchlist" }; }
                         $baseMainTemplate.parameters | Add-Member -MemberType NoteProperty -Name $watchlistIdParameterName -Value $watchlistIdParameter
 
                         # Replace watchlist resource id
