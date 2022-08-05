272522b2cccbcdebbea290ee5557884e983eeb3f
intrigus <intrigus@users.noreply.github.com>
2022-08-05 17:25:44 +0200

    Update kqlFuncYaml2Arm.ps1

--- .script/kqlFuncYaml2Arm.ps1
+++ .script/kqlFuncYaml2Arm.ps1
@@ -1,10 +1,6 @@
 $failed=0
 # The KqlFuncYaml2Arm script generates deployable ARM templates from KQL function YAML files.
 # Currently, the script only runs on the Schemas listed below.
-$modifiedSchemas = & "$($PSScriptRoot)/getModifiedASimSchemas.ps1"
-foreach($schema in $modifiedSchemas) {
-	Remove-Item "$($PSScriptRoot)/../Parsers/$schema/ARM" -Recurse
-	python ASIM/dev/ASimYaml2ARM/KqlFuncYaml2Arm.py -m asim -d Parsers/$schema/ARM Parsers/$schema/Parsers
-}
+python ASIM/dev/ASimYaml2ARM/KqlFuncYaml2Arm.py -m asim -d Parsers/f000/ARM Parsers/f000/Parsers
 
 exit $failed
