c5c9062328f63e99f3403ca086681b4359e4c498
Danielle Ohfeld Zats <danielohfeld@microsoft.com>
2022-09-14 10:39:13 +0300

    add project number and id as output

--- DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPAuditLogsSetup/GCPAuditLogsSetup.tf
+++ DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPAuditLogsSetup/GCPAuditLogsSetup.tf
@@ -36,6 +36,14 @@ resource "google_logging_project_sink" "sentinel-sink" {
   depends_on = [google_pubsub_topic.sentinel-topic]
 }
 
+output "GCP_project_id" {
+  value       = data.google_project.project.project_id
+}
+
+output "GCP_project_number" {
+  value       = data.google_project.project.number
+}
+
 output "GCP_subscription_name" {
   value       = google_pubsub_subscription.sentinel-subscription.name
 }
