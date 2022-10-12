07112046a558f483178f34920c1b856366a8d8f9
sivanguetta <sivang@microsoft.com>
2022-10-12 14:07:03 +0300

    KQL validation tests- support ASIM\lib\functions

---
+++ .script/tests/KqlvalidationsTests/FunctionSchemasLoaders/TypesDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Sentinel.KustoServices.Contract;
+using Microsoft.Azure.Sentinel.KustoServices.Implementation;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Kqlvalidations.Tests.FunctionSchemasLoaders
+{
+    public static class TypesDatabase
+    {
+        public static IReadOnlyDictionary<string, string> TypeToDefaultValueMapping = new Dictionary<string, string>()
+        {
+            { "string", "" },
+            { "int", "0" },
+            { "dynamic", "dynamic([])" },
+            { "table:(TimeGenerated:datetime)", "range TimeGenerated from ago(3d) to now() step 1d" },
+        };
+    }
+}
