0233b3928a371b7161bc7466043fe24f6df57f61
mkchiliveri <mkchiliveri@gmail.com>
2022-10-17 06:27:53 +0530

    updated

--- .script/checkThatTemplatesVersionWasChanged.sh
+++ .script/checkThatTemplatesVersionWasChanged.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 failed=0
-SAVEIFS=$IFS
-IFS=$'\n'
-filesThatWereChanged=$(git diff origin/master --diff-filter=M --name-only)
-IFS=$SAVEIFS
-for (( i=0; i<${#filesthatwerechanged[@]}; i++ ))
+readarray -t <<<$(git diff origin/master --diff-filter=M --name-only)
+for (( i=0; i<${#MAPFILE[@]}; i++ ))
     #Going over all the files that were changed in this PR
     #And making sure that in every file that its filename contains the word "Detection", the version was updated
     do
-    	echo processing the file ${filesthatwerechanged[$i]}.
-	if [[ "${filesthatwerechanged[$i]}" == *"Detections/"* || "${filesthatwerechanged[$i]}" == *"Analytic Rules/"* ]];
+    	echo processing the file ${MAPFILE[$i]}.
+	if [[ "${MAPFILE[$i]}" == *"Detections/"* || "${MAPFILE[$i]}" == *"Analytic Rules/"* ]];
 	then
-		echo ${filesthatwerechanged[$i]} is a detection
-		diffs=$(echo $(git diff origin/master -U0 --ignore-space-change ${filesthatwerechanged[$i]}))
+		echo ${MAPFILE[$i]} is a detection
+		diffs=$(echo $(git diff origin/master -U0 --ignore-space-change ${MAPFILE[$i]}))
 		if [[ "$diffs" == *"version:"* ]];
 		then
 			echo "all good - the version was updated"
 		else
-			echo "You **did not** change the version in this file: ${filesthatwerechanged[$i]}."
+			echo "You **did not** change the version in this file: ${MAPFILE[$i]}."
 			failed=1
 		fi
 
 	else
-		echo "${filesthatwerechanged[$i]} is not a detection."		
+		echo "${MAPFILE[$i]} is not a detection."		
     fi
 done
 
