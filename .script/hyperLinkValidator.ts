7e183bd2db7f9faa6d4d168b86876f043cff13d5
v-amolpatil <v-amolpatil@microsoft.com>
2022-11-23 22:23:34 +0530

    updated code to run only on Solutions data or Data connector folders only

--- .script/hyperLinkValidator.ts
+++ .script/hyperLinkValidator.ts
@@ -3,47 +3,63 @@ import { runCheckOverChangedFiles } from "./utils/changedFilesValidator";
 import { ExitCode } from "./utils/exitCode";
 import * as logger from "./utils/logger";
 
+export async function ValidateHyperlinks(filePath: string): Promise<ExitCode> 
+{
+  let splitPath = filePath.split("/")
+  if (splitPath[0] === "Solutions")
+  {
+    let dataFolderName = splitPath[2] === "Data" || splitPath[2] === "data" ? splitPath[2] : null
+    let dataConnectorFolderName = splitPath[2] === "DataConnectors" || splitPath[2] === "Data Connectors" ? splitPath[2] : null
+    if (dataFolderName == null && dataConnectorFolderName == null) 
+    {
+      console.log(`Skipping Hyperlink validation for file path : '${filePath}' as change is not in 'Data' and/or 'Data Connectors' folder`)
+      return ExitCode.SUCCESS;
+    }
+    
+    const content = fs.readFileSync(filePath, "utf8");
 
+    //get http or https links from the content
+    //const links = content.match(/https?:\/\/[^\s]+/g);
+    const links = content.match(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])+/g);
+    if (links) 
+    {
+      console.log(links)
+      var invalidLinks = new Array();
+      for (var link of links) 
+      {
+        link = link.replace(/["']/g, "")
 
+        //check if the link is valid
+        const isValid = await isValidLink(link);
+        if (!isValid) {
+          invalidLinks.push(link)
+        }
+      }
 
-export async function ValidateHyperlinks(filePath: string): Promise<ExitCode> {
-
-  const content = fs.readFileSync(filePath, "utf8");
-
-  //get http or https links from the content
-  //const links = content.match(/https?:\/\/[^\s]+/g);
-  const links = content.match(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])+/g);
-  if (links) {
-    console.log(links)
-    var invalidLinks = new Array();
-    for (var link of links) {
-      link = link.replace(/["']/g, "")
-
-      //check if the link is valid
-      const isValid = await isValidLink(link);
-      if (!isValid) {
-        //logger.logError(`Invalid link: ${link}`);
-        //throw new Error();
-        invalidLinks.push(link)
+      console.log(`Total Invalid Links: ${invalidLinks.length}`)
+      if (invalidLinks.length > 0) 
+      {
+        console.log(`Below are the invalid links:`)
+        invalidLinks.forEach(l => {
+          logger.logError(`\n ${l}`);
+        });
+        
+        throw new Error();
       }
     }
 
-    console.log(`Total Invalid Links: ${invalidLinks.length}`)
-    if (invalidLinks.length > 0) {
-      console.log(`Below are the invalid links:`)
-      invalidLinks.forEach(l => {
-        logger.logError(`\n ${l}`);
-      });
-      
-      throw new Error();
-    }
+    return ExitCode.SUCCESS
+  }
+  else
+  {
+    console.log(`Skipping Hyperlink validation for file path : '${filePath}' as change is not in 'Solutions' folder`)
+    return ExitCode.SUCCESS
   }
 
   //create a function to check if the link is valid
   async function isValidLink(link: string): Promise<boolean> {
     try {
       //import XMLHttpRequest from "xmlhttprequest"
-      //console.log("link to valid is ", link)
       const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
       const request = new XMLHttpRequest();
       request.open("GET", link, false);
@@ -64,16 +80,16 @@ export async function ValidateHyperlinks(filePath: string): Promise<ExitCode> {
       console.log(error);
       return false;
     }
-  }    
+  }
+
   return ExitCode.SUCCESS;
 }
 
 let fileTypeSuffixes = ["json"];
-let filePathFolderPrefixes = ["DataConnectors", "Data Connectors", "Solutions/Data"];
+let filePathFolderPrefixes = ["DataConnectors", "Data Connectors", "Solutions"];
 let fileKinds = ["Added", "Modified"];
 let CheckOptions = {
   onCheckFile: (filePath: string) => {
-    console.log(`File path is ${filePath}`)
     return ValidateHyperlinks(filePath);
   },
   onExecError: async (e: any, filePath: string) => {
