d6734b3ec1fac35292de10088239c3368c39b666
v-amolpatil <v-amolpatil@microsoft.com>
2022-11-23 17:17:57 +0530

    Update gitWrapper.ts

--- .script/utils/gitWrapper.ts
+++ .script/utils/gitWrapper.ts
@@ -12,6 +12,16 @@ export async function GetPRDetails() {
     console.log(`config Details are ${config}`)
     pullRequestDetails = await devOps.createPullRequestProperties(config);
     console.log(`PR Details are ${pullRequestDetails}`)
+    var ss1 = pullRequestDetails.structuralDiff
+    var ss2 = pullRequestDetails.targetBranch
+    var ss3 = pullRequestDetails.sourceBranch
+    var ss4 = pullRequestDetails.workingDir
+
+    console.log(`structuralDiff Details are ${ss1}`)
+    console.log(`targetBranch Details are ${ss2}`)
+    console.log(`sourceBranch Details are ${ss3}`)
+    console.log(`workingDir Details are ${ss4}`)
+
   }
   return pullRequestDetails;
 }
