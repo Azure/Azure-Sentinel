ab866e290e3a8c62664b75a2baf0d1274388b30a
sivanguetta <sivang@microsoft.com>
2022-11-30 13:01:46 +0200

    test

--- .script/kqlFuncYaml2Arm.ps1
+++ .script/kqlFuncYaml2Arm.ps1
@@ -3,7 +3,9 @@ $failed=0
 # Currently, the script only runs on the Schemas listed below.
 $modifiedSchemas = & "$($PSScriptRoot)/getModifiedASimSchemas.ps1"
 foreach($schema in $modifiedSchemas) {
-	Remove-Item "$($PSScriptRoot)/../Parsers/$schema/ARM" -Recurse
+	if (Test-Path "$($PSScriptRoot)/../Parsers/$schema/ARM") {
+		Remove-Item "$($PSScriptRoot)/../Parsers/$schema/ARM" -Recurse
+	}
 	python ASIM/dev/ASimYaml2ARM/KqlFuncYaml2Arm.py -m asim -d Parsers/$schema/ARM Parsers/$schema/Parsers
 }
 
