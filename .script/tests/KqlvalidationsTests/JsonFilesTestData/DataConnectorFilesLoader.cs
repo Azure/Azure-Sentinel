418eb5c96bc5c627d7b61195712c9b1733ced215
mkchiliveri <mkchiliveri@gmail.com>
2022-12-13 14:30:21 +0530

    updated

--- .script/tests/KqlvalidationsTests/JsonFilesTestData/DataConnectorFilesLoader.cs
+++ .script/tests/KqlvalidationsTests/JsonFilesTestData/DataConnectorFilesLoader.cs
@@ -23,7 +23,7 @@ namespace Kqlvalidations.Tests
         //over ride GetFilesNames method
         public override List<string> GetFilesNames()
         {
-            Console.WriteLine("PR number printed "+Environment.GetEnvironmentVariable("PRNumber"));
+            Console.WriteLine("PR number printed "+Environment.GetEnvironmentVariable("FOO"));
             var directoryPaths = GetDirectoryPaths();
             return directoryPaths.Aggregate(new List<string>(), (accumulator, directoryPath) =>
             {
