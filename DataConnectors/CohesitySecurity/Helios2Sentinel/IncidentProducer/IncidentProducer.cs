a3da7f4d4382060342c448a7df0e08c43e343fee
Ying Huang <ying.huang@cohesity.com>
2022-12-15 18:17:20 -0800

    add the vm playbook related parameters into blob storage in IncidentProducer.

--- DataConnectors/CohesitySecurity/Helios2Sentinel/IncidentProducer/IncidentProducer.cs
+++ DataConnectors/CohesitySecurity/Helios2Sentinel/IncidentProducer/IncidentProducer.cs
@@ -20,6 +20,8 @@ using System.Threading.Tasks;
 using System;
 using System.Dynamic;
 using Newtonsoft.Json.Converters;
+using Microsoft.WindowsAzure.Storage;
+using Helios2Sentinel;
 
 namespace Helios2Sentinel
 {
@@ -63,9 +65,9 @@ namespace Helios2Sentinel
             });
         }
 
-        public static void ParseAlertToQueue(
+        public static async Task ParseAlertToQueueAsync(
             [Queue("%CohesityQueueName%"), StorageAccount("AzureWebJobsStorage")] ICollector<string> outputQueueItem,
-            dynamic alert)
+            dynamic alert, ILogger log)
         {
             dynamic output = new ExpandoObject();
             output.properties = new ExpandoObject();
@@ -74,7 +76,9 @@ namespace Helios2Sentinel
             output.properties.status = ((string)alert.alertState).Equals("kOpen", StringComparison.OrdinalIgnoreCase) ? "New" : "Closed";
             output.properties.severity = "Medium";
 
+            string id = alert.id;
             int i = 0;
+
             foreach (var prop in alert.propertyList)
             {
                 switch (i)
@@ -88,8 +92,16 @@ namespace Helios2Sentinel
                     else
                         output.properties.severity = "Medium";
                     break;
+                case 0:
+                case 10:
+                case 4:
+                case 7:
+                case 11:
+                    TextWriter(id, (string)prop.key, (string)prop.value, log);
+                    break;
                 case 1:
                     output.properties.title += ". Object: " + prop.value;
+                    TextWriter(id, (string)prop.key, (string)prop.value, log);
                     break;
                 case 3:
                     output.properties.title += ". Source: " + prop.value;
@@ -102,6 +114,39 @@ namespace Helios2Sentinel
             lock (queueLock)
             {
                 outputQueueItem.Add(JsonConvert.SerializeObject(output));
+            }
+        }
+
+        private static void WriteData(string storageConnectionString, string containerName, string path, string data)
+        {
+            if (CloudStorageAccount.TryParse(storageConnectionString, out var storageAccount))
+            {
+                // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                var cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                var container = cloudBlobClient.GetContainerReference(containerName);
+                var blobRef = container.GetBlockBlobReference(path);
+                var dataAsBytes = Encoding.UTF8.GetBytes(data);
+                blobRef.UploadFromByteArrayAsync(dataAsBytes, 0, dataAsBytes.Length).Wait();
+            }
+            else
+            {
+                throw new InvalidProgramException("Invalid format string");
+            }
+        }
+
+        private static void TextWriter(string incidentID, string param, string value, ILogger log)
+        {
+            try
+            {
+                const string container = "extra-parameters";
+                string Blob = incidentID + "\\" + param; // ID unique to the incident
+                var blobStorageConnectionString = Environment.GetEnvironmentVariable("BlobStorageConnectionString");
+                var blobStorageKeys = Environment.GetEnvironmentVariable("BlobStorageAccountKeys");
+                WriteData(blobStorageConnectionString, container, Blob, value);
+            }
+            catch (Exception ex)
+            {
+                log.LogError("Exception --> " + ex.Message);
             }
         }
 
@@ -145,6 +190,7 @@ namespace Helios2Sentinel
                 log.LogInformation("endDateUsecs --> " + endDateUsecs.ToString());
 
                 string requestUriString = $"https://helios.cohesity.com/mcm/alerts?alertCategoryList=kSecurity&alertStateList=kOpen&startDateUsecs={startDateUsecs}&endDateUsecs={endDateUsecs}";
+                log.LogInformation("requestUriString --> " + requestUriString);
                 using HttpClient client = new ();
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Add("apiKey", System.Environment.GetEnvironmentVariable("apiKey"));
@@ -152,26 +198,13 @@ namespace Helios2Sentinel
                 StreamReader reader = new StreamReader(stream);
                 dynamic alerts = JsonConvert.DeserializeObject(reader.ReadToEnd());
 
-                var tasks = new List<Task>();
-
                 foreach (var alert in alerts)
                 {
-                    tasks.Add(Task.Run( () =>
-                    {
-                        ParseAlertToQueue(outputQueueItem, alert);
-                    }));
+                    await ParseAlertToQueueAsync(outputQueueItem, alert, log);
                 }
-                Task t = Task.WhenAll(tasks);
-                try
-                {
-                    t.Wait();
-                }
-                catch {}
 
-                if (t.Status == TaskStatus.RanToCompletion)
-                {
-                    db.StringSet(redisKey, endDateUsecs.ToString());
-                }
+                db.StringSet(redisKey, endDateUsecs.ToString());
+                log.LogInformation("new startDateUsecs --> " + endDateUsecs.ToString());
             }
             catch (Exception ex)
             {
