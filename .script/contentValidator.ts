5d4706533d0fccf3b45f6f898e2e8d36ecae9da2
v-amolpatil <v-amolpatil@microsoft.com>
2022-12-16 11:11:48 +0530

    Added code to validate Azure Sentinel in PR file changes

---
+++ .script/contentValidator.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import { runCheckOverChangedFiles } from "./utils/changedFilesValidator";
+import { ExitCode } from "./utils/exitCode";
+import * as logger from "./utils/logger";
+
+export async function ValidateFileContent(filePath: string): Promise<ExitCode> 
+{
+    // CHECK IF FILE CONTAINS "Azure Sentinel". IF YES THEN ERROR ELSE SUCCESS. THIS IS BECAUSE WE NOW WORK ON
+    if (!filePath.includes("azure-pipelines"))
+    {
+        const fileContent = fs.readFileSync(filePath, "utf8");
+        const searchText = "Azure Sentinel"
+        const replaceWithText = "Microsoft Sentinel"
+        const hasAzureSentinelText = fileContent.toLowerCase().includes(searchText.toLowerCase());
+        
+        if (hasAzureSentinelText)
+        {
+            throw new Error(`'${searchText}' text is not allowed. Please replace '${searchText}' text with '${replaceWithText}' in file '${filePath}'`);
+        }
+    }
+    return ExitCode.SUCCESS;
+}
+
+let fileTypeSuffixes = ["json", "txt", "md", "yaml", "yml", "py"];
+let fileKinds = ["Added", "Modified"];
+let CheckOptions = {
+    onCheckFile: (filePath: string) => {
+        return ValidateFileContent(filePath)
+    },
+    onExecError: async (e: any) => {
+        logger.logError(`Content Validation check Failed: ${e.message}`);
+    },
+    onFinalFailed: async () => {
+        logger.logError("An error occurred, please open an issue");
+    },
+};
+
+runCheckOverChangedFiles(CheckOptions, fileKinds, fileTypeSuffixes);
