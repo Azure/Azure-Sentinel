1f6d1aef77bb810b2aee780a38d9f1702ba0be0c
Danielle Ohfeld Zats <danielohfeld@microsoft.com>
2022-12-27 09:38:15 +0200

    add print of workload identity pool id

--- DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPInitialAuthenticationSetup/GCPInitialAuthenticationSetup.tf
+++ DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPInitialAuthenticationSetup/GCPInitialAuthenticationSetup.tf
@@ -11,7 +11,7 @@ terraform {
 
 locals{
   sentinel_app_id = "2041288c-b303-4ca0-9076-9612db3beeb2" // Do not change it. It's our Azure Active Directory app id that will be used for authentication with your project.
-  sentinel_tenant_id = "33e01921-4d64-4f8c-a055-5bdaffd5e33d" // Do not change it. It's our tenant id that will be used for authentication with your project.
+  sentinel_auth_id = "33e01921-4d64-4f8c-a055-5bdaffd5e33d" // Do not change it. It's our tenant id that will be used for authentication with your project.
   workload_identity_pool_id = replace(var.tenant-id, "-", "") // Do not change it.
 }
 
