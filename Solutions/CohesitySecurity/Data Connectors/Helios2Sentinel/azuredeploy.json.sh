d43cfccf320eca66bbb8f94eac77198dbaf04434
Ying Huang <ying.huang@cohesity.com>
2023-01-06 14:25:35 -0800

    update deploy scripts.

--- Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/azuredeploy.json.sh
+++ Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/azuredeploy.json.sh
@@ -3,8 +3,15 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./remove.py "cohesitypro" "ying-test-resource-group"
-./remove.py "cohesitycon" "ying-test-resource-group"
-./deploy_config.sh
-./deploy.py "cohesitypro" "./IncidentProducer/"
-./deploy.py "cohesitycon" "./IncidentConsumer/"
+resourcegroup="ying-test-resource-group"
+workspacename="auto-deploy-workspace"
+producer_fun_prefix="cohesitypro"
+producer_context="IncidentProducer"
+consumer_fun_prefix="cohesitypro"
+consumer_context="IncidentConsumer"
+
+./remove.py "$producer_fun_prefix" "$resourcegroup"
+./remove.py "$consumer_fun_prefix" "$resourcegroup"
+./deploy_config.sh "$workspacename" "$resourcegroup"
+./deploy.py "$producer_fun_prefix" "./$producer_context/"
+./deploy.py "$consumer_fun_prefix" "./$consumer_context/"
--- Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/deploy_config.sh
+++ Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/deploy_config.sh
@@ -1,11 +1,24 @@
 #!/bin/zsh
 
+workspacename="$1"
+resourcegroup="$2"
+
+az monitor log-analytics workspace delete \
+    --force \
+    --yes \
+    --resource-group "$resourcegroup" \
+    --workspace-name "$workspacename"
+
+az monitor log-analytics workspace create \
+    -g "$resourcegroup" \
+    -n "$workspacename"
+
 az deployment group create \
     --name ExampleDeployment \
-    --resource-group ying-test-resource-group \
+    --resource-group "$resourcegroup" \
     --template-file ./azuredeploy.json \
     --parameters ApiKey="33e44eac-ce99-46df-7f4e-9ac39446a66e" \
     --parameters ClientId="cf58a81b-bfc5-4942-9f5e-9cdc8d1d119d" \
     --parameters ClientKey="Xzf8Q~SxY28H4UA6fd70bt39DB92xoweNC_RRc_y" \
     --parameters StartDaysAgo="-30" \
-    --parameters Workspace="ying-workspace"
+    --parameters Workspace="$workspacename"
