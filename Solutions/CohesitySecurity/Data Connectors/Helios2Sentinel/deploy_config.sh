e655da1430181309b41c82c900a48c30aa8e2c3f
Ying Huang <ying.huang@cohesity.com>
2023-01-10 16:08:09 -0800

    update deploy script.

--- Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/deploy_config.sh
+++ Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/deploy_config.sh
@@ -3,8 +3,18 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-workspacename="$1"
-resourcegroup="$2"
+if [ -z "$1" ]
+then
+    workspacename="auto-deploy-workspace"
+else
+    workspacename="$1"
+fi
+
+if [ -z "$2" ]
+then
+    resourcegroup="ying-test-resource-group"
+else
+    resourcegroup="$2"
 fi
 
 az monitor log-analytics workspace delete \
