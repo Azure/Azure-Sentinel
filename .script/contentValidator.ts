22883bfe1cedac2cdefeabf34205e68219de3299
v-amolpatil <v-amolpatil@microsoft.com>
2023-01-16 22:53:59 +0530

    test1

--- .script/contentValidator.ts
+++ .script/contentValidator.ts
@@ -8,12 +8,17 @@ export async function ValidateFileContent(filePath: string): Promise<ExitCode>
     if (!filePath.includes("azure-pipelines"))
     {
         const fileContent = fs.readFileSync(filePath, "utf8");
+        console.log(`file content is ${fileContent}`)
+        console.log(`filePath is ${filePath}`)
         const searchText = "Azure Sentinel"
         const hasAzureSentinelText = fileContent.toLowerCase().includes(searchText.toLowerCase());
         const hasTargetProductAzureSentinel = fileContent.includes("''targetProduct'': ''Azure Sentinel''")
 
+        console.log(`hasAzureSentinelText is ${hasAzureSentinelText}`)
+        console.log(`hasTargetProductAzureSentinel is ${hasTargetProductAzureSentinel}`)
         if (hasAzureSentinelText && !hasTargetProductAzureSentinel)
         {
+            console.log(`Inside of if condition`)
             throw new Error(`Please update text from 'Azure Sentinel' to 'Microsoft Sentinel' in file '${filePath}'`);
         }
     }
