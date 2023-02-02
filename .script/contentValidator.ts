50a6e9945587329b4cb7a5a5ee4fc542f56b36db
v-amolpatil <v-amolpatil@microsoft.com>
2023-02-02 10:11:14 +0530

    updated code

--- .script/contentValidator.ts
+++ .script/contentValidator.ts
@@ -8,30 +8,39 @@ export async function ValidateFileContent(filePath: string): Promise<ExitCode>
     if (!filePath.includes("azure-pipelines"))
     {
         const fileContent = fs.readFileSync(filePath, "utf8");
-        const searchText = "Azure Sentinel"
+        const searchText = "Azure Sentinel";
 
+        // DEFINE SKIP TEXT TO BE SEARCHED
         const searchTargetProductAzureSentinelTag = '"targetProduct": "Azure Sentinel"';
-        const replaceTextTargetProductTag = /"targetProduct": "Azure Sentinel"/gi
-        const replaceAzureSentinelCommunityGithubText = /Azure Sentinel Community Github/gi;
+        const searchAzureSentinelCommunityGithubText = 'Azure Sentinel Community Github';
 
+        // SEARCH & CHECK IF SKIP TEXT EXIST IN THE FILE
         const hasTargetProductAzureSentinel = fileContent.includes(searchTargetProductAzureSentinelTag);
-        var replacedFileContent = fileContent.replace(replaceTextTargetProductTag, "");
-        replacedFileContent = fileContent.replace(replaceAzureSentinelCommunityGithubText, "");
-        const hasAzureSentinelText = replacedFileContent.toLowerCase().includes(searchText.toLowerCase());
+        const hasAzureSentinelCommunityGithubText = fileContent.includes(searchAzureSentinelCommunityGithubText);
 
+        // REPLACE ALL SKIP TEXT WITH BLANK
+        const skipText = [searchAzureSentinelCommunityGithubText, searchTargetProductAzureSentinelTag];
+        let replacedFileContent = fileContent.replace(new RegExp(skipText.join('|'), 'gi'), '');
+
+        // FIND IF AZURE SENTINEL TEXT PRESENT
+        let hasAzureSentinelText = replacedFileContent.toLowerCase().includes(searchText.toLowerCase());
         if (hasAzureSentinelText)
         {
-            if (hasTargetProductAzureSentinel)
-            {
-                throw new Error(`Please update text from 'Azure Sentinel' to 'Microsoft Sentinel' except 'targetProduct' key-value pair in file '${filePath}'`);
-            }
-            else
-            {
-                throw new Error(`Please update text from 'Azure Sentinel' to 'Microsoft Sentinel' in file '${filePath}'`);
-            }
+            // VALIDATE AND THROW ERROR
+            CustomizedException(hasTargetProductAzureSentinel, searchTargetProductAzureSentinelTag, searchText);
+            CustomizedException(hasAzureSentinelCommunityGithubText, searchAzureSentinelCommunityGithubText, searchText);
+
+            throw new Error(`Please update text from '${searchText}' to 'Microsoft Sentinel' in file '${filePath}'`);
         }
     }
     return ExitCode.SUCCESS;
+
+    function CustomizedException(hasSkipText: any, exceptText: any, searchText: any): void
+    {
+        if (hasSkipText) {
+            throw new Error(`Please update text from '${searchText}' to 'Microsoft Sentinel' except '${exceptText}' text in file '${filePath}'`);
+        }
+    }
 }
 
 let fileTypeSuffixes = ["json", "txt", "md", "yaml", "yml", "py"];
