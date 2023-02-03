5e02e1158fe9df6806067b7ad8d9cf8bd024bf70
NikTripathi <v-ntripathi@microsoft.com>
2023-02-03 19:37:54 +0530

    update

--- DataConnectors/AtlassianJiraAudit/JiraAuditAPISentinelConnector/__init__.py
+++ DataConnectors/AtlassianJiraAudit/JiraAuditAPISentinelConnector/__init__.py
@@ -10,6 +10,7 @@ import os
 import tempfile
 import logging
 import re
+from dateutil.parser import parse as parse_datetime
 from .state_manager import StateManager
 
 customer_id = os.environ['WorkspaceID'] 
