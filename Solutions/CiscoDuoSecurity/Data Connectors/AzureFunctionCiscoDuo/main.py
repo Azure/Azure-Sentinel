912f2cb80e995f068ab060c5e6fc1d1aa2e53308
v-rucdu <v-rucdu@microsoft.com>
2023-02-07 20:25:16 +0530

    Added logging and handled rate limit error

--- Solutions/CiscoDuoSecurity/Data Connectors/AzureFunctionCiscoDuo/main.py
+++ Solutions/CiscoDuoSecurity/Data Connectors/AzureFunctionCiscoDuo/main.py
@@ -145,8 +145,14 @@ def get_auth_logs(admin_api: duo_client.Admin, mintime: int, maxtime: int) -> It
             break
         logging.info('Making authentication logs request: next_offset={}'.format(next_offset))
         res = admin_api.get_authentication_log(api_version=2, mintime=mintime, maxtime=maxtime, limit=str(limit), sort='ts:asc', next_offset=next_offset)
-        events = res['authlogs']
-        logging.info('Obtained {} auth events'.format(len(events)))
+        logging.info('Response recieved {}'.format(res))
+        
+        if res.status_code == 429:
+            logging.info('429 exception occurred, trying retry after {} seconds'.format(int(res.headers["Retry-After"])))
+            time.sleep(int(res.headers["Retry-After"]))
+        else:
+            events = res['authlogs']
+            logging.info('Obtained {} auth events'.format(len(events)))
 
         for event in events:
             yield event
