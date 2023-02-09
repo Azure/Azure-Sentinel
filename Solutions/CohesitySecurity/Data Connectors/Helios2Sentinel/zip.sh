3eb7ffc7aae1590043f4cea4f8e72ceffa57aee9
yinghuang123 <115043385+yinghuang123@users.noreply.github.com>
2023-02-09 11:13:27 -0800

    Zip (#4)
    Add producer and consumer packaging scripts

---
+++ Solutions/CohesitySecurity/Data Connectors/Helios2Sentinel/zip.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# This script is used to publish the 2 function apps into 2 zip files, for later azure deployment.
+# no input needed, and it could be run from any where.
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+dotnet clean
+dotnet publish -c Release
+PUBLISH_DIR="./bin/Release/net6.0/publish/"
+
+find "$SCRIPTPATH"/ -type f -iname \*.csproj | while read ss; \
+do \
+    SCRIPT=$(realpath "$ss")
+    CSPROJDIR=$(dirname "$SCRIPT")
+    cd "$CSPROJDIR"
+
+    filename=$(basename -- "$ss")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    cd "$PUBLISH_DIR"
+    zip -r ./"$filename".zip ./*
+    mv -fv ./"$filename".zip "$CSPROJDIR/../Package/"
+done
