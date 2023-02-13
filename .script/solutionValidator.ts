5ccc0dc8f3409a4fd3a560d8297a8bb96fdf6d14
mkchiliveri <mkchiliveri@gmail.com>
2023-02-13 15:50:30 +0530

    added comments

--- .script/solutionValidator.ts
+++ .script/solutionValidator.ts
@@ -4,9 +4,11 @@ import { ExitCode } from "./utils/exitCode";
 import fs from "fs";
 import { MainTemplateValidationError } from "./utils/validationError";
 
+// initialize arrays to store valid domains and verticals
 let validDomains: string[] = [];
 let validVerticals: string[] = [];
 
+// read the valid domains and verticals from the JSON file
 try {
     const validDomainsVerticals = JSON.parse(fs.readFileSync('./.script/ValidDomainsVerticals.json', "utf8"));
     validDomains = validDomainsVerticals.validDomains;
@@ -15,37 +17,48 @@ try {
     logger.logError(`Error reading ValidDomainsVerticals.json file: ${error}`);
 }
 
+// function to check if the solution is valid
 export async function IsValidSolution(filePath: string): Promise<ExitCode> {
 
-
+    // check if the file is a mainTemplate.json file
     if (filePath.endsWith("mainTemplate.json")) {
+        // read the content of the file
         let jsonFile = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
+        // check if the file has a "resources" field
         if (!jsonFile.hasOwnProperty("resources")) {
             console.warn(`No "resources" field found in the file. Skipping file path: ${filePath}`);
             return ExitCode.SUCCESS;
         }
 
+        // get the resources from the file
         let resources = jsonFile.resources;
 
+        // filter resources that have type "Microsoft.OperationalInsights/workspaces/providers/metadata"
         const filteredResource = resources.filter(function (resource: { type: string; }) {
             return resource.type === "Microsoft.OperationalInsights/workspaces/providers/metadata";
         });
         if (filteredResource.length > 0) {
             filteredResource.forEach((element: { hasOwnProperty: (arg0: string) => boolean; properties: { hasOwnProperty: (arg0: string) => boolean; categories: any; }; }) => {
+                // check if the resource has a "properties" field
                 if (element.hasOwnProperty("properties") === true) {
+                    // check if the "properties" field has a "categories" field
                     if (element.properties.hasOwnProperty("categories") === true) {
                         const categories = element.properties.categories;
 
+                        // check if the categories have a "domains" field
                         if (categories.hasOwnProperty("domains")) {
                             let domains = categories.domains;
                             for (const domain of domains) {
+                                // check if the domain is valid
                                 if (!validDomains.includes(domain)) {
+                                    // throw error if the domain is not valid
                                     throw new MainTemplateValidationError(`Invald Domain ${domain} provided`);
                                 }
                             }
                         }
 
+                        // check if the categories have a "verticals" field
                         if (categories.hasOwnProperty("verticals")) {
                             let verticals = categories.verticals;
                             for (const vertical of verticals) {
@@ -59,11 +72,12 @@ export async function IsValidSolution(filePath: string): Promise<ExitCode> {
             });
         }
 
-    }
-    else {
+        // If the file is not identified as a main template, log a warning message
+    } else {
         console.warn(`Could not identify json file as a Main Template. Skipping File path: ${filePath}`);
     }
 
+    // Return success code after completion of the check
     return ExitCode.SUCCESS;
 }
 
@@ -71,19 +85,31 @@ export async function IsValidSolution(filePath: string): Promise<ExitCode> {
 
 
 
+// Array to store file type suffixes for the check
 let fileTypeSuffixes = ["json"];
+
+// Array to store file path folder prefixes for the check
 let filePathFolderPrefixes = ["Solutions"];
+
+// Array to store file kinds for the check
 let fileKinds = ["Added", "Modified"];
+
+// Options object to pass to the runCheckOverChangedFiles function
 let CheckOptions = {
+    // Callback function to check if a file is valid
     onCheckFile: (filePath: string) => {
         return IsValidSolution(filePath);
     },
+    // Callback function to handle errors during execution
     onExecError: async (e: any, filePath: string) => {
         console.log(`Main Template Validation Failed. File path: ${filePath}. Error message: ${e.message}`);
     },
+    // Callback function to handle final failure
     onFinalFailed: async () => {
         logger.logError("An error occurred, please open an issue");
     },
 };
 
+// Function call to start the check process
 runCheckOverChangedFiles(CheckOptions, fileKinds, fileTypeSuffixes, filePathFolderPrefixes);
+
