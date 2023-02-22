24c2da6c95a2603d28306eef3cb52721616fd0bf
mkchiliveri <mkchiliveri@gmail.com>
2023-02-22 15:43:26 +0530

    added pagination

--- Solutions/GoogleCloudPlatformIAM/Data Connectors/AzureFunctionGCPIAM/main.py
+++ Solutions/GoogleCloudPlatformIAM/Data Connectors/AzureFunctionGCPIAM/main.py
@@ -103,10 +103,13 @@ def main(mytimer: func.TimerRequest):
         timestamp>="{}"
     """.format(last_ts)
 
-# Get the list of resources and split them into batches
-    resources = get_recource_names()
+# Set the batch size and page size for pagination
     batch_size = 10
     page_size = 1000
+
+# Get the list of resources and split them into batches
+    resources = get_recource_names()
+
     resource_batches = [resources[i:i+batch_size] for i in range(0, len(resources), batch_size)]
     logging.info('Processing {} resource batches'.format(len(resource_batches)))
 
@@ -116,20 +119,35 @@ def main(mytimer: func.TimerRequest):
             resource_filter = ' OR '.join(['resource.type="{}" AND resource.labels.project_id="{}"'.format(resource_type, project_id) for resource_type, project_id in [resource.split('/') for resource in resource_batch]])
             batch_filt = '{} AND ({})'.format(filt, resource_filter)
             logging.info('Processing batch with filter: {}'.format(batch_filt))
-            for entry in gcp_cli.list_entries(filter_=batch_filt, order_by='timestamp', page_size=1000):
-                event = parse_entry(entry)
-                sentinel.send(event)
+             # Initialize the page token to None to start from the beginning
 
-                last_ts = event['timestamp']
-                if last_max_ts is None or last_ts > last_max_ts:
-                    last_max_ts= last_ts
+            page_token = None
+            while True:
+                # Call the list_entries() function with the page token and page size
+                results = gcp_cli.list_entries(filter_=batch_filt, order_by='timestamp', page_size=page_size, page_token=page_token)
 
-                if sentinel.is_empty():
-                    logging.info('Saving last max timestamp - {}'.format(last_max_ts))
-                    state_manager.post(last_max_ts)
-                    if check_if_script_runs_too_long(start_ts):
-                        logging.info('Script is running too long. Saving progress and exit.')
-                        break
+                # Iterate through the results and process each event
+                for entry in results:
+                    event = parse_entry(entry)
+                    sentinel.send(event)
+
+                    last_ts = event['timestamp']
+                    if last_max_ts is None or last_ts > last_max_ts:
+                        last_max_ts = last_ts
+
+                    if sentinel.is_empty():
+                        logging.info('Saving last max timestamp - {}'.format(last_max_ts))
+                        state_manager.post(last_max_ts)
+                        if check_if_script_runs_too_long(start_ts):
+                            logging.info('Script is running too long. Saving progress and exit.')
+                            break
+
+                # If the page token is None, there are no more pages to process
+                if not results.next_page_token:
+                    break
+
+                # Update the page token to the next page
+                page_token = results.next_page_token
 
             if sentinel.is_empty() and last_max_ts:
                 logging.info('Saving max last timestamp - {}'.format(last_max_ts))
