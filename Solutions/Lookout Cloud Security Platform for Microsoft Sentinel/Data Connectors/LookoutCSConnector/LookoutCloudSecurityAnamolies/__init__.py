8cf7c7a2a05a3f4dbc1a3f8219e4b1aa903eeb4d
Tichandr <tichandr@microsoft.com>
2023-03-15 18:20:09 +0530

    priniting msg changed

--- Solutions/Lookout Cloud Security Platform for Microsoft Sentinel/Data Connectors/LookoutCSConnector/LookoutCloudSecurityAnamolies/__init__.py
+++ Solutions/Lookout Cloud Security Platform for Microsoft Sentinel/Data Connectors/LookoutCSConnector/LookoutCloudSecurityAnamolies/__init__.py
@@ -285,11 +285,11 @@ def main(mytimer: func.TimerRequest) -> None:
     processes = []
     try:
         with ThreadPoolExecutor(max_workers=1) as executor:
-            futures = [executor.submit(ProcessData, x) for x in list(range(20))]
+            futures = [executor.submit(ProcessData, x) for x in list(range(26))]
             processes.append(futures)
         for future in as_completed(futures):
             print(future.result())
-            #logging.info("function result for thread: {} and it took --- {} seconds ---" .format(future.result()))
+            logging.info("{}" .format(future.result()))
         #print("Average time for all threads to run: {}".format(i/len(futures)))
         #print(i)
         print("End")
