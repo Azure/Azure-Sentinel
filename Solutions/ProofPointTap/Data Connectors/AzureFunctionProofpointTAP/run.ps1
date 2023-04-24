107b2f3e9bb10173201ac61a467ee42e2f83ffb2
v-rucdu <v-rucdu@microsoft.com>
2023-04-24 12:03:09 +0530

    Added output binding

--- Solutions/ProofPointTap/Data Connectors/AzureFunctionProofpointTAP/run.ps1
+++ Solutions/ProofPointTap/Data Connectors/AzureFunctionProofpointTAP/run.ps1
@@ -111,6 +111,8 @@ Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType)
     return $response.StatusCode
 
 }
+
+Write-Host("Powershell Output encoding is : " + $OutputEncoding)
 Write-Host ($response)
 
 # Iterate through the ProofPoint API response and if there are log events present, POST the events to the Log Analytics API into the respective tables.
