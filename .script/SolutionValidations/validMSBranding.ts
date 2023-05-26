62490d7601e07cc1d08a342d5126cdc0410237f7
mkchiliveri <mkchiliveri@gmail.com>
2023-05-26 15:26:18 +0530

    updated the code to show mutliple errors.

--- .script/SolutionValidations/validMSBranding.ts
+++ .script/SolutionValidations/validMSBranding.ts
@@ -3,28 +3,29 @@ import { ExitCode } from "../utils/exitCode";
 import fs from "fs";
 
 export function IsValidBrandingContent(filePath: string): ExitCode {
+    const errors: string[] = [];
 
     // check if the file is mainTemplate.json or createUiDefinition.json
     if (filePath.endsWith("mainTemplate.json") || filePath.endsWith("createUiDefinition.json")) {
         // read the content of the file
         let jsonFile = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-        // check if the file content contains the word "Sentinel" without "Microsoft" preceding it
+        // check if the file content contains " Sentinel" without being preceded by "Microsoft" and not part of a hyphenated word
         const fileContent = JSON.stringify(jsonFile);
-        const sentinelRegex = /(?<!Microsoft )Sentinel\b/g;
-        const sentinelMatches = fileContent.match(sentinelRegex);
-
-        if (sentinelMatches && sentinelMatches.length > 0) {
-            for (const match of sentinelMatches) {
-                if (match !== 'Microsoft Sentinel') {
-                    throw new InvalidFileContentError(`The string "${match}" is invalid. It should be "Microsoft Sentinel". File path: ${filePath}`);
-                }
-            }
+        const sentinelRegex = /(?<!Microsoft\s)(?<!-)\bSentinel\b/g;
+        let match;
+        while ((match = sentinelRegex.exec(fileContent))) {
+            errors.push(`The string 'Sentinel' is invalid. It should be preceded by 'Microsoft' at index ${match.index}`);
         }
 
         // If the file is not identified correctly, log a warning message
     } else {
         console.warn(`Could not identify JSON file as mainTemplate.json or createUiDefinition.json. Skipping. File path: ${filePath}`);
+    }
+
+    // Throw a single error with all the error messages concatenated
+    if (errors.length > 0) {
+        throw new InvalidFileContentError(errors.join("\n"));
     }
 
     // Return success code after completion of the check
