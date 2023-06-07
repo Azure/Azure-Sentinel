32c7b41cb9cde4d590ff494e052b110f688ffafd
mkchiliveri <mkchiliveri@gmail.com>
2023-06-07 17:39:29 +0530

    testing

--- .script/SolutionValidations/solutionValidator.ts
+++ .script/SolutionValidations/solutionValidator.ts
@@ -13,7 +13,7 @@ import { MainTemplateDomainVerticalValidationError, MainTemplateSupportObjectVal
 export async function IsValidSolution(filePath: string): Promise<ExitCode> {
     IsValidSolutionDomainsVerticals(filePath);
     IsValidSupportObject(filePath);
-    IsValidBrandingContent(filePath);
+    //IsValidBrandingContent(filePath);
     IsValidSolutionID(filePath);
     return ExitCode.SUCCESS;
 }
