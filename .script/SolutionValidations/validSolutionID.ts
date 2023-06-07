4508320dcac586aaeee49f2e19d58e04c8f037bf
mkchiliveri <mkchiliveri@gmail.com>
2023-06-07 19:49:18 +0530

    testing

--- .script/SolutionValidations/validSolutionID.ts
+++ .script/SolutionValidations/validSolutionID.ts
@@ -14,17 +14,22 @@ export function IsValidSolutionID(filePath: string): ExitCode {
     // Get the 'variables' section from the JSON
     const variables = jsonContent.variables;
 
-    // Check if 'solutionId' variable is present
-    if (variables && variables.solutionId) {
+    // Check if 'solutionId' attribute is present
+    if (variables && "solutionId" in variables) {
         const solutionId = variables.solutionId;
+
+        // Validate if the solution ID is empty
+        if (!solutionId) {
+            throw new InvalidSolutionIDValidationError(`Empty solution ID. Expected format: publisherID.solutionID and Solution ID must be in lowercase. Found empty value.`);
+        }
 
         // Validate the solution ID format
         const regex = /^[a-z0-9]+\.[a-z0-9_]+$/;
         if (!regex.test(solutionId)) {
-            throw new InvalidSolutionIDValidationError(`Invalid solution ID format. Expected format: publisherID.solutionID and it must be in lowercase. Found: ${solutionId}`);
+            throw new InvalidSolutionIDValidationError(`Invalid solution ID format. Expected format: publisherID.solutionID and  Solution ID must be in lowercase. Found: ${solutionId}`);
         }
     } else {
-        throw new InvalidSolutionIDValidationError(`'solutionId' variable not found in the file. File path: ${filePath}`);
+        throw new InvalidSolutionIDValidationError(`Missing 'solutionId' attribute in the file. File path: ${filePath}`);
     }
 
     // Return success code after completion of the check
