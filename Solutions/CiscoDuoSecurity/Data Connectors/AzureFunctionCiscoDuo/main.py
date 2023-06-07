48acc925ba5aef08d45a3dd1a1b06a34d3c922cf
Meena Kumari Chatla <v-mchatla@microsoft.com>
2023-06-07 14:08:00 +0530

    Updated max value to avoid TimeOut Issue
    Updated max value to avoid TimeOut Issue

--- Solutions/CiscoDuoSecurity/Data Connectors/AzureFunctionCiscoDuo/main.py
+++ Solutions/CiscoDuoSecurity/Data Connectors/AzureFunctionCiscoDuo/main.py
@@ -93,6 +93,9 @@ def process_trust_monitor_events(admin_api: duo_client.Admin, state_manager: Sta
         mintime = int(time.time() - 86400) * 1000
 
     maxtime = int(time.time() - 120) * 1000
+    diff = maxtime - mintime
+    if(diff > 3600000):
+        maxtime = (int(mintime) + 3600) * 1000
 
     logging.info('Making trust_monitor logs request: mintime={}, maxtime={}'.format(mintime, maxtime))
     for event in admin_api.get_trust_monitor_events_iterator(mintime=mintime, maxtime=maxtime):
@@ -114,11 +117,11 @@ def process_auth_logs(admin_api: duo_client.Admin, state_manager: StateManager, 
     else:
         logging.info('Last timestamp is not known. Getting data for last 24h')
         mintime = int(time.time() - 86400) * 1000
-        diff = maxtime - mintime
-        if(diff < 3600):
-            maxtime = int(mintime) + 3600
-        else:
-            maxtime = int(time.time() - 120) * 1000
+
+    maxtime = int(time.time() - 120) * 1000
+    diff = maxtime - mintime
+    if(diff > 3600000):
+        maxtime = (int(mintime) + 3600) * 1000
 
     for event in get_auth_logs(admin_api, mintime, maxtime):
         sentinel.send(event)
