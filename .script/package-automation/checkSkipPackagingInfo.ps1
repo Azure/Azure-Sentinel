4a943df096051d387574e741e3d8c8954c9f17d0
v-amolpatil <v-amolpatil@microsoft.com>
2023-07-05 18:36:53 +0530

    Updated code and added appinsights code

--- .script/package-automation/checkSkipPackagingInfo.ps1
+++ .script/package-automation/checkSkipPackagingInfo.ps1
@@ -1,8 +1,13 @@
 
-param ($solutionName, $pullRequestNumber, $runId, $baseFolderPath)
+param ($solutionName, $pullRequestNumber, $runId, $baseFolderPath, $instrumentationKey)
+. ./Tools/Create-Azure-Sentinel-Solution/common/LogAppInsights.ps1
 
 try 
 {
+    $customProperties = @{ 'RunId'="$runId"; 'PullRequestNumber'= "$pullRequestNumber"; "EventName"="CheckPackagingSkipStatus"; }
+    Send-AppInsightsEventTelemetry -InstrumentationKey $instrumentationKey -EventName "CheckPackagingSkipStatus" -CustomProperties $customProperties
+    Send-AppInsightsTraceTelemetry -InstrumentationKey $instrumentationKey -Message "Execution for CheckPackagingSkipStatus started, Job Run Id : $runId" -Severity Information -CustomProperties $customProperties
+
     $filesList = git ls-files | Where-Object { $_ -like "Solutions/$solutionName/data/*" } | Where-Object { $_ -match ([regex]::Escape(".json")) } | Where-Object { $_ -notlike 'parameters.json' }
 
     Write-Host "Files List $filesList"
@@ -24,6 +29,9 @@ try
         if ($hasCreatePackageAttribute -eq $true -and $isCreatePackageSetToTrue -eq $false) {
             Write-Host "::warning::Skipping Package Creation for Solution '$solutionName', as Data File has attribute 'createPackage' set to False!"
             Write-Output "isPackagingRequired=$false" >> $env:GITHUB_OUTPUT
+
+            $customProperties['isPackagingRequired'] = $false
+            Send-AppInsightsTraceTelemetry -InstrumentationKey $instrumentationKey -Message "Execution for CheckPackagingSkipStatus started, Job Run Id : $runId" -Severity Information -CustomProperties $customProperties
         }
         else
         {
@@ -37,17 +45,22 @@ try
             {
                 # WE NEED PACKAGING
                 $isPackagingRequired = $true
+                $customProperties['isPackagingRequired'] =
                 Write-Output "isPackagingRequired=$true" >> $env:GITHUB_OUTPUT
             }
 
+            $customProperties['isPackagingRequired'] = $isPackagingRequired
             Write-Host "isPackagingRequired set to $isPackagingRequired"
             Write-Output "isPackagingRequired=$isPackagingRequired" >> $env:GITHUB_OUTPUT
 
+            Send-AppInsightsTraceTelemetry -InstrumentationKey $instrumentationKey -Message "CheckPackagingSkipStatus started, Job Run Id : $runId" -Severity Information -CustomProperties $customProperties
         }
     }
 }
 catch
 {
     Write-Output "isPackagingRequired=$true" >> $env:GITHUB_OUTPUT
+    
+    Send-AppInsightsExceptionTelemetry -InstrumentationKey $instrumentationKey -Exception $_.Exception -CustomProperties @{ 'RunId' = "$runId"; 'SolutionName' = "$solutionName"; 'PullRequestNumber' = "$pullRequestNumber"; 'ErrorDetails' = "CheckPackagingSkipStatus : Error occured in catch block: $_"; 'EventName' = "CheckPackagingSkipStatus"; }
     exit 1
 }
