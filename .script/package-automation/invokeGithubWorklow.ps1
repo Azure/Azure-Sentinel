5d76bff979cfeefef0a4dfdd99bd6a64b02ca60b
v-amolpatil <v-amolpatil@microsoft.com>
2023-07-11 18:47:39 +0530

    Updated code

---
+++ .script/package-automation/invokeGithubWorklow.ps1
@@ -0,0 +1,33 @@
+param($pullRequestBranchName, $pullRequestNumber)
+$header = @{
+    "Accept" = "application/vnd.github+json"
+}
+
+$githubToken = "$($env:GITHUB_TOKEN_VALUE)"
+
+$token = $githubToken | ConvertTo-SecureString -AsPlainText -Force
+$pullRequestNumberInt = [int]$pullRequestNumber
+$client_payload = @{
+    "command" = "ping"
+    "pullRequestBranchName" = "$pullRequestBranchName"
+    "pullRequestNumber" = $pullRequestNumberInt
+}
+
+$BodyJson = @{
+    "event_type" = "package-command"
+    "client_payload" = $client_payload
+} 
+
+$jsonBody = $BodyJson | ConvertTo-Json
+$Parameters = @{
+    Method      = "POST"
+    Uri         = "https://api.github.com/repos/v-amolpatil/packagingrepo/dispatches"
+    Headers     = $Header
+    ContentType = "application/json"
+    Body        = $jsonBody
+    Authentication = "Bearer"
+    Token = $token
+}
+
+$result = Invoke-RestMethod @Parameters
+Write-Host $result
