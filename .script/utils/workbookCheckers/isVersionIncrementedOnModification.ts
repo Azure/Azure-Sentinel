0a65fca4a51eeb1baf3d935b704fd7b71c37f577
mkchiliveri <mkchiliveri@gmail.com>
2023-07-20 14:58:31 +0530

    testing

--- .script/utils/workbookCheckers/isVersionIncrementedOnModification.ts
+++ .script/utils/workbookCheckers/isVersionIncrementedOnModification.ts
@@ -19,33 +19,21 @@ export async function isVersionIncrementedOnModification(items: Array<WorkbookMe
 
   if(pr){ // pr may return undefined
       const changedFiles = await GetDiffFiles(fileKinds, fileTypeSuffixes, filePathFolderPrefixes);
-      console.log(`Changed files: ${changedFiles}`);
     if(changedFiles && changedFiles.length > 0){
       const options = [pr.targetBranch, pr.sourceBranch, gitDiffFileFullContentOption, `${workbooksDirectoryPath}/WorkbooksMetadata.json`];
         const diffSummary = await git.diff(options);
-        console.log(`Diff summary: ${diffSummary}`);
         const diffLinesArray = diffSummary.split('\n').map(l => l.trim());
-        console.log(`Diff lines array: ${diffLinesArray}`);
         const versionChanges = extractVersionChangesByWorkbook(diffLinesArray);
-        console.log(`Version changes: ${versionChanges}`);
 
       items
       .filter((workbookMetadata: WorkbookMetadata) => changedFiles.includes(`${workbooksDirectoryPath}/${workbookMetadata.templateRelativePath}`))
       .forEach((workbookMetadata: WorkbookMetadata) => {
           const templateRelativePath = workbookMetadata.templateRelativePath;
-          console.log(`Template relative path: ${templateRelativePath}`);
           if (versionChanges[templateRelativePath] == null) {
-              //console.log(`Version changes in the if: ${versionChanges[templateRelativePath]}`);
-              //console.log('new version in the if block: ' + versionChanges[templateRelativePath]['newVersion']);
-              //console.log('old version in the if block: ' + versionChanges[templateRelativePath]['oldVersion']);
           // If the workbook has changed but the version was not updated (a matching key was not found in the versionChanges dictionary) - throw error
           throw new WorkbookValidationError(`The workbook ${workbookMetadata.templateRelativePath} has been modified but the version has not been incremented in the ${workbooksDirectoryPath}/WorkbooksMetadata.json file.`);
         }
         else {
-            //console.log(`Version changes in the else: ${versionChanges[templateRelativePath]}`);
-            //console.log('new version in the else block: ' + versionChanges[templateRelativePath]['newVersion']);
-            //console.log('old version in the else block: ' + versionChanges[templateRelativePath]['oldVersion']);
-
           const isNewVersionGreaterThanOldVersion = versionChanges[templateRelativePath]["newVersion"] > versionChanges[templateRelativePath]["oldVersion"];
 
           if(!isNewVersionGreaterThanOldVersion){ // If the version was updated but the new version is not greater than old version - throw error
@@ -80,17 +68,14 @@ function extractVersionChangesByWorkbook(diffLines: string[]) {
 
                 if (line.trim().startsWith('"templateRelativePath":')) {
                     templateRelativePath = line.split(':')[1].trim().replace(replaceQuotesRegex, "").replace(',', "");
-                    console.log("Template relative path: " + templateRelativePath);
                 }
 
                 if ((line.trim().startsWith('+') || line.trim().startsWith('-')) && line.includes('"version":')) {
                     const version = line.split(':')[1].trim().replace(replaceQuotesRegex, "").replace(',', "");
                     if (line.trim().startsWith('+')) {
                         newVersion = version;
-                        console.log("New version: " + newVersion);
                     } else {
                         oldVersion = version;
-                        console.log("Old version: " + oldVersion);
                     }
                 }
 
@@ -98,11 +83,6 @@ function extractVersionChangesByWorkbook(diffLines: string[]) {
             }
 
             if (templateRelativePath && newVersion && oldVersion) {
-
-                console.log("template relative path at assingment " + templateRelativePath);
-                console.log("new version at assingment " + newVersion);
-                console.log("oldVersion at assingment " + oldVersion);
-
                 workbookVersionChanges[templateRelativePath] = { "newVersion": newVersion, "oldVersion": oldVersion };
             }
         }
