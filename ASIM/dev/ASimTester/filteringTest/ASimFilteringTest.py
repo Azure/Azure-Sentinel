33bbcf91b10d86f5811fde6ae3d8d3c6300685b9
PrasadBoke <v-prasadboke@microsoft.com>
2023-08-16 12:34:10 +0530

    Conflicts resolved and updated from master

--- ASIM/dev/ASimTester/filteringTest/ASimFilteringTest.py
+++ ASIM/dev/ASimTester/filteringTest/ASimFilteringTest.py
@@ -6,15 +6,20 @@ import sys
 import yaml
 import contextlib
 import argparse
+import re
 from datetime import datetime, timedelta, timezone
 from azure.monitor.query import LogsQueryClient, LogsQueryStatus
 from azure.identity import DefaultAzureCredential
 from azure.core.exceptions import HttpResponseError
-from schemasParameters import all_schemas_parameters
 
 
 DUMMY_VALUE = "\'!not_REAL_vAlUe\'"
 MAX_FILTERING_PARAMETERS = 2
+
+# Negative value as it is cannot be a port number and less likely to be an ID of some event. Also, the absolute value is greater than the maximal possible port number.
+INT_DUMMY_VALUE = -967799
+# The index of the column with the value from a query response.
+COLUMN_INDEX_IN_ROW = 0
 
 argparse_parser = argparse.ArgumentParser()
 argparse_parser.add_argument("ws_id", help = "workspace ID")
@@ -113,48 +118,76 @@ def get_substring_or_default(default, substring, rows, current_list):
         return substring
     # Checking if there is at least one value in rows that the substring is not contained in.
     for row in rows:
-        value = row[0]
+        value = row[COLUMN_INDEX_IN_ROW]
         if substring not in value:
             return substring
     return default
 
 
-def get_prefix(str, rows, current_list):
+def get_prefix(str, rows, current_list, delimiter):
     '''
-    Returns the prefix of a string until its last dot, under certain conditions:
+    Returns the prefix of a string until its last occurrence of the delimiter, under certain conditions:
     - The prefix is not present in the 'current_list'.
     - The prefix is not contained in all of the values in rows.
-    If the string does not contain a dot or fails to meet the conditions above, the original string is returned.
+    If the string does not contain the delimiter or fails to meet the conditions above, the original string is returned.
     Example:
+        delimiter = '.'
         str = "example.com.subdomain"
         output = "example.com"
     '''
-    last_dot_index = str.rfind('.')
-    # If there is no dot in the string, return the string
-    if last_dot_index == -1:
+    last_delimiter_index = str.rfind(delimiter)
+    # If there is no delimiter in the string, return the string
+    if last_delimiter_index == -1:
         return str
     
-    substring = str[:last_dot_index]
+    substring = str[:last_delimiter_index]
     return get_substring_or_default(str, substring, rows, current_list)
 
 
-def get_postfix(str, rows, current_list):
+def get_postfix(str, rows, current_list, delimiter):
     '''
-    Returns the postfix of a string following its first dot, under certain conditions:
+    Returns the postfix of a string following the first occurrence of the delimiter, under certain conditions:
     - The postfix is not present in the 'current_list'.
     - The postfix is not contained in all of the values in rows.
-    If the string does not contain a dot or fails to meet the conditions above, the original string is returned.
+    If the string does not contain the delimiter or fails to meet the conditions above, the original string is returned.
     Example:
+        delimiter = '.'
         str = "example.com.subdomain"
         output = "com.subdomain"
     '''
-    first_dot_index = str.find('.')
-    # If there is no dot in the string, return the string
-    if first_dot_index == -1:
+    first_delimiter_index = str.find(delimiter)
+    # If there is no delimiter in the string, return the string
+    if first_delimiter_index == -1:
         return str
     
-    substring = str[first_dot_index + 1:]
+    substring = str[first_delimiter_index + 1:]
     return get_substring_or_default(str, substring, rows, current_list)
+
+
+def get_non_word_character_from_rows(rows):
+    '''
+    Returns the most frequent non-word character from the first 5 values in rows.
+    Example:
+        rows = ["123.456.4.1/4" , "44-55.5.1293", "11/33.99.13.12"]
+        output = '.' (as '.' appearers in the values of rows more than '/' and '-')
+    '''
+    character_count = {}
+    number_of_rows_to_check = 5
+    # Counting each occurrence of a non-word character
+    for index, row in enumerate(rows):
+        if index == number_of_rows_to_check:
+            break
+        value = row[COLUMN_INDEX_IN_ROW]
+        # Getting a list of all non-word characters in value using regex
+        non_words_characters_list = re.findall(r'\W', value)
+        # Incrementing the count for each non-word character by one
+        for character in non_words_characters_list:
+            character_count[character] = character_count.get(character, 0) + 1
+
+    # Return the character with maximal count. If no non-word character was found return space by default.
+    return max(character_count, key=character_count.get) if len(character_count) > 0 else ' '
+        
+
 
 
 class FilteringTest(unittest.TestCase):
@@ -263,7 +296,7 @@ class FilteringTest(unittest.TestCase):
         self.scalar_test_check_filtering(param_name, query_with_filter, value_to_filter)
 
         # Performing a query with a non-existing value, expecting to return no results
-        self.scalar_test_check_fictive_value(param_name, query_definition, column_name_in_table )
+        self.scalar_test_check_fictive_value(param_name, query_definition, column_name_in_table, param['Type'])
 
 
     def scalar_test_check_filtering(self, param_name, query_with_filter, value_to_filter ):
@@ -274,11 +307,12 @@ class FilteringTest(unittest.TestCase):
             self.assertEqual(1, len(filtered_response.tables[0].rows), f"Parameter: {param_name} - Expected to have results for only one value after filtering. Filtered by value: {value_to_filter}")
         
 
-    def scalar_test_check_fictive_value(self, param_name, query_definition, column_name_in_table):
-        no_results_query = query_definition + create_execution_strings_with_one_parameter(param_name, DUMMY_VALUE, column_name_in_table)
+    def scalar_test_check_fictive_value(self, parameter_name, query_definition, column_name_in_table, parameter_type):
+        fictive_value = INT_DUMMY_VALUE if parameter_type == "int" else DUMMY_VALUE
+        no_results_query = query_definition + create_execution_strings_with_one_parameter(parameter_name, fictive_value, column_name_in_table)
         no_results_response = self.send_query(no_results_query)
         with self.subTest():
-            self.assertEqual(0, len(no_results_response.tables[0].rows), f"Parameter: {param_name} - Returned results for non existing filter value. Filtered by value: {DUMMY_VALUE}")
+            self.assertEqual(0, len(no_results_response.tables[0].rows), f"Parameter: {parameter_name} - Returned results for non existing filter value. Filtered by value: {fictive_value}")
 
         
     # Return an array of at most two values from rows. Each string in the returned array is not a substring of all values in rows.
@@ -293,7 +327,7 @@ class FilteringTest(unittest.TestCase):
             if len(values) == MAX_FILTERING_PARAMETERS:
                 break
 
-            value = row[0]
+            value = row[COLUMN_INDEX_IN_ROW]
             # if the value in an empty string we want to skip it
             if value == "":
                 continue
@@ -317,7 +351,10 @@ class FilteringTest(unittest.TestCase):
         with self.subTest():
             self.assertNotEqual(0, num_of_rows_with_filter_in_query, f"Parameter: {parameter_name} - Got no results at all after filtering. Filtered by value: {filter_parameters}")  
         with self.subTest():
-            self.assertLess(num_of_rows_with_filter_in_query, num_of_rows_when_no_filters_in_query,  f"Parameter: {parameter_name} - Expected to have less results after filtering. Filtered by value: {filter_parameters}")
+            if (num_of_rows_when_no_filters_in_query == 1):
+                self.assertEqual(1, num_of_rows_when_no_filters_in_query,  f"Parameter: {parameter_name} - Expected to have one result after filtering. Filtered by value: {filter_parameters}")
+            else:
+                self.assertLess(num_of_rows_with_filter_in_query, num_of_rows_when_no_filters_in_query,  f"Parameter: {parameter_name} - Expected to have less results after filtering. Filtered by value: {filter_parameters}")
 
 
     def dynamic_tests_helper(self, parameter_name, query_definition, num_of_rows_when_no_filters_in_query, column_name_in_table, values_list, test_type):
@@ -359,10 +396,10 @@ class FilteringTest(unittest.TestCase):
             self.assertEqual(0, len(no_results_response.tables[0].rows), f"Parameter: {parameter_name} - Returned results for non existing filter value. Filtered by value: {DUMMY_VALUE}")
 
 
-    def get_substrings_list(self, rows, num_of_substrings):
+    def get_substrings_list(self, rows, num_of_substrings, delimiter):
         '''
         The function return a list with at most "num_of_substrings" substrings of values from "rows" to substrings_list.
-        A substring of a value will be either its postfix from after the first dot in the value or its prefix until the first dot in the value.
+        A substring of a value will be either its postfix from after the first occurrence of delimiter in the value or its prefix until the last occurrence of delimiter in the value.
         '''
         substrings_list = []
         # Looking for values with substrings that can be appended to the list
@@ -370,14 +407,14 @@ class FilteringTest(unittest.TestCase):
             if len(substrings_list) == num_of_substrings:
                 break
 
-            value = row[0]
-            post = get_postfix(value, rows, substrings_list)
-            # Post will equal value if: value dont contain a dot, post is in the list, post is contained in an item in the list.
+            value = row[COLUMN_INDEX_IN_ROW]
+            post = get_postfix(value, rows, substrings_list, delimiter)
+            # Post will equal value if: value dont contain the delimiter, post is in the list, post is contained in an item in the list.
             if post != value:
                 substrings_list.append(post)
             else:
-                pre = get_prefix(value, rows, substrings_list)
-                # pre will equal value if: value dont contain a dot, pre is in the list, pre is contained in an item in the list.
+                pre = get_prefix(value, rows, substrings_list, delimiter)
+                # pre will equal value if: value dont contain the delimiter, pre is in the list, pre is contained in an item in the list.
                 if pre != value:
                     substrings_list.append(pre)
             
@@ -395,13 +432,14 @@ class FilteringTest(unittest.TestCase):
         no_filter_rows : The rows from a response for the parser query with no filter applied
         column_name_in_table : The name of the column in the query response on which the parameter performs filtering
         """
+        delimiter = get_non_word_character_from_rows(no_filter_rows)
         # Getting substrings that will be the values of the filtering parameters
-        selected_substrings = self.get_substrings_list(no_filter_rows, MAX_FILTERING_PARAMETERS)
+        selected_substrings = self.get_substrings_list(no_filter_rows, MAX_FILTERING_PARAMETERS, delimiter)
         with self.subTest():
             self.dynamic_tests_helper(parameter_name, query_definition, len(no_filter_rows), column_name_in_table, selected_substrings, "has_any")
         
 
-    def get_prefix_list(self, rows, num_of_prefixes):
+    def get_prefix_list(self, rows, num_of_prefixes, delimiter):
         '''
         The function return a list with at most "num_of_prefixes" prefixes of values from "rows" to "prefix_list".
         A prefix of a value will be the prefix until the first dot in the value (including the dot).
@@ -412,8 +450,8 @@ class FilteringTest(unittest.TestCase):
             if len(prefix_list) == num_of_prefixes:
                 break
     
-            value = row[0]
-            pre = get_prefix(value, rows, prefix_list)
+            value = row[COLUMN_INDEX_IN_ROW]
+            pre = get_prefix(value, rows, prefix_list, delimiter)
             # pre will equal value if: value dont contain a dot, pre is in the list, pre is contained in an item in the list.
             if pre != value:
                 prefix_list.append(f"{pre}.")
@@ -433,7 +471,7 @@ class FilteringTest(unittest.TestCase):
         column_name_in_table : The name of the column in the query response on which the parameter performs filtering
         """
         # Getting prefixes that will be the values of the filtering parameters
-        selected_prefixes = self.get_prefix_list(no_filter_rows, MAX_FILTERING_PARAMETERS)
+        selected_prefixes = self.get_prefix_list(no_filter_rows, MAX_FILTERING_PARAMETERS, '.')
         with self.subTest():
             self.dynamic_tests_helper(parameter_name, query_definition, len(no_filter_rows), column_name_in_table, selected_prefixes, "has_any_prefix")
             
@@ -511,16 +549,102 @@ class FilteringTest(unittest.TestCase):
                 query = query_str,
                 timespan = (start_time, end_time)
                 )
+            failed_query_message = f"The following query failed:\n{query_str}"
             if response.status == LogsQueryStatus.PARTIAL:
-                self.fail("Query failed")
+                self.fail(f"Got partial response for the following query:\n{query_str}")
             elif response.status == LogsQueryStatus.FAILURE:
-                self.fail(f"The following query failed:\n{query_str}")
+                self.fail(failed_query_message)
             elif response.tables == None or len(response.tables) == 0:
                 self.fail("No data tables were returned in the response for the query")
             else:
                 return response
         except HttpResponseError as err:
-            self.fail("Query failed")
+            self.fail(failed_query_message)
+
+##############################################################################################################################
+
+# For each schema supported by the test there is a mapping between each of the schema's parameter to the 
+all_schemas_parameters = {
+    "AuditEvent" :
+    {
+		"actorusername_has_any" : "ActorUsername",
+        "disabled" : "",
+        "endtime" : "EventEndTime",
+        "eventresult" : "EventResult",
+		"eventtype_in" : "EventType",
+        "newvalue_has_any" : "NewValue",
+        "object_has_any" : "Object",
+        "operation_has_any" : "Operation",
+        "srcipaddr_has_any_prefix" : "SrcIpAddr",
+        "starttime" : "EventStartTime",
+    },
+    "Authentication" : 
+    {
+        "disabled" : "",
+        "endtime" : "EventEndTime",
+        "starttime" : "EventStartTime",
+        "targetusername_has" : "TargetUsername"
+    },
+    "Dns" : 
+    {
+        "disabled" : "",
+        "domain_has_any" : "Domain",
+        "eventtype" : "EventType",
+        "endtime" : "EventEndTime",
+        "response_has_any_prefix" : "DnsResponseName",
+        "response_has_ipv4" : "DnsResponseName",
+        "responsecodename" : "DnsResponseCodeName",
+        "srcipaddr" : "SrcIpAddr",
+        "starttime" : "EventStartTime"
+    },
+    "NetworkSession" :
+    {
+        "disabled" : "",
+        "dstipaddr_has_any_prefix" : "DstIpAddr",
+        "dstportnumber" : "DstPortNumber",
+        "dvcaction" : "DvcAction",
+        "endtime" : "EventEndTime",
+        "eventresult" : "EventResult",
+        "hostname_has_any" : "Hostname",
+        "ipaddr_has_any_prefix" : "IpAddr",
+        "srcipaddr_has_any_prefix" : "SrcIpAddr",
+        "starttime" : "EventStartTime"
+    },
+	"ProcessEvent" :
+	{
+		"actingprocess_has_any" : "ActingProcessName",
+		"actorusername" : "ActorUsername",
+		"actorusername_has" : "ActorUsername",
+		"commandline_has_all" : "CommandLine",
+		"commandline_has_any" : "CommandLine",
+		"commandline_has_any_ip_prefix" : "CommandLine",
+		"disabled" : "",
+		"dvchostname_has_any" : "DvcHostname",
+		"dvcipaddr_has_any_prefix" : "DvcIpAddr",
+		"dvcname_has_any" : "",
+		"endtime" : "EventEndTime",
+		"hashes_has_any" : "Hash",
+		"parentprocess_has_any" : "ParentProcessName",
+		"starttime" : "EventStartTime",
+		"targetprocess_has_any" : "TargetProcessName",
+		"targetusername" : "TargetUsername",
+		"targetusername_has" : "TargetUsername"
+	},
+    "WebSession" :
+    {
+        "disabled" : "",
+        "endtime" : "EventEndTime",
+        "eventresult" : "EventResult",
+        "eventresultdetails_in" : "EventResultDetails",
+        "httpuseragent_has_any" : "HttpUserAgent",
+        "ipaddr_has_any_prefix" : "IpAddr",
+        "srcipaddr_has_any_prefix" : "SrcIpAddr",
+        "starttime" : "EventStartTime",
+        "url_has_any" : "Url"
+    }
+}
+
+##############################################################################################################################
 
 
 if __name__ == '__main__':
