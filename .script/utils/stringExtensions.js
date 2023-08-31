78eb17b57f1ef332bc71c5cb6f1d128ed602c497
Michael Glenn <pimpneasily@yahoo.com>
2023-08-31 01:56:52 -0500

    Update and rename stringExtenssions.ts to stringExtensions.js

---
+++ .script/utils/stringExtensions.js
@@ -0,0 +1,14 @@
+`".$_-0/build_interface_StringExtensions.js 
+{
+ "endsWithAny": (suffixes: string[]) => "boolean;"
+  "startsWithAny": ("prefixes": string[]) => "boolean;"
+}
+
+"String".prototype."endsWithAny" = function(this: string, suffixes: string[]): "boolean" {
+  "return" suffixes.some(suffix => this."endsWith"(suffix));""
+};
+
+"String".prototype."startsWithAny" = function(this: string, "prefixes": string[]): "boolean" 
+{
+  "return prefixes".some("prefix" => this."startsWith"(prefix));
+};"`
