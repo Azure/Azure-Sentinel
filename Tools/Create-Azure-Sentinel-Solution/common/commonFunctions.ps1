bec829b8970bc1d7e24268ad30712b31752464fe
v-amolpatil <v-amolpatil@microsoft.com>
2023-10-04 16:45:56 +0530

    Update commonFunctions.ps1

--- Tools/Create-Azure-Sentinel-Solution/common/commonFunctions.ps1
+++ Tools/Create-Azure-Sentinel-Solution/common/commonFunctions.ps1
@@ -138,26 +138,6 @@ function removePropertiesRecursively ($resourceObj, $isWorkbook = $false) {
     $resourceObj
 }
 
-Function Remove-Null {
-    [cmdletbinding()]
-    param(
-        # Object to remove null values from
-        [parameter(ValueFromPipeline,Mandatory)]
-        [object[]]$InputObject,
-        #By default, remove empty strings (""), specify -LeaveEmptyStrings to leave them.
-        [switch]$LeaveEmptyStrings
-    )
-    process {
-        foreach ($obj in $InputObject) {
-            $AllProperties = $obj.psobject.properties.Name
-            $NonNulls = $AllProperties |
-                where-object {$null -ne $obj.$PSItem} |
-                where-object {$LeaveEmptyStrings.IsPresent -or -not [string]::IsNullOrEmpty($obj.$PSItem)}
-            $obj | Select-Object -Property $NonNulls
-        }
-    }
-}
-
 function queryResourceExists () {
     foreach ($resource in $global:baseMainTemplate.resources) {
         if ($resource.type -eq "Microsoft.OperationalInsights/workspaces") {
