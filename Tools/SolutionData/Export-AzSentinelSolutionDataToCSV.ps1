55542aef7639dd0c592cd6823e3dc07b2db15c1a
Gary Bushey <garybushey@gmail.com>
2023-10-09 14:06:54 -0400

    Uploaded SolutionData project
    Created a new Tools folder called "SolutionData" and added all the code to  create a new CSV file that contains all the solution data which can be used in watchlist, workbook, or PowerBI

---
+++ Tools/SolutionData/Export-AzSentinelSolutionDataToCSV.ps1
@@ -0,0 +1,130 @@
+
+#requires -version 6.2
+<#
+.SYNOPSIS
+
+This command will generate a CSV file containing all the MS Sentinel solution information
+    .DESCRIPTION
+        Based on the parameters used, this command will eitehr generate a CSV file containing the information about all the Microsoft Sentinel
+        MITRE tactics and techniques being used, or a listing of the rules that use the tactics and techniques
+    .PARAMETER WorkspaceName
+        Enter the Log Analytics workspace name, this is a required parameter
+    .PARAMETER ResourceGroupName
+        Enter the Log Analytics workspace name, this is a required parameter
+    .PARAMETER FileName
+        Enter the file name to use.  Defaults to "mitrerules" and ".csv" will be appended to all FileNames that do not already include it
+   
+    .NOTES
+        AUTHOR= Gary Bushey
+        LASTEDIT= 30 Sept 2022
+    .EXAMPLE
+        Export-AzSentineMITREIncidentsToCSV -WorkspaceName "WorkspaceName" -ResourceGroupName "rgname"
+        In this example you will get the file named "mitrerules.csv" generated containing the count of the active rule's MITRE information
+    .EXAMPLE
+        Export-AzSentineMITREIncidentsToCSV -WorkspaceName "WorkspaceName" -ResourceGroupName "rgname" -FileName "test"
+        In this example you will get the file named "test.csv" generated containing  the count of the active rule's MITRE information
+#>
+
+[CmdletBinding()]
+param (
+  ## The name of the workspace.  Required
+  [Parameter(Mandatory = $true)]
+  [string]$WorkspaceName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$ResourceGroupName,
+
+  [string]$FileName = "solutionexport.csv"
+)
+
+Add-Type -AssemblyName System.Collections
+
+$outputObject = New-Object system.Data.DataTable
+[void]$outputObject.Columns.Add('SolutionName', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('SolutionType', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('SolutionDescription', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('ResourceType', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('ResourceName', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('RequiredDataConnectors', [string]::empty.GetType() )
+[void]$outputObject.Columns.Add('RequiredDataTypes', [string]::empty.GetType() )
+
+
+Function Export-AzSentinelSolutionDataToCSV ($WorkspaceName, $ResourceGroupName, $FileName) {
+  if (! $FileName.EndsWith(".csv")) {
+    $FileName += ".csv"
+  }
+
+  #Setup the Authentication header needed for the REST calls
+  $context = Get-AzContext
+  $userProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+  $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($userProfile)
+  $token = $profileClient.AcquireAccessToken($context.Subscription.TenantId)
+  $authHeader = @{
+    'Content-Type'  = 'application/json' 
+    'Authorization' = 'Bearer ' + $token.AccessToken 
+  }
+    
+  $subscriptionId = (Get-AzContext).Subscription.Id
+  $url = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourceGroups/$($ResourceGroupName)/providers/Microsoft.OperationalInsights/workspaces/$($WorkspaceName)/providers/Microsoft.SecurityInsights/contentProductPackages?api-version=2023-04-01-preview"
+
+  $solutions = (Invoke-RestMethod -Method "Get" -Uri $url -Headers $authHeader ).value
+  $count = 1
+  $total = $solutions.count
+
+
+  foreach ($solution in $solutions) {
+    Write-Host $count " of " $total ": " $solution.properties.displayName
+    $count = $count + 1
+    <# if ($count -eq 10)
+    {
+      break;
+    } #>
+    $solutionUrl = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourceGroups/$($ResourceGroupName)/providers/Microsoft.OperationalInsights/workspaces/$($WorkspaceName)/providers/Microsoft.SecurityInsights/contentProductPackages/" + $solution.name + "?api-version=2023-04-01-preview"
+    $solutionData = (Invoke-RestMethod -Method "Get" -Uri $solutionUrl -Headers $authHeader )
+    $requiredDataConnectors = ""
+    $requiredDataTypes="";
+    #Solution
+    if ($solution.properties.contentKind -eq "Solution") {
+      foreach ($resource in $solutionData.properties.packagedContent.resources) {
+        if (($null -ne $resource.properties.contentKind) -and ("Solution" -ne $resource.properties.contentKind)) {
+          $newRow = $outputObject.NewRow()
+          $newRow.SolutionName = $solution.properties.displayName
+          $newRow.SolutionType = $solution.properties.contentKind
+          $newRow.SolutionDescription = $solution.properties.descriptionHtml
+          $newRow.ResourceType = $resource.properties.contentKind
+          $newRow.ResourceName = $resource.properties.displayName
+          if ($null -ne $resource.properties.mainTemplate.resources.properties.requiredDataConnectors.connectorId)
+          { 
+            $requiredDataConnectors = [String]::Join('|', $resource.properties.mainTemplate.resources.properties.requiredDataConnectors.connectorId) 
+            $requiredDataTypes = [String]::Join('|', $resource.properties.mainTemplate.resources.properties.requiredDataConnectors.dataTypes) 
+          }
+          $newRow.RequiredDataConnectors = $requiredDataConnectors
+          $newRow.RequiredDataTypes = $requiredDataConnectors
+          [void]$outputObject.Rows.Add( $newRow )
+        }
+      }
+    }
+    #StandAlone
+    else {
+      $resourceType = $solutionData.properties.packagedContent.resources[0].properties.contentKind
+      $resourceName = $solutionData.properties.packagedContent.resources[1].properties.displayName
+      $newRow = $outputObject.NewRow()
+      $newRow.SolutionName = $solution.properties.displayName
+      $newRow.SolutionType = $solution.properties.contentKind
+      $newRow.SolutionDescription = $solution.properties.description
+      $newRow.ResourceType = $resourceType
+      $newRow.ResourceName = $resourceName
+      $newRow.RequiredDataConnectors = $requiredDataConnectors
+      $newRow.RequiredDataTypes = $requiredDataConnectors
+      [void]$outputObject.Rows.Add( $newRow )
+    }
+  }
+
+  $outputObject |  Export-Csv -QuoteFields "SolutionName", "SolutionDescription", "ResourceName" -Path $FileName -Append
+  $outputObject.Clear()
+}
+
+
+
+#Execute the code
+Export-AzSentinelSolutionDataToCSV $WorkspaceName $ResourceGroupName $FileName 
---
+++ Tools/SolutionData/README.md
@@ -0,0 +1,18 @@
+# SolutionData
+Create a CSV to show solution information.  Includes PowerBI and Workbook to show data
+
+To Run:
+
+ Export-AzSentineMITREIncidentsToCSV -WorkspaceName "WorkspaceName" -ResourceGroupName "rgname"
+
+        In this example you will get the file named "mitrerules.csv" generated containing the count of the active rule's MITRE information
+
+Export-AzSentineMITREIncidentsToCSV -WorkspaceName "WorkspaceName" -ResourceGroupName "rgname" -FileName "test"
+
+        In this example you will get the file named "test.csv" generated containing  the count of the active rule's MITRE information
+
+There is an included "solutionexport.csv" file that serves as an example of the output.  You can upload this into a Microsoft Sentinel watchlist and then use the included workbook to view it.
+
+> Note:  Due to the size of the CSV file, it must first be uploaded into an Azure Blog storage account and then used to create the watchlist. [Create Large Watchlists using SAS Key](https://techcommunity.microsoft.com/t5/microsoft-sentinel-blog/large-watchlist-using-sas-key-is-in-public-preview)
+
+There can be an issue where the CSV file failed to work correctly when creating the watchlist.  In that case, open it in Excel and save it again.
