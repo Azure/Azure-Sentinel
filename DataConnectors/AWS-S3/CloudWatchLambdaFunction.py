ec0342e027b73df0b224cdd1d09374f19fdf83e9
Sreedhar Ande <andesreedhar@gmail.com>
2023-11-03 13:37:19 -0700

    Getting all loggroups and streamnames

--- DataConnectors/AWS-S3/CloudWatchLambdaFunction.py
+++ DataConnectors/AWS-S3/CloudWatchLambdaFunction.py
@@ -12,8 +12,6 @@ s3 = boto3.resource('s3')
 
 
 # Please set the following parameters:
-LOG_GROUP_NAME = os.environ['LOG_GROUP_NAME'] # Please enter log group name
-LOG_STREAM_NAME = os.environ['LOG_STREAM_NAME'] # Please enter log stream name
 BUCKET_NAME = os.environ['BUCKET_NAME'] # Please enter bucket name
 BUCKET_PREFIX = os.environ['BUCKET_PREFIX'] # Please enter bucket prefix that ends with '/' , if no such, leave empty
 OUTPUT_FILE_NAME = os.environ['OUTPUT_FILE_NAME'] # Please change to desired name
@@ -29,36 +27,49 @@ def lambda_handler(event, context):
     unix_start_time = int(time.mktime(START_TIME_UTC.timetuple()))*1000
     unix_end_time = int(time.mktime(END_TIME_UTC.timetuple()))*1000
     try:
-        # Gets objects from cloud watch
-        response = logs.get_log_events(
-            logGroupName=LOG_GROUP_NAME,
-            logStreamName=LOG_STREAM_NAME,
-            startTime=unix_start_time,
-            endTime=unix_end_time,
-        )
-        
-        # Convert events to json object
-        json_string = json.dumps(response)
-        json_object = json.loads(json_string)
-        
-        df = pd.DataFrame(json_object['events'])
-        if df.empty:
-            print('No events for specified time')
-            return None
-        
-        # Convert unix time to zulu time for example from 1671086934783 to 2022-12-15T06:48:54.783Z
-        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%dT%H:%M:%S.%f').str[:-3]+'Z'
-        
-        # Remove unnecessary column
-        fileToS3 = df.drop(columns=["ingestionTime"])
+        # List log groups
+        log_groups = logs.describe_log_groups()
+        log_groups_dict = {}
 
-        # Export data to temporary file in the right format, which will be deleted as soon as the session ends
-        fileToS3.to_csv( f'/tmp/{OUTPUT_FILE_NAME}.gz', index=False, header=False, compression='gzip', sep = ' ', escapechar=' ',  doublequote=False, quoting=csv.QUOTE_NONE)
+        for log_group in log_groups['logGroups']:
+            LOG_GROUP_NAME = log_group['logGroupName']
+            # List log streams for each log group
+            log_streams = logs.describe_log_streams(logGroupName=LOG_GROUP_NAME)
+            for log_stream in log_streams['logStreams']:
+                LOG_STREAM_NAME = log_stream['logStreamName']
+                log_groups_dict[LOG_GROUP_NAME] = LOG_STREAM_NAME
         
-        # Upload data to desired folder in bucket
-        s3.Bucket(BUCKET_NAME).upload_file(f'/tmp/{OUTPUT_FILE_NAME}.gz', f'{BUCKET_PREFIX}{OUTPUT_FILE_NAME}.gz')
+        # Iterate through log_groups_dict
+        for key in log_groups_dict:            
+            # Gets objects from cloud watch
+            response = logs.get_log_events(
+                logGroupName = key.LOG_GROUP_NAME,
+                logStreamName = key.LOG_STREAM_NAME,
+                startTime=unix_start_time,
+                endTime=unix_end_time,
+            )
+            
+            # Convert events to json object
+            json_string = json.dumps(response)
+            json_object = json.loads(json_string)
+            
+            df = pd.DataFrame(json_object['events'])
+            if df.empty:
+                print('No events for specified time')
+                return None
+            
+            # Convert unix time to zulu time for example from 1671086934783 to 2022-12-15T06:48:54.783Z
+            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+            df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%dT%H:%M:%S.%f').str[:-3]+'Z'
+            
+            # Remove unnecessary column
+            fileToS3 = df.drop(columns=["ingestionTime"])
 
+            # Export data to temporary file in the right format, which will be deleted as soon as the session ends
+            fileToS3.to_csv( f'/tmp/{OUTPUT_FILE_NAME}.gz', index=False, header=False, compression='gzip', sep = ' ', escapechar=' ',  doublequote=False, quoting=csv.QUOTE_NONE)
+            
+            # Upload data to desired folder in bucket
+            s3.Bucket(BUCKET_NAME).upload_file(f'/tmp/{OUTPUT_FILE_NAME}.gz', f'{BUCKET_PREFIX}{OUTPUT_FILE_NAME}.gz')
     except Exception as e:
         print("    Error exporting %s: %s" % (LOG_GROUP_NAME, getattr(e, 'message', repr(e))))
 
