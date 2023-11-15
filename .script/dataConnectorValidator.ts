cda2e5417c050d96a282b6a146eb36cb88dd7046
v-atulyadav <104008048+v-atulyadav@users.noreply.github.com>
2023-11-15 15:58:55 +0530

    Update dataConnectorValidator.ts

--- .script/dataConnectorValidator.ts
+++ .script/dataConnectorValidator.ts
@@ -124,10 +124,6 @@ function getConnectorCategory(dataTypes : any, instructionSteps:[])
   {
     return ConnectorCategory.CorelightConnectorExporter;
   }
-  else if (dataTypes[0].name.includes("SecurityAlert"))
-  {
-    return ConnectorCategory.SecurityAlert;
-  }
   return "";
 } 
 let fileTypeSuffixes = ["json"];
