054585cc0b3a2c6bc2c5b7ee52e1a50de8e4cd05
AzureKid <rogierdijkman@hotmail.com>
2023-11-16 22:31:22 +0100

    initial checkin

---
+++ Solutions/1Password/Data Connectors/1Password/Modules/HelperFunctions/HelperFunctions.psm1
@@ -0,0 +1,401 @@
+Function Get-AuditLogs {
+    <#
+    .SYNOPSIS
+        Retrieves audit logs from 1Password API.
+
+    .DESCRIPTION
+        This function retrieves audit logs from 1Password API using the provided parameters. It can retrieve logs starting from a specific time or using a cursor value. The retrieved logs are then processed to rename reserved Microsoft Sentinel column names.
+
+    .PARAMETER lastRunTime
+        Specifies the start time to retrieve audit logs from. If not provided, the function retrieves all available logs.
+
+    .PARAMETER cursor
+        Specifies the cursor value to retrieve audit logs from. If not provided, the function retrieves logs starting from the provided lastRunTime.
+
+    .PARAMETER api
+        Specifies the API endpoint to retrieve audit logs from.
+
+    .EXAMPLE
+        Get-AuditLogs -lastRunTime "2022-01-01T00:00:00Z" -api "https://my-1password-api.com"
+
+        This example retrieves all audit logs starting from January 1st, 2022 from the specified 1Password API endpoint.
+
+    .NOTES
+        Author: Rogier Dijkman
+    #>
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$lastRunTime,
+
+        [Parameter(Mandatory = $false)]
+        [string]$cursor,
+
+        [Parameter(Mandatory = $false)]
+        [string]$api
+    )
+
+    $result = @()
+
+    $headers = @{
+        'Authorization' = "Bearer $env:BearerToken"
+        'ContentType'   = 'Application/Json'
+    }
+
+    if ($cursor) {
+        $payload = @{
+            'cursor' = $cursor
+        }
+    }
+    else {
+        $payload = @{
+            'start_time' = $lastRunTime
+            'limit'      = 100
+        }
+    }
+
+    try {
+        $uri = "$($env:apiEndpoint)/api/v1/$api"
+        Do {
+            $apiResponse = (Invoke-RestMethod -Method POST -Uri $uri -Headers $headers -body ($payload | ConvertTo-Json))
+            $results += $apiResponse.items
+
+            $payload = @{
+                "cursor" = $apiResponse.cursor
+            }
+        } Until ($apiResponse.has_more -eq $false)
+
+        # rename reserved Microsoft Sentinel column names [uuid and type]
+        if ($results.uuid) {
+            $results | ForEach-Object {
+                $_ | add-member "uuid_s" -NotePropertyValue $_."uuid"
+                $_.psobject.properties.remove("uuid")
+            }
+        }
+
+        if ($results.type) {
+            $results | ForEach-Object {
+                $_ | add-member "action_type" -NotePropertyValue $_."type"
+                $_.psobject.properties.remove("type")
+            }
+        }
+        Write-Host "Results found: $($results.count)"
+    }
+    catch {
+        Write-Warning "Unable to connect to API [$($env:apiEndpoint)]"
+    }
+    Set-Cursor -cursor $api -cursorValue $apiResponse.cursor @storagePayload
+
+    return $results
+}
+
+Function Set-Cursor {
+    <#
+    .SYNOPSIS
+        Sets the cursor value for a given cursor.
+    .DESCRIPTION
+        This function sets the cursor value for a given cursor. The cursor value is saved in a JSON file in the temp folder and then uploaded to Azure Blob Storage.
+    .PARAMETER cursor
+        The name of the cursor to set.
+    .PARAMETER cursorValue
+        The value to set the cursor to.
+    .PARAMETER AzureWebJobsStorage
+        The connection string for the Azure Blob Storage account.
+    .PARAMETER storageAccountContainer
+        The name of the container in the Azure Blob Storage account to store the cursor file in.
+    .EXAMPLE
+        Set-Cursor -cursor 'myCursor' -cursorValue '12345' -AzureWebJobsStorage $AzureWebJobsStorage -storageAccountContainer 'myContainer'
+    #>
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$cursor,
+
+        [Parameter(Mandatory = $true)]
+        [string]$cursorValue,
+
+        [Parameter(Mandatory = $true)]
+        [string]$AzureWebJobsStorage,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountContainer
+    )
+
+    $body = @{
+        "cursor" = $cursorValue
+    }
+
+    $body | ConvertTo-Json | Out-File "$env:temp\$cursor.json"
+
+    try {
+        Write-Verbose "Selecting Storage Context"
+        $storageAccountContext = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+    }
+    catch {
+        return 'Unable to connect to Storage Context'
+    }
+
+    Write-Verbose 'Saving new cursor'
+    try {
+        $null = Set-AzStorageBlobContent `
+            -Blob "$cursor.json" `
+            -Container $storageAccountContainer `
+            -Context $storageAccountContext `
+            -File "$env:temp\$cursor.json" `
+            -Force
+    }
+    catch {
+        return "Unable to create new $cursor"
+    }
+}
+
+Function Get-Cursor {
+    <#
+    .SYNOPSIS
+        Retrieves the cursor value from a JSON file stored in Azure Blob Storage.
+
+    .DESCRIPTION
+        This function retrieves the cursor value from a JSON file stored in Azure Blob Storage. The cursor value is used to keep track of the last processed event in the 1Password data connector.
+
+    .PARAMETER AzureWebJobsStorage
+        The connection string for the Azure Blob Storage account.
+
+    .PARAMETER storageAccountContainer
+        The name of the container in the Azure Blob Storage account where the JSON file is stored.
+
+    .PARAMETER cursor
+        The name of the JSON file containing the cursor value.
+
+    .EXAMPLE
+        Get-Cursor -AzureWebJobsStorage $AzureWebJobsStorage -storageAccountContainer "1password" -cursor "1password_cursor"
+
+    .NOTES
+        Author: Rogier Dijkman
+    #>
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$AzureWebJobsStorage,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountContainer,
+
+        [Parameter(Mandatory = $true)]
+        [string]$cursor
+    )
+
+    $storageAccountContext = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+
+    try {
+        Write-Verbose "Get Blob Context"
+        $blobContext = Get-AzStorageBlob `
+            -Blob "$cursor.json" `
+            -Container $storageAccountContainer `
+            -Context $storageAccountContext
+    }
+    catch {
+        Write-Output "Unable to access [$cursor.json]"
+    }
+
+    if (![string]::IsNullOrEmpty($blobContext)) {
+        Write-Verbose "Get Blob File"
+        try {
+            $null = Get-AzStorageBlobContent `
+                -Blob "$cursor.json" `
+                -Container $storageAccountContainer `
+                -Context $storageAccountContext `
+                -Destination "$env:temp\$cursor.json" `
+                -Force
+
+            Write-Verbose "Get File Content"
+            $lastRunAuditContext = Get-Content "$env:temp\$cursor.json" | ConvertFrom-Json
+            if ($null -ne $lastRunAuditContext) {
+                $cursorValue = ($lastRunAuditContext.cursor)
+                return $cursorValue
+            }
+            else {
+                Write-Warning "[!] No context was found for cursor [$cursor]"
+                $timestamp = Set-TimeStamp -AzureWebJobsStorage $AzureWebJobsStorage -storageAccountContainer $storageAccountContainer
+            }
+        }
+        catch {
+            Write-Warning "[! An error has occured fetching the cursor for '$cursor']"
+        }
+    }
+}
+
+Function Send-Data {
+    <#
+    .SYNOPSIS
+    Sends data to a specified endpoint using an Azure access token.
+
+    .DESCRIPTION
+    This function sends data to a specified endpoint using an Azure access token. The access token is obtained using the Get-AzAccessToken cmdlet.
+
+    .PARAMETER body
+    The data to be sent to the endpoint.
+
+    .EXAMPLE
+    $body = @{
+        "key1" = "value1"
+        "key2" = "value2"
+    }
+    Send-Data -body $body
+
+    .NOTES
+    This function requires the Get-AzAccessToken cmdlet to be installed. It also requires the $env:dataCollectionEndpoint environment variable to be set to the desired endpoint URL.
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$body
+    )
+
+    $uri = "$env:dataCollectionEndpoint"
+
+    $token = Get-AzAccessToken -ResourceUrl https://monitor.azure.com
+
+    $requestHeader = @{
+        "Token"          = ($token.token | ConvertTo-SecureString -AsPlainText -Force)
+        "Authentication" = 'OAuth'
+        "Method"         = 'POST'
+        "ContentType"    = 'application/json'
+    }
+
+    try {
+        Invoke-RestMethod -Uri "$uri" -Body $body @requestHeader
+    } catch {
+        Write-Warning "Unable to sent data. Validate if the account '$($token.UserId)' has Access to the Data Collection Rule"
+    }
+
+}
+
+Function Set-TimeStamp {
+    <#
+    .SYNOPSIS
+        Sets a timestamp for the last run of a function and saves it to Azure Storage Blob.
+    .DESCRIPTION
+        This function sets a timestamp for the last run of a function and saves it to Azure Storage Blob. If no previous timestamp is provided, the current timestamp is used. The timestamp is saved as a JSON file in the temp folder and then uploaded to the specified Azure Storage Blob container.
+    .PARAMETER lastRun
+        The timestamp of the last run of the function. If not provided, the current timestamp is used.
+    .PARAMETER AzureWebJobsStorage
+        The connection string for the Azure Storage Account.
+    .PARAMETER storageAccountContainer
+        The name of the Azure Storage Blob container where the timestamp file will be saved.
+    .EXAMPLE
+        Set-TimeStamp -AzureWebJobsStorage $AzureWebJobsStorage -storageAccountContainer $storageAccountContainer
+    #>
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$lastRun,
+
+        [Parameter(Mandatory = $true)]
+        [string]$AzureWebJobsStorage,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountContainer
+    )
+
+    if ([string]::IsNullOrEmpty($lastRun)) {
+        $lastRun = (Get-Date).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+    }
+
+    $lastRunAudit = @{
+        "lastRun" = $lastRun
+    }
+
+    $lastRunAudit | ConvertTo-Json | Out-File "$env:temp\timestamp.json"
+
+    try {
+        Write-Verbose "Selecting Storage Context"
+        $storageAccountContext = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+    }
+    catch {
+        return 'Unable to connect to Storage Context'
+    }
+
+    Write-Verbose 'Saving new timestamp'
+    try {
+        $null = Set-AzStorageBlobContent `
+            -Blob "timestamp.json" `
+            -Container $storageAccountContainer `
+            -Context $storageAccountContext `
+            -File "$env:temp\timestamp.json" `
+            -Force
+    }
+    catch {
+        return 'Unable to create new timestamp'
+    }
+    return $lastRun
+}
+
+Function Get-TimeStamp {
+    <#
+    .SYNOPSIS
+        This function retrieves the timestamp of the last run of a script from a JSON file stored in an Azure Storage Account.
+    .DESCRIPTION
+        This function retrieves the timestamp of the last run of a script from a JSON file stored in an Azure Storage Account. If the JSON file is not found or is empty, the function sets the timestamp to the current time and writes it to the JSON file.
+    .PARAMETER AzureWebJobsStorage
+        The connection string for the Azure Storage Account.
+    .PARAMETER storageAccountContainer
+        The name of the container in the Azure Storage Account where the JSON file is stored.
+    .EXAMPLE
+        Get-TimeStamp -AzureWebJobsStorage $AzureWebJobsStorage -storageAccountContainer $storageAccountContainer
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$AzureWebJobsStorage,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountContainer
+    )
+
+    $storageAccountContext = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+
+    $storageAccountContext = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+    $blobs = Get-AzStorageBlob -Container $storageAccountContainer -Context $storageAccountContext | Select Name
+    if ($null -eq $blobs) {
+        Set-TimeStamp @storagePayload
+    }
+
+    try {
+        Write-Verbose "Get Blob Context"
+        $blobContext = Get-AzStorageBlob `
+            -Blob "timestamp.json" `
+            -Container $storageAccountContainer `
+            -Context $storageAccountContext
+    }
+    catch {
+        Write-Host "Unable to access [timestamp.json]"
+        $timestamp = Set-TimeStamp @storagePayload
+    }
+
+    if (![string]::IsNullOrEmpty($blobContext)) {
+        Write-Verbose "Get Blob File"
+        try {
+            $null = Get-AzStorageBlobContent `
+                -Blob "timestamp.json" `
+                -Container $storageAccountContainer `
+                -Context $storageAccountContext `
+                -Destination "$env:temp\timestamp.json" `
+                -Force
+
+            Write-Verbose "Get File Content"
+            $lastRunAuditContext = Get-Content "$env:temp\timestamp.json" | ConvertFrom-Json
+            if ($null -ne $lastRunAuditContext) {
+                $timestamp = ($lastRunAuditContext.lastRun).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+            }
+            else {
+                $timestamp = Set-TimeStamp @storagePayload
+            }
+        }
+        catch {
+            $timestamp = Set-TimeStamp @storagePayload
+        }
+        return $timestamp
+    }
+}
+
+Function Get-Variables {
+    $global:storagePayload = @{
+        'AzureWebJobsStorage'     = $env:AzureWebJobsStorage
+        'storageAccountContainer' = "cursors"
+    }
+}
---
+++ Solutions/1Password/Data Connectors/1Password/function/run.ps1
@@ -0,0 +1,45 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Get the current universal time in the default string format.
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+
+$currentStartTime = (Get-Date).AddDays(-30).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+
+try {
+    Get-Variables
+} catch {
+    Write-Warning "Unable to load required module"
+    exit
+}
+$results = @()
+$lastRunTime = Get-TimeStamp @storagePayload
+
+$endpoints = @('auditevents', 'signinattempts', 'itemusages')
+
+foreach ($api in $endpoints) {
+    try {
+        $cursor = Get-Cursor @storagePayload -cursor $api
+        $result += Get-AuditLogs -cursor $cursor -api $api
+    } catch {
+        $result += Get-AuditLogs -lastRunTime $currentStartTime -api $api
+    }
+
+    if ($result.count -gt 1) {
+        Write-Host "Sending $($result.count) to the Data Collection Rule"
+        Send-Data -body ($result | ConvertTo-Json)
+        $updateTime = $true
+    } else {
+        Write-Host "No new data was found"
+    }
+}
+
+    if ($true -eq $updateTime) {
+        $null = Set-TimeStamp @storagePayload -lastRun $currentUTCtime
+    }
+
+    #clear the temp folder
+    Remove-Item $env:temp\* -Recurse -Force -ErrorAction SilentlyContinue
