78b1aaaa6bd761c9c9f4c58bc06d72b751640b3a
Murali Krishna Dev Uppugunduri <139563098+v-muuppugund@users.noreply.github.com>
2023-11-30 18:26:44 +0530

    Size in GB updates

--- Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/VMWareCloudProcessor/run.ps1
+++ Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/VMWareCloudProcessor/run.ps1
@@ -425,7 +425,9 @@ function  GetBucketDetails {
         while ($startTime -le $now) {
             $keyPrefix = "$prefixFolder/org_key=$OrgKey/year=$($startTime.Year)/month=$($startTime.Month)/day=$($startTime.Day)/hour=$($startTime.Hour)/minute=$($startTime.Minute)"
             #$keyPrefix="carbon-black-events/org_key=7DESJ9GN/year=2023/month=9/day=6/hour=13/minute=46"
-            
+            $TotalSizeinGB = 0
+            foreach ($items in (Get-S3Object -BucketName $s3BucketName -keyPrefix $keyPrefix | Select-Object Size)) { $TotalSizeinGB += $items.Size/1GB }
+            Write-Host "Bucket files size is $TotalSizeinGB under $s3BucketName and Prefix folder $keyPrefix ."
             Get-S3Object -BucketName $s3BucketName -keyPrefix $keyPrefix | Read-S3Object -Folder "C:\tmp"
             Write-Host "Files under $keyPrefix are downloaded."
 
