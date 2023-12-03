1b10a445b2d566e81c39c62b9cc20d500a90d1b8
v-amolpatil <v-amolpatil@microsoft.com>
2023-12-03 16:11:46 +0530

    testParameters and other file changes to exclude it

--- .script/package-automation/partnerCenterDeploy.ps1
+++ .script/package-automation/partnerCenterDeploy.ps1
@@ -63,7 +63,7 @@ try {
     $solutionFolderPath = 'Solutions/' + $solutionName + "/"
     $filesList = git ls-files | Where-Object { $_ -like "$solutionFolderPath*" }
 
-    $dataFolderFiles = $filesList | Where-Object { $_ -like "*/Data/*" } | Where-Object { $_ -notlike '*system_generated_metadata.json' }
+    $dataFolderFiles = $filesList | Where-Object { $_ -like "*/Data/*" } | Where-Object { $_ -notlike '*system_generated_metadata.json' } | Where-Object { $_ -notlike '*testParameters.json' }
 
     if ($dataFolderFiles.Count -gt 0) {
         $selectFirstdataFolderFile = $dataFolderFiles | Select-Object -first 1
