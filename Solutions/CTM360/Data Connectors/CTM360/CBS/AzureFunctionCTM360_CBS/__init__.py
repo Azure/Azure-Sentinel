9f82d3921b278760f53d4f1b413e200d498fccac
Sayed Ali <sayedali@ctm360.com>
2023-12-05 17:20:40 +0300

    update the cbs code

--- Solutions/CTM360/Data Connectors/CTM360/CBS/AzureFunctionCTM360_CBS/__init__.py
+++ Solutions/CTM360/Data Connectors/CTM360/CBS/AzureFunctionCTM360_CBS/__init__.py
@@ -9,20 +9,18 @@ import base64
 import requests
 import datetime
 import traceback
+import time
 from time import time, localtime, strftime, gmtime
-cbs_api_key = os.environ.get('api_key')
+HV_api_key = os.environ.get('api_key')
 customer_id = os.environ.get('WorkspaceID')
 shared_key = os.environ.get('WorkspaceKey')
 logAnalyticsUri = os.environ.get('logAnalyticsUri')
 olddate_from = os.environ.get('date_from')
 olddate_to = os.environ.get('date_to')
 backupflag = os.environ.get('backupflag')
-log_type = 'CBSLog_Azure_1_CL'
-
-logging.info(f"before main: {shared_key=}")
-logging.info(f"before main: {customer_id=}")
-logging.info(f"before main: {cbs_api_key=}")
-
+log_type = 'HackerViewLog_Azure_1_CL'
+MAX_RETRIES = 3
+RETRY_INTERVAL_SECONDS = 60
 
 if ((logAnalyticsUri in (None, '') or str(logAnalyticsUri).isspace())):
     logAnalyticsUri = 'https://' + customer_id + '.ods.opinsights.azure.com'
@@ -31,18 +29,15 @@ match = re.match(pattern, str(logAnalyticsUri))
 if (not match):
     raise Exception("CBS Data Connector: Invalid Log Analytics Uri.")
 
+bahrain_timezone_offset = 0
 
-# Set the timezone offset for Bahrain (in seconds)
-bahrain_timezone_offset = 3 * 60 * 60  # Adjust this value based on the actual offset
 
 def format_iso8601(timestamp):
-    return strftime("%Y-%m-%dT%H:%M:%S", gmtime(timestamp))
+    return strftime("%d-%m-%Y %H:%M", gmtime(timestamp))
 
 
 def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
-    logging.info(f"after main: {shared_key=}")
-    logging.info(f"after main: {customer_id=}")
-    logging.info(f"after main: {cbs_api_key=}")
+
     x_headers = 'x-ms-date:' + date
     string_to_hash = method + "\n" + \
         str(content_length) + "\n" + content_type + \
@@ -52,13 +47,54 @@ def build_signature(customer_id, shared_key, date, content_length, method, conte
     encoded_hash = base64.b64encode(hmac.new(
         decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
     authorization = "SharedKey {}:{}".format(customer_id, encoded_hash)
-    logging.info(f"after main: {authorization=}")
 
     return authorization
 
 
+def perform_request(url, headers):
+    for attempt in range(MAX_RETRIES):
+        try:
+            # Set a timeout for the request
+            response = requests.get(url, headers=headers, timeout=(5, 10))
+            response.raise_for_status()  # Raises HTTPError for bad responses
+
+            if response.status_code == 429:
+                # Log specific error for HTTP status code 429 (Too Many Requests)
+                logging.warning(
+                    "HTTP status code 429 (Too Many Requests). Retrying...")
+                time.sleep(RETRY_INTERVAL_SECONDS)
+            else:
+                message1 = response.json()
+                
+                post_data_to_sentinel(json.dumps(message1["issues"]))
+                return True  # Request successful, break out of the loop
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Request failed: {str(e)}")
+
+            if "Max retries exceeded with url" in str(e):
+                # Log specific error for Max retries exceeded with URL
+                logging.error("Max retries exceeded with the URL.")
+            elif "ReadTimeout" in str(e):
+                # Log specific error for ReadTimeout
+                logging.error("ReadTimeout occurred.")
+            elif "HTTPSConnectionPool" in str(e) or "ConnectionError" in str(e):
+                # Log specific error for ConnectionError
+                logging.error("ConnectionError: HTTPSConnectionPool.")
+            else:
+                # Log other types of exceptions
+                logging.error(f"Unhandled exception: {str(e)}")
+
+            if attempt < MAX_RETRIES - 1:
+                logging.info(
+                    f"Retrying in {RETRY_INTERVAL_SECONDS} seconds...")
+                time.sleep(RETRY_INTERVAL_SECONDS)
+            else:
+                logging.error(f"Max retries reached. Exiting.")
+                return False  # Max retries reached, break out of the loop
+
+
 def post_data_to_sentinel(body):
-    logging.info("entring post fucn")
 
     method = 'POST'
     content_type = 'application/json'
@@ -71,7 +107,6 @@ def post_data_to_sentinel(body):
 
     uri = logAnalyticsUri + resource + '?api-version=2016-04-01'
 
-    logging.info(uri)
     headers = {
         'content-type': content_type,
         'Authorization': signature,
@@ -79,13 +114,11 @@ def post_data_to_sentinel(body):
         'x-ms-date': rfc1123date
     }
 
-    logging.info(headers)
     try:
         response = requests.post(uri, data=body, headers=headers)
-    except Exception :
+        print(uri, headers, "hghg")
+    except Exception:
         print(traceback.format_exc())
-
-    logging.info(response.text)
 
     if (response.status_code >= 200 and response.status_code <= 299):
         logging.info(
@@ -97,85 +130,64 @@ def post_data_to_sentinel(body):
         return None
 
 
-
 def main(mytimer: func.TimerRequest, inputblob: func.InputStream, outputblob:  func.Out[str]) -> None:
     if mytimer.past_due:
         logging.warn('The timer is past due!')
 
-    logging.info('Starting program')
-        
-    # Get current time in seconds since the epoch
     current_time = time()
 
-    # Calculate date and time 5 minutes ago in the Bahrain timezone
     five_minutes_ago_bahrain = current_time - 5 * 60 + bahrain_timezone_offset
 
-    # Format the time objects as strings in ISO 8601-like format
     current_time_str = format_iso8601(current_time + bahrain_timezone_offset)
     five_minutes_ago_str = format_iso8601(five_minutes_ago_bahrain)
 
-    # Print the results
-    print("Current Date and Time in Bahrain timezone:", current_time_str)
-    print("Date and Time 5 Minutes Ago in Bahrain timezone:", five_minutes_ago_str)
     if backupflag == "true":
         if inputblob is not None:
             input1 = str(inputblob.read(), "utf-8")
             print(input1)
             if input1 == "false":
-                print("no old data insert")
                 date_from = olddate_from
-                date_to = olddate_to
-                url = f"https://cbs.ctm360.com/api/v2/incidents?date_from={date_from}&date_to={date_to}"
-                logging.info(url)
-                logging.info("old data not inserted 1st loop")
+                
+                url = f"https://hackerview.ctm360.com/api/v2/issues?first_seen={date_from}"
                 headers = {
                     "accept": "application/json",
-                    "api-key": cbs_api_key
+                    "api-key": HV_api_key
                 }
                 response = requests.get(url, headers=headers)
-                logging.info(response)
                 message1 = response.json()
-                logging.info(message1)
-          
-                post_data_to_sentinel(json.dumps(message1["incident_list"]))
-                outputblob.set("true")
-
-                    
+                print(message1)
+                if perform_request(url, headers):
+                    outputblob.set("true")
             else:
-                logging.info("old data alreay inserted")
                 date_from = five_minutes_ago_str
-                date_to = current_time_str
-                url = f"https://cbs.ctm360.com/api/v2/incidents?date_from={date_from}&date_to={date_to}"
-                logging.info(url)
-
+                
+                url = f"https://hackerview.ctm360.com/api/v2/issues?first_seen={date_from}"
+                print(url)
                 headers = {
                     "accept": "application/json",
-                    "api-key": cbs_api_key
+                    "api-key": HV_api_key
                 }
 
                 response = requests.get(url, headers=headers)
-
-
+                print(response.json())
                 message1 = response.json()
-                post_data_to_sentinel(json.dumps(message1["incident_list"]))
-
+                print(message1)
+                perform_request(url, headers)
 
         else:
             logging.error("no such file inide the blob")
             exit()
     else:
-        logging.info('take all data for the past 5 min')
         date_from = five_minutes_ago_str
-        date_to = current_time_str
-        url = f"https://cbs.ctm360.com/api/v2/incidents?date_from={date_from}&date_to={date_to}"
-        logging.info(url)
+        
+        url = f"https://hackerview.ctm360.com/api/v2/issues?first_seen={date_from}"
         headers = {
             "accept": "application/json",
-            "api-key": cbs_api_key
+            "api-key": HV_api_key
         }
 
         response = requests.get(url, headers=headers)
-
-
+        print(response.json())
         message1 = response.json()
-        post_data_to_sentinel(json.dumps(message1["incident_list"]))
+        print(message1)
+        perform_request(url, headers)
