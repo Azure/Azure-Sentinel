10703c51554bbe739fd422368725903f17b2a3d2
Murali Krishna Dev Uppugunduri <139563098+v-muuppugund@users.noreply.github.com>
2023-12-14 21:16:47 +0530

    updates

--- Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/S3ToMainQueue/run.ps1
+++ Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/S3ToMainQueue/run.ps1
@@ -46,6 +46,8 @@ General notes
 #>
 function GenerateDate()
 {
+    
+    $time= [int]$Env:timeInterval
     $startTime=getCheckpoint
     if($null -ne $startTime)
     {
@@ -57,11 +59,12 @@ function GenerateDate()
         Write-Host "The last start time in file share is" $startTime
     }
     else {
-        $startTime = [System.DateTime]::UtcNow.AddMinutes(-$(5))
+        $startTime = [System.DateTime]::UtcNow.AddMinutes(-$($time))
     }
 
     $now = [System.DateTime]::UtcNow
     #$Duration = New-TimeSpan -Start $startTime -End $now()
+
     if($startTime -le $now)
     {
         [int]$noofmins = $($now-$startTime).TotalMinutes
@@ -69,13 +72,13 @@ function GenerateDate()
     else {
         Write-Host "Start time is greater than current time,Please check the start time and correct it."
     }
-    if($noofmins -gt 5)
+    if($noofmins -gt $time)
     {
         if($null -ne $startTime)
         {
-          $now=$startTime.AddMinutes(5)
+          $now=$startTime.AddMinutes($time)
         }
-        Write-Host "The no of mins b/w start and end time is greater than 5"
+        Write-Host "The no of mins b/w start and end time is greater than $time"
     }
     Write-Host "The now time is" $now
 
@@ -126,10 +129,10 @@ function CarbonBlackS3Messages()
     {
         if ($SIEMapiKey -eq '<Optional>' -or  $SIEMapiId -eq '<Optional>'  -or [string]::IsNullOrWhitespace($SIEMapiKey) -or  [string]::IsNullOrWhitespace($SIEMapiId))
         {
-            $LogTypeArr = @("event")
+            $LogTypeArr = @("event","audit")
         }
         else{
-            $LogTypeArr = @("event","alertSIEMAPI")
+            $LogTypeArr = @("event","audit","alertSIEMAPI")
         }
     }else {
         if($logType -like "``[*``]")
@@ -148,8 +151,11 @@ function ProcessMessagesToQueue()
     {
         if($LogTypeArr -contains "event")
         {
+        
+            $time=[System.DateTime]::UtcNow
             # get queue count ---> if less than < 200 call below fn, if no, wait for 10sec and check again queue count size
             GetBucketFiles($EventprefixFolder)
+            Write-Host "Events are processsed in $(([System.DateTime]::UtcNow - $time).Seconds) Seconds"
         }
         else{
             Write-Warning "'Event' was not selected as a LogType, therefore event logs will not be ingested to the workspace."
@@ -161,10 +167,14 @@ function ProcessMessagesToQueue()
 
     if($LogTypeArr -contains "alertSIEMAPI" -or $LogTypeArr -contains "alertAWSS3")
     {
-         if(-not([string]::IsNullOrWhiteSpace($s3BucketName)) -and -not([string]::IsNullOrWhiteSpace($AWSAccessKeyId)) -and -not([string]::IsNullOrWhiteSpace($AWSSecretAccessKey)) -and -not([string]::IsNullOrWhiteSpace($OrgKey)))
+        
+            if(-not([string]::IsNullOrWhiteSpace($s3BucketName)) -and -not([string]::IsNullOrWhiteSpace($AWSAccessKeyId)) -and -not([string]::IsNullOrWhiteSpace($AWSSecretAccessKey)) -and -not([string]::IsNullOrWhiteSpace($OrgKey)))
             {
+                $time=[System.DateTime]::UtcNow
                 GetBucketFiles($AlertprefixFolder)
+                Write-Host "Alerts are processsed in $(([System.DateTime]::UtcNow - $time).Seconds) Seconds"
             }
+        
     }
     else {
         Write-Host "Input parameters are empty for alert logs"
@@ -277,6 +287,59 @@ function check_if_script_runs_too_long($percentage, $script_start_time)
  [double]$maxduration= $temp * 0.8
  return $duration -gt $maxduration
 }
+
+<#
+.SYNOPSIS
+This method used for posting to queue
+.DESCRIPTION
+Long description
+
+.PARAMETER message
+$message from convert json from object
+
+.EXAMPLE
+An example
+
+.NOTES
+General methods
+#>
+function CreateQueue($queueNameParameter)
+{
+   
+try
+{
+    if(-not([string]::IsNullOrWhiteSpace($carbonBlackStorage)) -and -not([string]::IsNullOrWhiteSpace($queueNameParameter)))
+    {
+        $ctx = New-AzStorageContext -ConnectionString $carbonBlackStorage
+        if ($null -ne $ctx)
+        {
+            
+            $queue = Get-AzStorageQueue –Name $queueNameParameter –Context $ctx
+            if(-not $queue)
+            {
+               #Creating the queue
+               $queue = New-AzStorageQueue –Name $queueNameParameter -Context $ctx  
+            }
+            else {
+                
+                #Queue already present
+            }         
+        }
+        else
+        {
+          Write-Host "Storage context not available"
+        }
+    }
+    else
+    {
+        Write-Host "Input parameters are empty"
+    }
+}
+catch {
+  Write-Error "Failed at CreateQueue with error message: $($_.Exception.Message)" -ErrorAction SilentlyContinue
+}
+
+}
 <#
 .SYNOPSIS
 #
@@ -294,11 +357,14 @@ function GetBucketFiles($prefixFolder)
 {
     IF ($Null -ne $s3BucketName) {
         Set-AWSCredentials -AccessKey $AWSAccessKeyId -SecretKey $AWSSecretAccessKey
+        $totalTime
+        [int]$totalQueueCount=0
+        $started=[System.DateTime]::UtcNow 
+        $time=[int]$Env:timeInterval
         while ($startTime -le $now) {
             $keyPrefix = "$prefixFolder/org_key=$OrgKey/year=$($startTime.Year)/month=$($startTime.Month)/day=$($startTime.Day)/hour=$($startTime.Hour)/minute=$($startTime.Minute)"
-            #$keyPrefix="carbon-black-events/org_key=7DESJ9GN/year=2023/month=12/day=6/hour=15/minute=15
-            #$keyPrefix="carbon-black-events/org_key=7DESJ9GN/year=2023/month=12/day=7/hour=6/minute=3"
             $paths=@()
+            $msgs=@()
             foreach ($items in (Get-S3Object -BucketName $s3BucketName -KeyPrefix $keyPrefix) | Select-Object Key ) 
             {
                 if($items.Key.Contains(".gz"))
@@ -312,7 +378,16 @@ function GetBucketFiles($prefixFolder)
                 }
                 if(("$($keyValuePairs[0])/org_key=$OrgKey/year=$($s3Dict["year"])/month=$($s3Dict["month"])/day=$($s3Dict["day"])/hour=$($s3Dict["hour"])/minute=$($s3Dict["minute"])") -eq $keyPrefix)
                 {
-                    $paths += $path
+                    $paths += $items.Key
+                    if($items.Key.Contains($EventprefixFolder))
+                    {
+                        $json = Convert-ToJSON -s3BucketName $s3BucketName -keyPrefix $items.Key -tableName $EventLogTable -logtype "event"
+                    }
+                    if($items.Key.Contains($AlertprefixFolder))
+                    {
+                        $json = Convert-ToJSON -s3BucketName $s3BucketName -keyPrefix $item -tableName $NotificationTable -logtype "alert"
+                    }
+                    $msgs+=$json
                 }
                 else {
                     Write-Host "Paths doesn't have gz files" $items.Key
@@ -320,34 +395,21 @@ function GetBucketFiles($prefixFolder)
                 }
            }
         $paths = $paths | sort -Unique
-        Write-Host $paths
-        foreach($item in $paths)
-        {
-          [int]$i=0
-          Write-Host "Number of files paths is: " $paths "Count: " $paths.Count
-          Write-Host "Paths from s3" $item "at start time" $startTime "now time" $now
-          if($item.Contains($EventprefixFolder))
-           {
-            $json = Convert-ToJSON -s3BucketName $s3BucketName -keyPrefix $item -tableName $EventLogTable -logtype "event"
-           }
-           if($item.Contains($AlertprefixFolder))
-            {
-              $json = Convert-ToJSON -s3BucketName $s3BucketName -keyPrefix $item -tableName $NotificationTable -logtype "alert"
-            }
-    
-            Write-Host "Main queue message has been posted" $json
-            CreateQueuePostMessageToQueue -message $json -queueN $queueName -i $i
-            if((check_if_script_runs_too_long -percentage 0.8 -script_start_time $script_start_time))
-            {
-                Write-Host "Script is running long"
-                return
-            }
-
+        $totalQueueCount+=$msgs.Count
+        $startQueue=[System.DateTime]::UtcNow 
+        try {
+            $queueCount = GetQueueCount
+            Write-Host "Queue Count in main queue is $($queueCount)" 
+            Push-OutputBinding -Name Msg -Value $msgs
+            Write-Host "Total time to process messages to queue under 1 min for $prefixFolder in seconds $(([System.DateTime]::UtcNow-$startQueue).seconds)" 
+        }
+        catch {
+            "Failed at Pushoutpubinding with error message: $($_.Exception.Message)"
         }
             $startTime = $startTime.AddMinutes(1)
-            Write-Host "Start time incremented by 1" $startTime
+            
     }
-
+    Write-Host "Total Messages pushed to queue under $time mins for $prefixFolder : $totalQueueCount" 
     }
 }
 <#
@@ -426,8 +488,10 @@ catch {
 
 $azstoragestring = $Env:WEBSITE_CONTENTAZUREFILECONNECTIONSTRING
 $queueName=$env:queueName
+$maxMainQueuemessages=$env:maxMainQueuemessages
 $carbonBlackStorage=$env:AzureWebJobsStorage
 $Context = New-AzStorageContext -ConnectionString $azstoragestring
+CreateQueue($queueName)
 $startTime,$now=GenerateDate
 $startTime
 $now
@@ -436,7 +500,9 @@ $now
 $sleepTime = 15
 Do
 {
+    
     $queueCount = GetQueueCount
+    Write-Host "The queue count before do while is $queueCount"
     if($queueCount -lt $maxMainQueuemessages)
     {
         try {
@@ -447,10 +513,15 @@ Do
                 Set-AzStorageBlobContent -file "$env:TEMP\lastlog.log" -Container (Get-AzStorageContainer -Name "lastlog" -Context $Context).Name -Context $Context -Force
             }
             else {
+            $blob=(Get-AzStorageContainer -Context $Context).Name -contains "lastlog"
+            if(-not $blob)
+            {
             $azStorageContainer = New-AzStorageContainer -Name "lastlog" -Context $Context
             $now | Get-Date -Format yyyy-MM-ddTHH:mm:ss | Out-File "$env:TEMP\lastlog.log"
             Set-AzStorageBlobContent -file "$env:TEMP\lastlog.log" -Container $azStorageContainer.name -Context $Context -Force
             }
+            }
+            Write-Host "The now time in file share is" $now
             break
         }
         catch {
@@ -460,7 +531,7 @@ Do
     Start-Sleep -s 15
     $sleepTime = $sleepTime + 15
 }
-while ($sleepTime -lt 271) {
+while ($sleepTime -lt 168) {
 }
 
 #This method posts the message to queue
--- Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/VMWareCloudProcessor/run.ps1
+++ Solutions/VMware Carbon Black Cloud/Data Connectors/AzureFunctionVMwareCarbonBlack/VMWareCloudProcessor/run.ps1
@@ -47,13 +47,11 @@ function ProcessBucketFiles ()
         {
             try {
                     Split-EventsAndProcess -alleventobjs $totalEvents -tableName $tableName -logType $logtype
-                    #ProcessData -alleventobjs $totalEvents -tableName $tableName -logtype $logtype -endTime $([DateTime]::UtcNow)
                     Write-Host "Pushed total no of events : $($totalEvents.Count)  to $($tableName) for file $($fileName)"
                 }
             catch {
                 Write-Error "Failed at PushDataToSentinel with error message: $($_.Exception.Message)" -ErrorAction SilentlyContinue
                 }
-            Write-Host("$($responseObj.count) new Carbon Black Events as of $([DateTime]::UtcNow). Pushed data to Azure sentinel Status code:$($status)")
             Write-Host "Successfully processed the carbon black files from AWS and FA instance took $(([System.DateTime]::UtcNow - $now).Seconds) seconds to process the data"
         }
         else {
@@ -330,7 +328,7 @@ function Split-EventsAndProcess($alleventobjs, $tableName, $logType) {
         try {
             $jsonPayload = $chunk | ConvertTo-Json -Depth 3
             $mbytes = [System.Text.Encoding]::UTF8.GetBytes($jsonPayload).Count / 1024 / 1024
-
+            Write-Host "total '$mbytes' MB for table name'$tableName' and log type '$logType'"
             if ($mbytes -le 30) {
                 $responseCode = Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($jsonPayload)) -logType $tableName
                 Write-Host "SUCCESS: $($chunk.Count) total '$logType' events posted to Log Analytics: $mbytes MB" -ForegroundColor Green
@@ -451,17 +449,6 @@ function  GetBucketDetails {
             $keyPrefix = Split-Path $prefixFolder
             Set-AWSCredentials -AccessKey $AWSAccessKeyId -SecretKey $AWSSecretAccessKey
             if($startTime -le $now) {
-                # $keyValuePairs = $prefixFolder -split '\\'
-                # $s3Dict = @{}
-                # foreach ($pair in $keyValuePairs) {
-                #     $key, $value = $pair -split '='
-                #     $s3Dict[$key] = $value
-                # }
-                #$keyPrefix = $prefixFolder
-                #"$($keyValuePairs[0])/org_key=$OrgKey/year=$($s3Dict["year"])/month=$($s3Dict["month"])/day=$($s3Dict["day"])/hour=$($s3Dict["hour"])/minute=$($s3Dict["minute"])/second=$($s3Dict["second"])"
-                #$keyPrefix = "carbon-black-events/org_key=$OrgKey/year=2023/month=12/day=5/hour=11/minute=55/second=01"
-                #$keyPrefix = "carbon-black-events/org_key=$OrgKey/year=2023/month=12/day=6/hour=15/minute=19/second=2"
-                #$obj = Get-S3Object -BucketName $s3BucketName -Key $prefixFolder
                 $obj = Get-S3Object -BucketName $s3BucketName -Key $prefixFolder
                 $obj | % {
                     if ($_.Size -gt 0)
@@ -515,11 +502,9 @@ function  GetBucketDetails {
                                 Write-Host "Error in reading file from tmp folder. S3File: $($loopItem.FullName), Error: $err"
                             }
                             finally {
-                                    #Remove-Item -LiteralPath $filename -Force -Recurse
                                     PushDataToSentinel -totalEvents $fileEvents -logtype $logtype -tableName $tableName -fileName $filename
                                 }
-                            #Write-Host "Data has been Pushed to Sentinel completed.  S3File: $($filename), S3Bucket: $($_.BucketName), No of Events Sent: $($fileEvents.Count), from AWS S3 successfully time in Seconds: $(([System.DateTime]::UtcNow - $time).Seconds)"
-                        }
+                            }
                     catch {
                             $err = $_.Exception.Message
                             Write-Host "Error in downloading file from Tmp Folder. S3File: $($loopItem.FullName), S3Bucket: $($s3BucketName), Error: $err"
