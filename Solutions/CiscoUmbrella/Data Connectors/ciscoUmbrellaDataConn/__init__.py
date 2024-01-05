88256509fadf27a7729652224266a1c258fac6b1
Murali Krishna Dev Uppugunduri <139563098+v-muuppugund@users.noreply.github.com>
2024-01-05 19:21:40 +0530

    Reverted back the changes

--- Solutions/CiscoUmbrella/Data Connectors/ciscoUmbrellaDataConn/__init__.py
+++ Solutions/CiscoUmbrella/Data Connectors/ciscoUmbrellaDataConn/__init__.py
@@ -441,7 +441,7 @@ class UmbrellaClient:
                         'destinationPort': row[10],
                         'dataCenter': row[11],
                         'ruleId': row[12],
-                        'verdict': row[13]
+                        'action': row[13]
                     }
                 else:
                     event = {"message": convert_list_to_csv_line(row)}
@@ -467,9 +467,7 @@ class UmbrellaClient:
                         'destinationPort': row[10],
                         'dataCenter': row[11],
                         'ruleId': row[12],
-                        'verdict': row[13],
-                        "fqdn": row[28],
-                        "destinationlistid" : row[29]
+                        'verdict': row[13]
                     }
                 else:
                     event = {"message": convert_list_to_csv_line(row)}
