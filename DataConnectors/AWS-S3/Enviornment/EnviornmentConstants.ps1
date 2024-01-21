3bf47d80c0ef327b690c87289da4a7f7cf81913d
stavbella <stavbelladev@microsoft.com>
2024-01-21 15:40:23 +0200

    change zips

--- DataConnectors/AWS-S3/Enviornment/EnviornmentConstants.ps1
+++ DataConnectors/AWS-S3/Enviornment/EnviornmentConstants.ps1
@@ -1,3 +1,3 @@
 $script:SentinelClientId = '21f935c0-8092-4b62-a772-5a2afd714569'
 $script:SentinelTenantId = '33e01921-4d64-4f8c-a055-5bdaffd5e33d'
-$script:CloudEnv = 'Gov'
+$script:CloudEnv = 'Com'
