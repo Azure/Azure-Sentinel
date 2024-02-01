311b0010757214f98c3d3e2e138b62b2f8190b12
v-amolpatil <v-amolpatil@microsoft.com>
2024-02-01 16:38:22 +0530

    updated code

--- .script/package-automation/validateClassicAppInsights.ps1
+++ .script/package-automation/validateClassicAppInsights.ps1
@@ -3,6 +3,8 @@ param($runId, $pullRequestNumber, $instrumentationKey, $baseFolderPath)
 Write-Host "Inside of Validate Classic App Insights!"
 
 try {
+  $baseFolderPath = $baseFolderPath + "/"
+  $baseFolderPath = $baseFolderPath.replace("//", "/")
 
   function ReadFileContent($filePath) {
     try {
@@ -26,11 +28,12 @@ try {
     $appInsightResourceWithoutWorkspaceResourceList = @()
     foreach($file in $filesList) {
       $filePath = $baseFolderPath + "/" + $file;
+      $filePath = $filePath.replace("//", "/")
       Write-Host "File Path is $filePath"
       $fileHasAppInsightsComponentType = Select-String -Path "$filePath" -Pattern '"type": "Microsoft.Insights/components"', '"type":"Microsoft.Insights/components"'
 
       if ($null -ne $fileHasAppInsightsComponentType) {
-        $fileContent = Select-String -Path "$filePath"
+        $fileContent = ReadFileContent -filePath "$filePath"
         $objFileContent = $fileContent | ConvertFrom-Json 
         $appInsightComponentObject = $objFileContent.resources | Where-Object { $_.type -eq 'Microsoft.Insights/components'}
 
@@ -45,25 +48,22 @@ try {
     return $appInsightResourceWithoutWorkspaceResourceList;
   }
 
-  $baseFolderPath = $baseFolderPath + "/"
-  $baseFolderPath = $baseFolderPath.replace("//", "/")
-
   # identify if changes are in dataConnectors folder which is at root of the repo
   $diff = git diff --diff-filter=d --name-only --first-parent HEAD^ HEAD
   Write-Host "List of files in PR: $diff"
   $standaloneDataConnectors = $diff | Where-Object { $_ -like 'DataConnectors/*' -and $_ -like '*azuredeploy*' }
-
   $hasStandaloneDataConnectors = ($null -ne $standaloneDataConnectors -and $standaloneDataConnectors.Count -gt 0) ? $true : $false
 
   if ($hasStandaloneDataConnectors) {
+    Write-Host "Standalone dataConnector files $standaloneDataConnectors"
     $failedStandaloneDataConnectorsList = @()
     # has change in standalone dataconnectors folder
-    $failedStandaloneDataConnectorsList = GetFilesWithoutWorkspaceResourceIds
+    $failedStandaloneDataConnectorsList = GetFilesWithoutWorkspaceResourceIds -filesList $standaloneDataConnectors
 
     if ($failedStandaloneDataConnectorsList.Count -gt 0) {
-      Write-Host "Please add property 'WorkspaceResourceId' for 'Microsoft.Insights/components' type in below given file(s)!"
+      Write-Host "::error:: Please add property 'WorkspaceResourceId' for 'Microsoft.Insights/components' type in below given file(s)!"
       foreach ($filePath in $failedStandaloneDataConnectorsList) {
-        Write-Host "$filePath"
+        Write-Host "::error:: --> $filePath"
       }
     }
   }
@@ -75,25 +75,31 @@ try {
     Write-Host "SolutionName is $solutionName"
     $solutionFolderPath = 'Solutions/' + $solutionName + "/"
     $filesinSolutions = git ls-files | Where-Object { $_ -like "$solutionFolderPath*" }
-
+    Write-Host "Solution files $filesinSolutions"
     if ($null -ne $filesinSolutions -and $filesinSolutions.Count -gt 0) {
       $filterSolutionFiles = $filesinSolutions | Where-Object { $_ -like '*azureDeploy*' }
 
       if ($null -ne $filterSolutionFiles -and $filterSolutionFiles.Count -gt 0) {
         $filterSolutionFilesCount = $filterSolutionFiles.Count
-        Write-Host "azuredeploy files found $filterSolutionFilesCount"
+        Write-Host "Number of azuredeploy files found in Solution $filterSolutionFilesCount"
         
         $appInsightResourceWithoutWorkspaceResourceList = @()
-        $appInsightResourceWithoutWorkspaceResourceList = GetFilesWithoutWorkspaceResourceIds
+        $appInsightResourceWithoutWorkspaceResourceList = GetFilesWithoutWorkspaceResourceIds -filesList $filterSolutionFiles
 
         if ($appInsightResourceWithoutWorkspaceResourceList.Count -gt 0) {
-          Write-Host "Please add property 'WorkspaceResourceId' for 'Microsoft.Insights/components' type in below given file(s)!"
+          Write-Host "::error:: Please add property 'WorkspaceResourceId' for 'Microsoft.Insights/components' type in below given file(s)!"
           foreach ($filePath in $appInsightResourceWithoutWorkspaceResourceList) {
-            Write-Host "$filePath"
+            Write-Host "::error:: --> $filePath"
           }
         }
       }
     }
+  }
+
+  if (($null -ne $failedStandaloneDataConnectorsList -and 
+  $failedStandaloneDataConnectorsList.Count -gt 0) -or 
+  ($null -ne $appInsightResourceWithoutWorkspaceResourceList -and $appInsightResourceWithoutWorkspaceResourceList.Count -gt 0)) {
+    exit 1
   }
 }
 catch {
