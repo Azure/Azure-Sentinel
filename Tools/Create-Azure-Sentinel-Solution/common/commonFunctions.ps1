07184aa7c9fa4e3f2b63b9ec2d019989f6b25d4c
v-amolpatil <v-amolpatil@microsoft.com>
2024-02-18 02:33:19 +0530

    added change for automatic release note

--- Tools/Create-Azure-Sentinel-Solution/common/commonFunctions.ps1
+++ Tools/Create-Azure-Sentinel-Solution/common/commonFunctions.ps1
@@ -2976,6 +2976,8 @@ function PrepareSolutionMetadata($solutionMetadataRawContent, $contentResourceDe
     {
         if ($contentToImport.Description) {
             $global:baseCreateUiDefinition.parameters.config.basics.description = $global:baseCreateUiDefinition.parameters.config.basics.description -replace "{{SolutionDescription}}", $contentToImport.Description
+
+            $global:baseCreateUiDefinition.parameters.config.basics.description = $global:baseCreateUiDefinition.parameters.config.basics.description -replace "{{SolutionName}}", $solutionName
         }
         else {
             $global:baseCreateUiDefinition.parameters.config.basics.description = $global:baseCreateUiDefinition.parameters.config.basics.description -replace "{{SolutionDescription}}", ""
