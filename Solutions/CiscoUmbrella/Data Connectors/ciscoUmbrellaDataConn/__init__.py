276813cfbc5e67900eb9245dd027dcbf573d7a77
Meena Kumari Chatla <v-mchatla@microsoft.com>
2024-03-01 13:09:22 +0530

    Updating statemarker with EndTime when there is no file to process

--- Solutions/CiscoUmbrella/Data Connectors/ciscoUmbrellaDataConn/__init__.py
+++ Solutions/CiscoUmbrella/Data Connectors/ciscoUmbrellaDataConn/__init__.py
@@ -58,7 +58,7 @@ def main(mytimer: func.TimerRequest) -> None:
     if ts_from is not None:
         if (datetime.datetime.utcnow() - datetime.timedelta(days=3)) > datetime.datetime.strptime(ts_from,"%Y-%m-%dT%H:%M:%S.%fZ"):
             ts_from = parse_date_from(ts_from)
-            ts_to = ts_from +  datetime.timedelta(minutes=30)
+            ts_to = ts_from +  datetime.timedelta(days=1)
         else:
             ts_to = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
     ts_from = parse_date_from(ts_from)
@@ -109,6 +109,16 @@ def main(mytimer: func.TimerRequest) -> None:
                     if check_if_script_runs_too_long(script_start_time):
                         logging.info(f'Script is running too long. Stop processing new events. Finish script.')
                         break
+                else:
+                    state_manager_cu.post(datetime.datetime.strftime(ts_to, '%Y-%m-%dT%H:%M:%S.%fZ'))
+                    if check_if_script_runs_too_long(script_start_time):
+                        logging.info(f'Script is running too long. Stop processing new events. Finish script.')
+                        break
+        if last_ts:
+            state_manager_cu.post(datetime.datetime.strftime(last_ts, '%Y-%m-%dT%H:%M:%S.%fZ'))
+        else:
+            state_manager_cu.post(datetime.datetime.strftime(ts_to, '%Y-%m-%dT%H:%M:%S.%fZ'))
+
         failed_sent_events_number = sum([sentinel.failed_sent_events_number for sentinel in sentinel_dict.values()])
         successfull_sent_events_number = sum([sentinel.successfull_sent_events_number for sentinel in sentinel_dict.values()])
 
@@ -124,9 +134,16 @@ def main(mytimer: func.TimerRequest) -> None:
                     if check_if_script_runs_too_long(script_start_time):
                         logging.info(f'Script is running too long. Stop processing new events. Finish script.')
                         return
+                else:
+                    state_manager_cu.post(datetime.datetime.strftime(ts_to, '%Y-%m-%dT%H:%M:%S.%fZ'))
+                    if check_if_script_runs_too_long(script_start_time):
+                        logging.info(f'Script is running too long. Stop processing new events. Finish script.')
+                        return
             
             if last_ts:
                 state_manager_cu.post(datetime.datetime.strftime(last_ts, '%Y-%m-%dT%H:%M:%S.%fZ'))
+            else:
+                state_manager_cu.post(datetime.datetime.strftime(ts_to, '%Y-%m-%dT%H:%M:%S.%fZ'))   
                 
         failed_sent_events_number += sentinel.failed_sent_events_number
         successfull_sent_events_number += sentinel.successfull_sent_events_number
