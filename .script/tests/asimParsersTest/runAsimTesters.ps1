46cdb66c7c9c3f0ecb0c878095ba83b285f35025
vakohl <97222872+vakohl@users.noreply.github.com>
2024-05-16 17:15:01 +0530

    test

--- .script/tests/asimParsersTest/runAsimTesters.ps1
+++ .script/tests/asimParsersTest/runAsimTesters.ps1
@@ -22,20 +22,20 @@ function run {
 }
 
 function getModifiedAsimSchemas() {
-    $schemas = ("ASimDns", "ASimWebSession", "ASimNetworkSession", "ASimProcessEvent", "ASimAuditEvent", "ASimAuthentication", "ASimFileEvent", "ASimRegistryEvent","ASimUserManagement","ASimDhcpEvent")
+    #$schemas = ("ASimDns", "ASimWebSession", "ASimNetworkSession", "ASimProcessEvent", "ASimAuditEvent", "ASimAuthentication", "ASimFileEvent", "ASimRegistryEvent","ASimUserManagement","ASimDhcpEvent")
     #$modifiedSchemas = @()
     $modifiedFiles = @()
-    foreach ($schema in $schemas) {
-        $filesThatWereChanged= Invoke-Expression "git diff origin/master  --name-only -- $($PSScriptRoot)/../Parsers/$($schema)/Parsers"
-        if ($filesThatWereChanged) {
-            Write-Host Files that were changed under Azure-Sentinel/Parsers/$schema/ARM:
-			Write-Host  - $filesThatWereChanged
-            $modifiedFiles += $filesThatWereChanged
-        }
-        else {
-            Write-Host "No files were changed under Azure-Sentinel/Parsers/$schema/"
-        }
-    }
+    #foreach ($schema in $schemas) {
+        $modifiedFiles= Invoke-Expression "git diff origin/master  --name-only -- $($PSScriptRoot)/../Parsers/"
+        # if ($filesThatWereChanged) {
+        #     Write-Host Files that were changed under Azure-Sentinel/Parsers/$schema/ARM:
+		# 	Write-Host  - $filesThatWereChanged
+        #    $modifiedFiles = $filesThatWereChanged
+        # }
+        # else {
+        #     Write-Host "No files were changed under Azure-Sentinel/Parsers/$schema/"
+        # }
+    #}
 
     return $modifiedFiles
 }
