a86569cbf470011438edf428d52d779bbac664a0
vakohl <97222872+vakohl@users.noreply.github.com>
2024-06-18 13:37:42 +0530

    test

--- .script/tests/asimParsersTest/VerifyASimParserTemplate.py
+++ .script/tests/asimParsersTest/VerifyASimParserTemplate.py
@@ -95,10 +95,10 @@ def extract_and_check_properties(Parser_file, Union_Parser__file, FileType, Pars
     # If 'EventProduct' was found in the KQL query, extract its value
     if match:
         event_product = match.group(1)
-        results.append((event_product, 'EventProduct found in parser query', True))
+        results.append((event_product, 'EventProduct found in parser query', 'Pass'))
     # If 'EventProduct' was not found in the KQL query, add to results
     else:
-        results.append(('\033[91mEventProduct\033[0m', '\033[91mEventProduct not found in Parser query\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mEventProduct\033[0m', '\033[91mEventProduct not found in Parser query\033[0m', '\033[91mFail\033[0m'))
 
     # Use a regular expression to find 'EventVendor' in the KQL query
     match = re.search(r'EventVendor\s*=\s*[\'"]([^\'"]+)[\'"]', parser_query)
@@ -106,71 +106,71 @@ def extract_and_check_properties(Parser_file, Union_Parser__file, FileType, Pars
     # If 'EventVendor' was found in the KQL query, extract its value
     if match:
         event_vendor = match.group(1)
-        results.append((event_vendor, 'EventVendor found in parser query', True))
+        results.append((event_vendor, 'EventVendor found in parser query', 'Pass'))
     # If 'EventVendor' was not found in the KQL query, add to results
     else:
-        results.append(('\033[91mEventVendor\033[0m', '\033[91mEventVendor not found in Parser query\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mEventVendor\033[0m', '\033[91mEventVendor not found in Parser query\033[0m', '\033[91mFail\033[0m'))
 
     # Check if parser_name exists in another_yaml_file's 'ParserQuery'
     if parser_name:
         if parser_name in Union_Parser__file.get('ParserQuery', ''):
-            results.append((parser_name, 'ParserName exist in union parser', True))
+            results.append((parser_name, 'ParserName exist in union parser', 'Pass'))
         else:
-            results.append((parser_name, 'ParserName not found in union parser', False))
+            results.append((parser_name, 'ParserName not found in union parser', '\033[91mFail\033[0m'))
 
     # Check if equivalent_built_in_parser exists in another_yaml_file's 'Parsers'
     if equivalent_built_in_parser:
         if equivalent_built_in_parser in Union_Parser__file.get('Parsers', []):
-            results.append((equivalent_built_in_parser, 'EquivalentBuiltInParser exist in union parser', True))
+            results.append((equivalent_built_in_parser, 'EquivalentBuiltInParser exist in union parser', 'Pass'))
         else:
-            results.append(('\033[91m' + str(equivalent_built_in_parser) + '\033[0m', '\033[91mEquivalentBuiltInParser not found in union parser\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(equivalent_built_in_parser) + '\033[0m', '\033[91mEquivalentBuiltInParser not found in union parser\033[0m', '\033[91mFail\033[0m'))
 
     # Check if title exists in yaml_file's 'Parser'->'Title'       
     if title:
-        results.append((title, 'This value exist in Title property', True))
+        results.append((title, 'This value exist in Title property', 'Pass'))
     else:
-        results.append(('Title', 'Title not found in parser YAML', False))
+        results.append(('Title', 'Title not found in parser YAML', '\033[91mFail\033[0m'))
     # Check if version exists in yaml_file's 'Parser'->'Version' and matches the format X.X.X
     if version:
         if re.match(r'^\d+\.\d+\.\d+$', version):
-            results.append((version, 'This value exist in Version property', True))
+            results.append((version, 'This value exist in Version property', 'Pass'))
         else:
-            results.append(('\033[91m' + str(version) + '\033[0m', '\033[91mVersion exist but format is incorrect\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(version) + '\033[0m', '\033[91mVersion exist but format is incorrect\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('Version', 'Version not found in parser YAML', False))
+        results.append(('Version', 'Version not found in parser YAML', '\033[91mFail\033[0m'))
 
     # Check if last_updated exists in yaml_file's 'Parser'->'LastUpdated' and matches the format MMM DD YY
     if last_updated:
         try:
             datetime.strptime(last_updated, '%b %d, %Y')
-            results.append((last_updated, 'This value exist in LastUpdated property', True))
+            results.append((last_updated, 'This value exist in LastUpdated property', 'Pass'))
         except ValueError:
-            results.append(('\033[91m' + str(last_updated) + '\033[0m', '\033[91mLastUpdated exist but format is incorrect\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(last_updated) + '\033[0m', '\033[91mLastUpdated exist but format is incorrect\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('\033[91mLastUpdated\033[0m', '\033[91mLastUpdated not found in parser YAML\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mLastUpdated\033[0m', '\033[91mLastUpdated not found in parser YAML\033[0m', '\033[91mFail\033[0m'))
     
     # Check if schema exists in yaml_file's 'Normalization'->'Schema' and matches with our SchemaInfo
     if schema:
         for info in SCHEMA_INFO:
             if info['SchemaName'] == schema:
-                results.append((schema, 'Schema name is correct', True))
+                results.append((schema, 'Schema name is correct', 'Pass'))
                 break
         else:
-            results.append(('\033[91m' + str(schema) + '\033[0m', '\033[91mSchema name is incorrect\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(schema) + '\033[0m', '\033[91mSchema name is incorrect\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('Schema', 'Schema name not found in parser YAML', False))
+        results.append(('Schema', 'Schema name not found in parser YAML', '\033[91mFail\033[0m'))
     
     # Check if Schema Version exists in yaml_file's 'Normalization'->'Schema' and matches with our SchemaInfo
     if schemaVersion:
         for info in SCHEMA_INFO:
             if schema == info.get('SchemaName'):
                 if info['SchemaVersion'] == schemaVersion and info['SchemaName'] == schema:
-                    results.append((schemaVersion, 'Schema Version is correct', True))
+                    results.append((schemaVersion, 'Schema Version is correct', 'Pass'))
                     break
                 else:
-                    results.append(('\033[91m' + str(schemaVersion) + '\033[0m', '\033[91mSchema Version is incorrect\033[0m', '\033[91mFalse\033[0m'))
+                    results.append(('\033[91m' + str(schemaVersion) + '\033[0m', '\033[91mSchema Version is incorrect\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('Version', 'Schema Version not found in parser YAML', False))
+        results.append(('Version', 'Schema Version not found in parser YAML', '\033[91mFail\033[0m'))
 
     # Check if references exist in yaml_file's 'References'
     if references:
@@ -183,32 +183,32 @@ def extract_and_check_properties(Parser_file, Union_Parser__file, FileType, Pars
                 linkSchemaInfo = info.get('SchemaLink')
                 if schema == info.get('SchemaName'):
                     if title == titleSchemaInfo and link == linkSchemaInfo:
-                        results.append((title, 'Schema specific reference link matching', True))
+                        results.append((title, 'Schema specific reference link matching', 'Pass'))
                     elif title == 'ASIM' and link == 'https:/aka.ms/AboutASIM':
-                        results.append((title, 'ASim doc reference link matching', True))
+                        results.append((title, 'ASim doc reference link matching', 'Pass'))
                     else:
-                        results.append(('\033[91m' + str(title) + '\033[0m', '\033[91mreference title or link not matching\033[0m', '\033[91mFalse\033[0m'))
+                        results.append(('\033[91m' + str(title) + '\033[0m', '\033[91mreference title or link not matching\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('\033[91mReferences\033[0m', '\033[91mReferences\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mReferences\033[0m', '\033[91mReferences\033[0m', '\033[91mFail\033[0m'))
 
     # Check if ParserName exists in yaml_file and matches the format ASIMAuditEvent<ProductName>
     if parser_name:
         if re.match(rf'{FileType}{schema}', parser_name):
-            results.append((parser_name, 'ParserName is in correct format', True))
+            results.append((parser_name, 'ParserName is in correct format', 'Pass'))
         else:
-            results.append(('\033[91m' + str(parser_name) + '\033[0m', '\033[91mParserName is not in correct format\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(parser_name) + '\033[0m', '\033[91mParserName is not in correct format\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('\033[91mParserName\033[0m', '\033[91mParserName not found\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mParserName\033[0m', '\033[91mParserName not found\033[0m', '\033[91mFail\033[0m'))
 
     # Check if EquivalentBuiltInParser exists in yaml_file and matches the format _ASIM_<Schema><ProductName>
     FileType = "Im" if FileType == "vim" else FileType
     if equivalent_built_in_parser:
         if re.match(rf'_{FileType}_{schema}_', equivalent_built_in_parser):
-            results.append((equivalent_built_in_parser, 'EquivalentBuiltInParser is in correct format', True))
+            results.append((equivalent_built_in_parser, 'EquivalentBuiltInParser is in correct format', 'Pass'))
         else:
-            results.append(('\033[91m' + str(equivalent_built_in_parser) + '\033[0m', '\033[91mEquivalentBuiltInParser is not in correct format\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(equivalent_built_in_parser) + '\033[0m', '\033[91mEquivalentBuiltInParser is not in correct format\033[0m', '\033[91mFail\033[0m'))
     else:
-        results.append(('\033[91mEquivalentBuiltInParser\033[0m', '\033[91mEquivalentBuiltInParser not found\033[0m', '\033[91mFalse\033[0m'))
+        results.append(('\033[91mEquivalentBuiltInParser\033[0m', '\033[91mEquivalentBuiltInParser not found\033[0m', '\033[91mFail\033[0m'))
 
     # Multi-line comment
     '''
@@ -228,9 +228,9 @@ def extract_and_check_properties(Parser_file, Union_Parser__file, FileType, Pars
         DataTestUrl = url_without_filename + "//Tests//" + filename
         response = requests.get(DataTestUrl)
         if response.status_code == 200:
-            results.append((filename, 'Tester file exists', True))
+            results.append((filename, 'Tester file exists', 'Pass'))
         else:
-            results.append(('\033[91m' + str(filename) + '\033[0m', '\033[91mTester file does not exist\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(filename) + '\033[0m', '\033[91mTester file does not exist\033[0m', '\033[91mFail\033[0m'))
     '''
     
     # Check if sample data files exists or not (Only applicable for ASim FileType)
@@ -242,9 +242,9 @@ def extract_and_check_properties(Parser_file, Union_Parser__file, FileType, Pars
         # check if file exists
         response = requests.get(SampleDataUrl)
         if response.status_code == 200:
-            results.append((SampleDataFile, 'Sample data exists', True))
+            results.append((SampleDataFile, 'Sample data exists', 'Pass'))
         else:
-            results.append(('\033[91m' + str(SampleDataFile) + '\033[0m', '\033[91mSample data does not exist\033[0m', '\033[91mFalse\033[0m'))
+            results.append(('\033[91m' + str(SampleDataFile) + '\033[0m', '\033[91mSample data does not exist\033[0m', '\033[91mFail\033[0m'))
     return results
 
 def filter_yaml_files(modified_files):
