a98f3f54ef0aba1bffbbe90bfce34f453ecd4c12
v-sabiraj <v-sabiraj@microsoft.com>
2024-06-26 16:41:17 +0530

    import error

--- Solutions/Box/Data Connectors/AzureFunctionBox/main.py
+++ Solutions/Box/Data Connectors/AzureFunctionBox/main.py
@@ -80,7 +80,7 @@ def main(mytimer: func.TimerRequest):
     date_time_str = '2024-05-28 17:49:00+00:00'
     date_format_str = '%Y-%m-%d %H:%M:%S%z'
 
-    if created_before >= datetime.strptime(date_time_str, date_format_str):
+    if created_before >= datetime.datetime.strptime(date_time_str, date_format_str):
         return
 
     sentinel = AzureSentinelConnector(workspace_id=WORKSPACE_ID, logAnalyticsUri=logAnalyticsUri,
