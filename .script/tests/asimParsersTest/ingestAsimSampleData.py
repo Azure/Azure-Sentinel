c63a26938f46e196c170870233f9f29249eea3c6
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-08-02 12:49:42 +0530

    Update ingestAsimSampleData.py

--- .script/tests/asimParsersTest/ingestAsimSampleData.py
+++ .script/tests/asimParsersTest/ingestAsimSampleData.py
@@ -302,6 +302,7 @@ for file in parser_yaml_files:
         # create table 
         request_body, url_to_call , method_to_use = create_table(json.dumps(schema_result, indent=4),table_name)
         response_body=hit_api(url_to_call,request_body,method_to_use)
+	print(f"Response of table creation: {response_body.text}")    
         #Once table is created now creating DCR
         request_body, url_to_call , method_to_use ,stream_name = create_dcr(json.dumps(schema_result, indent=4),table_name,"Custom")  
         response_body=hit_api(url_to_call,request_body,method_to_use)
