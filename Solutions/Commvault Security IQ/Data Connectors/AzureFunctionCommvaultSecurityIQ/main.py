b68fdea46de9d4d9d630361b573f7f94af87b3c4
Sentinel <v-sudkharat@microsoft.com>
2024-08-30 18:14:48 +0530

    Tokenupdate

--- Solutions/Commvault Security IQ/Data Connectors/AzureFunctionCommvaultSecurityIQ/main.py
+++ Solutions/Commvault Security IQ/Data Connectors/AzureFunctionCommvaultSecurityIQ/main.py
@@ -35,7 +35,7 @@ if (not match):
 key_vault_name = os.environ.get("KeyVaultName")
 uri = None
 url = None
-qsdk_token = qsdk_token or ""
+qsdk_token = os.environ.get("QSDKToken")
 headers = {
     "authtoken": "QSDK " + qsdk_token,
     "Content-Type": "application/json",
