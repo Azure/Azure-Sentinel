6d55b6d6568043b20376e3f3cfd5209dd4cb2f81
Gary Bushey <garybushey@gmail.com>
2024-09-25 13:32:29 -0400

    Update Create-NewSolutionAndRulesFromList.ps1

--- Tools/Sentinel-All-In-One/v2/Scripts/Create-NewSolutionAndRulesFromList.ps1
+++ Tools/Sentinel-All-In-One/v2/Scripts/Create-NewSolutionAndRulesFromList.ps1
@@ -14,7 +14,7 @@ $VerbosePreference = "Continue"
 $result = Clear-AzContext -Force -PasSThru
 Write-Output "Clear AzContext: $result"
 
-Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId
+Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId -UseDeviceAuthentication
 
 $context = Get-AzContext
 Write-Output "Connected to Azure with context: " $context
