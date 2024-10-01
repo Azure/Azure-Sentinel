ae0a4d3dc1da83a57f160e568d621d353e9d7cc6
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-10-01 12:47:04 +0000

    package sh

---
+++ Solutions/AbnormalSecurity/Data Connectors/package.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf .python_packages
+
+# Get the version of Python 3
+PYTHON_VERSION=$(python3 --version 2>&1)
+
+# Check if it contains "Python 3.11"
+if [[ $PYTHON_VERSION == "Python 3.11"* ]]; then
+    echo "Python 3.11 is being used."
+else
+    echo "Python 3.11 is NOT being used."
+    exit 1
+fi
+
+python3 -m venv .python_packages
+source .python_packages/bin/activate
+pip install -r requirements.txt
+
+cd .python_packages/lib
+ln -s python3.11/site-packages site-packages
+cd ../..
+
+git checkout origin/master AbnormalSecurityConn.zip
+
+zip -r AbnormalSecurityConnTest.zip SentinelFunctionsOrchestrator .python_packages/lib/site-packages requirements.txt 
