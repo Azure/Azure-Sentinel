7a33ea2aaf827e1e1c1ed5eea50f474a1cefbe7d
Varun Kohli <97222872+vakohl@users.noreply.github.com>
2024-10-21 10:37:47 +0530

    test

--- .script/tests/asimParsersTest/ASimFilteringTest.py
+++ .script/tests/asimParsersTest/ASimFilteringTest.py
@@ -585,13 +585,19 @@ class FilteringTest(unittest.TestCase):
 
             value = row[COLUMN_INDEX_IN_ROW]
             post = get_postfix(value, rows, substrings_list, delimiter)
-            # Post will equal value if: value dont contain the delimiter, post is in the list, post is contained in an item in the list.
-            if post != value:
+
+            # Add post to the list if it's not already present
+            if post not in substrings_list:
                 substrings_list.append(post)
-            else:
+
+            # If the list has reached the required number of substrings, break the loop
+            if len(substrings_list) == num_of_substrings:
+                break
+            
+            # If post is equal to value, also add pre to the list
+            if post == value:
                 pre = get_prefix(value, rows, substrings_list, delimiter)
-                # pre will equal value if: value dont contain the delimiter, pre is in the list, pre is contained in an item in the list.
-                if pre != value:
+                if pre not in substrings_list:
                     substrings_list.append(pre)
             
         return substrings_list
