28d9e7546a846153b6f69a5fd7ba20cf949fb36f
v-amolpatil <v-amolpatil@microsoft.com>
2024-11-18 16:53:16 +0530

    updated all test cases

--- .script/tests/KqlvalidationsTests/KqlValidationTests.cs
+++ .script/tests/KqlvalidationsTests/KqlValidationTests.cs
@@ -90,7 +90,7 @@ namespace Kqlvalidations.Tests
             var id = (string)res["id"];
 
             //we ignore known issues. We also ignore templates that are not in the skipped templates list.
-            if (ShouldSkipTemplateValidation(id))
+            if (ShouldSkipTemplateValidation(id) || !res.ContainsKey("query"))
             {
                 return;
             }
@@ -115,7 +115,7 @@ namespace Kqlvalidations.Tests
             var id = (string)res["id"];
 
             //we ignore known issues
-            if (ShouldSkipTemplateValidation(id))
+            if (ShouldSkipTemplateValidation(id) || !res.ContainsKey("query"))
             {
                 return;
             }
@@ -271,8 +271,8 @@ namespace Kqlvalidations.Tests
             var queryParamsAsLetStatements = GenerateFunctionParametersAsLetStatements(yaml);
 
             //Ignore known issues
-            yaml.TryGetValue("Id", out object id);
-            if (id != null && ShouldSkipTemplateValidation((string)yaml["Id"]))
+            yaml.TryGetValue("id", out object id);
+            if (id != null && ShouldSkipTemplateValidation((string)yaml["id"]))
             {
                 return;
             }
