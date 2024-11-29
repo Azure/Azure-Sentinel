8a04a9e3d6bb95154a8fb2d8f320f656fac282b7
v-sabiraj <v-sabiraj@microsoft.com>
2024-11-29 09:50:25 +0530

    changing cv

--- Solutions/Bitglass/Data Connectors/BitglassSentinelConnector/__init__.py
+++ Solutions/Bitglass/Data Connectors/BitglassSentinelConnector/__init__.py
@@ -58,7 +58,7 @@ class BG_CASB():
         if token is None:
             self.params = {
                 "startdate": self.from_time,
-                "cv": "1.0.1",
+                "cv": "1.1.5",
                 "type": self.event_type,
                 "responseformat": "json"
             }
