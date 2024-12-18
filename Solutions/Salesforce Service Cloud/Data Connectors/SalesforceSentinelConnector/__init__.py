929362595b780cfe54447e2704099613c6a45942
Ido Shabi <idoshabi@microsoft.com>
2024-12-18 10:16:36 +0200

    adding fix

--- Solutions/Salesforce Service Cloud/Data Connectors/SalesforceSentinelConnector/__init__.py
+++ Solutions/Salesforce Service Cloud/Data Connectors/SalesforceSentinelConnector/__init__.py
@@ -66,17 +66,17 @@ def generate_date():
     return past_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 def check_size(queue):
-        data_bytes_len = len(json.dumps(queue).encode())
-        return data_bytes_len < FIELD_SIZE_LIMIT_BYTES
+    data_bytes_len = len(json.dumps(queue).encode())
+    return data_bytes_len < FIELD_SIZE_LIMIT_BYTES
 
 
 def split_big_request(queue):
-        if check_size(queue):
-            return [queue]
-        else:
-            middle = int(len(queue) / 2)
-            queues_list = [queue[:middle], queue[middle:]]
-            return split_big_request(queues_list[0]) + split_big_request(queues_list[1])
+    if check_size(queue):
+        return [queue]
+    else:
+        middle = int(len(queue) / 2)
+        queues_list = [queue[:middle], queue[middle:]]
+        return split_big_request(queues_list[0]) + split_big_request(queues_list[1])
 
 def pull_log_files():
     past_time = generate_date()
@@ -153,21 +153,21 @@ def gen_chunks(file_in_tmp_path):
             if 'action' in row:
                 if ('message' in row['action']) and (len(json.dumps(row['action']['message']).encode()) > FIELD_SIZE_LIMIT_BYTES):
                     if isinstance(row['action']['message'],list):
-                            queue_list = split_big_request(row['action']['message'])
-                            count = 1
-                            for q in queue_list:
-                                columnname = 'messagePart' + str(count)
-                                row['action'][columnname] = q
-                                count+=1
-                            del row['action']['message']
+                        queue_list = split_big_request(row['action']['message'])
+                        count = 1
+                        for q in queue_list:
+                            columnname = 'messagePart' + str(count)
+                            row['action'][columnname] = q
+                            count+=1
+                        del row['action']['message']
                             
                     elif isinstance(row['action']['message'],dict):
-                            queue_list = list(row['action']['message'].keys())
-                            for count, key in enumerate(queue_list, 1):
-                                if count > 10:
-                                    break
-                                row['action'][f"messagePart{key}"] = row['action']['message'][key]
-                            del row['action']['message']
+                        queue_list = list(row['action']['message'].keys())
+                        for count, key in enumerate(queue_list, 1):
+                            if count > 10:
+                                break
+                            row['action'][f"messagePart{key}"] = row['action']['message'][key]
+                        del row['action']['message']
 
                     else:
                         pass
