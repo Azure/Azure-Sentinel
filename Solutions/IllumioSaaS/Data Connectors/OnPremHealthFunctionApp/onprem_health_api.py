5d265fa939f75c2c02ffa371f6ae1a7d00fa8f1d
ashwin <ashwin.venkatesha@illumio.com>
2025-01-16 14:31:17 -0800

    Added onprem support for Illumio solution; Added a new workbook and modified existing to handle onprem and saas pce; modified function app code

---
+++ Solutions/IllumioSaaS/Data Connectors/OnPremHealthFunctionApp/onprem_health_api.py
@@ -0,0 +1,35 @@
+import azure.functions as func
+import logging
+import json
+import requests
+from base64 import b64encode
+from ..CommonCode.constants import (
+    ONPREM_API_KEY,
+    ONPREM_API_SECRET,
+    ONPREM_PCE_FQDN,
+    ONPREM_PCE_PORT,
+)
+
+URL = "https://{}:{}/api/v2/health".format(ONPREM_PCE_FQDN, ONPREM_PCE_PORT)
+
+credentials = b64encode(f"{ONPREM_API_KEY}:{ONPREM_API_SECRET}".encode()).decode(
+    "utf-8"
+)
+headers = {"Authorization": f"Basic {credentials}", "Content-type": "application/json"}
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Making an api call to onprem PCE to get health metadata")
+    response = requests.request("GET", URL, headers=headers, data={})
+
+    if response:
+        logging.info("[TimedApi] Response from url is {}".format(response.headers))
+    else:
+        logging.info("[TimedApi] Error in response {}".format(response))
+        return
+
+    body = json.loads(response.text)
+
+    return func.HttpResponse(
+        json.dumps(body), status_code=response.status_code, mimetype="application/json"
+    )
