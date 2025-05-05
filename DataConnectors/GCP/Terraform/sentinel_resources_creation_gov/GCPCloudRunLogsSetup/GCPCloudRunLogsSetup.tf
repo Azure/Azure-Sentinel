559f45c26e7687ec1ca8475c241301fff4995464
v-anjohari <v-anjohari@microsoft.com>
2025-05-05 16:01:53 +0530

    halting

--- DataConnectors/GCP/Terraform/sentinel_resources_creation_gov/GCPCloudRunLogsSetup/GCPCloudRunLogsSetup.tf
+++ DataConnectors/GCP/Terraform/sentinel_resources_creation_gov/GCPCloudRunLogsSetup/GCPCloudRunLogsSetup.tf
@@ -55,7 +55,7 @@ resource "google_logging_project_sink" "sentinel-sink" {
   destination = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic-name}"
   depends_on = [google_pubsub_topic.sentinel-cloudrun-topic]
   
-  filter = "resource.type=\"cloud_run_revision\""
+  filter = "resource.type=\"cloud_run_revision\" AND logName=\"projects/${data.google_project.project.project_id}/logs/run.googleapis.com%2Frequests\""
   unique_writer_identity = true
 }
 
@@ -65,7 +65,7 @@ resource "google_logging_organization_sink" "sentinel-organization-sink" {
   org_id = var.organization-id
   destination = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic-name}"
 
-  filter = "resource.type=\"cloud_run_revision\""
+  filter = "resource.type=\"cloud_run_revision\" AND logName=\"projects/${data.google_project.project.project_id}/logs/run.googleapis.com%2Frequests\""
   include_children = true
 }
 
