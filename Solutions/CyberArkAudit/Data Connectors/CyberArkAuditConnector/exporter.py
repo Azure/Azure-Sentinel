d0f4a9c864c1ef6f9201783cea81d505b5a2b261
nitsan-tzur <153099157+nitsan-tzur@users.noreply.github.com>
2025-07-14 16:53:31 -0400

    add new columns

--- Solutions/CyberArkAudit/Data Connectors/CyberArkAuditConnector/exporter.py
+++ Solutions/CyberArkAudit/Data Connectors/CyberArkAuditConnector/exporter.py
@@ -9,6 +9,7 @@ def _transform_schema(audit_events: list) -> list:
         {
             'CyberArkTenantId': audit['tenantId'],
             'accountName': audit.get('accountName') or '',
+            'accessMethod': audit.get('accessMethod') or '',
             'action': audit.get('action') or '',
             'actionType': audit.get('actionType') or '',
             'applicationCode': audit.get('applicationCode') or '',
@@ -18,8 +19,10 @@ def _transform_schema(audit_events: list) -> list:
             'cloudAssets': audit.get('cloudAssets') or '',
             'cloudIdentities': audit.get('cloudIdentities') or '',
             'cloudProvider': audit.get('cloudProvider') or '',
+            'cloudWorkspacesAndRoles': audit.get('cloudWorkspacesAndRoles') or '',
             'command': audit.get('command') or '',
             'component': audit.get('component') or '',
+            'customData': audit.get('customData') or '',
             'identityType': audit.get('identityType') or '',
             'message': audit.get('message') or '',
             'target': audit.get('target') or '',
@@ -31,7 +34,8 @@ def _transform_schema(audit_events: list) -> list:
             'serviceName': audit.get('serviceName') or '',
             'source': audit.get('source') or '',
             'userId': audit.get('userId') or '',
-            'username': audit.get('username') or ''
+            'username': audit.get('username') or '',
+            'uuid': audit.get('uuid') or ''
         }
         for audit in audit_events]
     return dcr_events
