a931b6f881976d355a100a17ff4ec3d01e92a89d
Nikita Konoplin <nikita.konoplin@veeam.com>
2025-08-29 17:59:58 +0200

    feat: change solution structure and fix validations

---
+++ Solutions/Veeam/Data Connectors/AzureFunctionVeeam/Veeam.Sentinel.FunctionApp/Constants/VoneRestApiConstants.cs
@@ -0,0 +1,12 @@
+namespace Sentinel.Constants
+{
+    internal static class VoneRestApiConstants
+    {
+        public const string Authorization = "Authorization";
+        public const string Bearer = "bearer";
+        public const string VonePasswordAlias = "keyVaultPasswordId";
+        public const string VoneUsernameAlias = "keyVaultUsernameId";
+        public const string VoneBaseUrlAlias = "baseUrl";
+        public const string LoginFailedIncorrectCredentials = "Login failed. Incorrect credentials.";
+    }
+}
--- Solutions/Veeam/Data Connectors/FunctionApp/README.md
+++
@@ -1,5 +0,0 @@
-Roles:
-- KeyVault Officer
-- Sentinel Contributor
-- 92aaf0da-9dab-42b6-94a3-d43ce8d16293 to log analytics 
-- 3913510d-42f4-4e42-8a64-420c390055eb to every dcr 
