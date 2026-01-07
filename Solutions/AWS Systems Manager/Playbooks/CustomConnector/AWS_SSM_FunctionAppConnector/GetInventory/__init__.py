1d26facb4b50d1645941c8e3d983c8372d21f1f1
v-kasghosh <v-kasghosh@microsoft.com>
2026-01-07 12:28:40 +0530

    Fix JSON parsing in GetInventory main function

--- Solutions/AWS Systems Manager/Playbooks/CustomConnector/AWS_SSM_FunctionAppConnector/GetInventory/__init__.py
+++ Solutions/AWS Systems Manager/Playbooks/CustomConnector/AWS_SSM_FunctionAppConnector/GetInventory/__init__.py
@@ -39,7 +39,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if not (filters and aggregators and result_attributes and next_token and max_results):
         try:
-            req_body = json.loads(req.get_json())
+            req_body = req.get_json(silent=True) or {}
         except ValueError:
             pass
             logging.error('Error parsing JSON body.')
