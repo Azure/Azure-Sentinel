8376a2cbc703ef76c63eba9fce6e4b2dce4eee1f
v-rusraut <v-rusraut@microsoft.com>
2026-06-22 16:42:47 +0530

    Update Test.js

--- Solutions/Amazon Web Services/Test.js
+++ Solutions/Amazon Web Services/Test.js
@@ -1,10 +1,44 @@
-// test-vuln.js
 const express = require("express");
-const app = express();
 const mysql = require("mysql");
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "testdb",
+});
+
+connection.connect();
 
 app.get("/user", (req, res) => {
   const id = req.query.id;
   const query = "SELECT * FROM users WHERE id = '" + id + "'"; // CWE-89: SQL Injection
-  connection.query(query);
+  connection.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send("Error");
+      return;
+    }
+    res.json(results);
+  });
 });
+
+app.get("/exec", (req, res) => {
+  const { exec } = require("child_process");
+  const cmd = req.query.cmd;
+  exec("ping " + cmd, (err, stdout) => {
+    // CWE-78: OS Command Injection
+    res.send(stdout);
+  });
+});
+
+app.get("/file", (req, res) => {
+  const fs = require("fs");
+  const filePath = req.query.path;
+  fs.readFile(filePath, "utf8", (err, data) => {
+    // CWE-22: Path Traversal
+    res.send(data);
+  });
+});
+
+app.listen(3000);
